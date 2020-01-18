06ad4de54a08ff861c4a0d914411e2c9d8e95518
Michael Brandt <michaelbrandt5@gmail.com>
2020-01-18 16:14:23 -0700

    tidy up View::Tile a little, add more comments

--- assets/js/view/tile.rb
+++ assets/js/view/tile.rb
@@ -10,32 +10,44 @@ module View
       a = path.min
       b = path.max
 
+      # diff = how many steps apart the two edges connected by the path are
+      #
+      # rotate = degrees to rotate the svg path for this track path; e.g., a
+      # normal gentle is 0,2; for 1,3, rotate = 60
       diff = b - a
-      rotate = 60 * a
-
       if diff > 3
         diff = (a - b) % 6
         rotate = 60 * b
+      else
+        rotate = 60 * a
       end
 
       transform = "rotate(#{rotate})"
 
-      case diff
-      when 1  # sharp
-        [
-          h(:path, attrs: { transform: transform, d: 'm 0 85 L 0 75 A 43.30125 43.30125 0 0 0 -64.951875 37.5 L -73.612125 42.5', stroke: 'black', 'stroke-width' => 8 }),
-        ]
-      when 2  # gentle
-        [
-          h(:path, attrs: { transform: transform, d: 'm 0 85 L 0 75 A 129.90375 129.90375 0 0 0 -64.951875 -37.5 L -73.612125 -42.5', stroke: 'black', 'stroke-width' => 8 }),
-        ]
-      when 3  # straight
-        [
-          h(:path, attrs: { transform: transform, d: 'm 0 87 L 0 -87', stroke: 'black', 'stroke-width' => 8 }),
+      d = case diff
+
+      # sharp
+      when 1
+          'm 0 85 L 0 75 A 43.30125 43.30125 0 0 0 -64.951875 37.5 L -73.612125 42.5'
+
+      # gentle
+      when 2
+          'm 0 85 L 0 75 A 129.90375 129.90375 0 0 0 -64.951875 -37.5 L -73.612125 -42.5'
+
+      # straight
+      when 3
+          'm 0 87 L 0 -87'
           # h(:path, attrs: { d: 'm -4 86 L -4 -86', stroke: 'white', 'stroke-width' => 2 }),
-          # h(:path, attrs: { d: 'm 4 86 L 4 -86', stroke: 'white', 'stroke-width' => 2 }),
-        ]
+          # h(:path, attrs: { d: 'm 4 86 L 4 -86', stroke: 'white',
+          # 'stroke-width' => 2 }),
+
+      else
+        ''
       end
+
+      [
+        h(:path, attrs: { transform: transform, d: d, stroke: 'black', 'stroke-width' => 8 }),
+      ]
     end
 
     def hex_paths_to_svg_paths
