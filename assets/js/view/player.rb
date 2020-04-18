5a830b9c1f3a89a4a4fbda58779b8c775cf94998
Talbatross <cuband@gmail.com>
2020-04-17 19:09:07 -0700

    Make Player Holdings Prettier

--- assets/js/view/player.rb
+++ assets/js/view/player.rb
@@ -3,24 +3,57 @@
 module View
   class Player < Snabberb::Component
     needs :player
-    needs :game, store: true
+    needs :game
+
+    def render_shares
+      owned_shares = @player.shares_by_corporation.select { |_corporation, shares| shares.any? }
+
+      return 'Shares: None' if owned_shares.empty?
+
+      shares_div = owned_shares.map do |corporation, shares|
+        president = shares.any?(&:president)
+        corp_style = {
+          background: corporation.color,
+          color: 'white',
+          padding: '0.25rem'
+        }
+        h(:div, { style: corp_style }, "#{corporation.name} #{president ? '*' : ''}#{shares.sum(&:percent)}%")
+      end
+      [h(:div, 'Shares:'), *shares_div]
+    end
 
     def render
       style = {
-        border: 'solid 1px rgba(0,0,0,0.2)',
         display: 'inline-block',
+        cursor: 'pointer',
+        border: 'solid 1px gainsboro',
+        padding: '0.5rem',
+        margin: '0.5rem 0.5rem 0 0',
+        width: '300px',
+        'text-align': 'center',
+        'font-weight': 'bold',
+        'vertical-align': 'top',
       }
 
-      shares_div = @player.shares_by_corporation.map do |corporation, shares|
-        president = shares.any?(&:president)
-        h(:div, "#{corporation.name} #{president ? '*' : ''}#{shares.sum(&:percent)}%")
-      end
+      name_style = {
+        border: '1px solid gainsboro',
+        padding: '0.5rem',
+        'font-size': '150%'
+      }
+
+      cash_style = {
+        margin: '0.5rem 0'
+      }
+
+      shares_title_style = {
+        margin: '1rem 0'
+      }
 
       h(:div, { style: style }, [
-        h(:div, "name: #{@player.name}"),
-        h(:div, "cash: #{@game.format_currency(@player.cash)}"),
-        h(:div, "companies: #{@player.companies.map(&:name)}"),
-        *shares_div,
+        h(:div, { style: name_style }, @player.name),
+        h(:div, { style: cash_style }, "Cash: #{@game.format_currency(@player.cash)}"),
+        h(:div, { style: shares_title_style }, render_shares),
+        h(:div, "Companies: #{@player.companies.map(&:name)}"),
       ])
     end
   end
