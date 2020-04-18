e2234af99db33fbd7c1f636892be7d7e6f2ab1b3
Dan Schmidt <dfan@dfan.org>
2020-04-18 09:15:00 -0400

    Add vertical space between elements on game screen

--- assets/js/view/game.rb
+++ assets/js/view/game.rb
@@ -158,14 +158,23 @@ module View
     def render_game
       @round = @game.round
 
+      padding = {
+        margin: '20px 0px 20px 0px'
+      }
+
       h('div.game', [
         render_round,
+        h(:div, style: padding),
         h(Log, log: @game.log),
+        h(:div, style: padding),
         h(EntityOrder, round: @round),
+        h(:div, style: padding),
         render_action,
+        h(:div, style: padding),
         h(Exchange),
-        h(:div, 'Players'),
+        h(:div, style: padding),
         *@game.players.map { |p| h(Player, player: p, game: @game) },
+        h(:div, style: padding),
         @round.operating? ? h(Map, game: @game) : h(StockMarket, stock_market: @game.stock_market),
       ])
     end
