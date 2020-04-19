d29c8c2dcc56967c1d4829907d44e1a8966bd788
H C <1674771+hcarver@users.noreply.github.com>
2020-04-19 08:12:34 +0100

    Pacify Rubocop

--- assets/js/view/about.rb
+++ assets/js/view/about.rb
@@ -3,14 +3,15 @@
 module View
   class About < Snabberb::Component
     def render
+      about_string = <<~ABOUT
+        18xx.games is created and maintained by Toby Mao. It is an open source project, and you can find the
+        code at https://github.com/tobymao/18xx
+      ABOUT
       h(:div,
-        [h(:p, <<END
-18xx.games is created and maintained by Toby Mao. It is an open source project, and you can find the
-code at https://github.com/tobymao/18xx
-END
-        ),
-        h(:p, "The 1889 game is used with kind permission from Josh Starr at Grand Trunk Games.")
-      ])
+        [
+          h(:p, about_string),
+          h(:p, 'The 1889 game is used with kind permission from Josh Starr at Grand Trunk Games.')
+        ])
     end
   end
 end
