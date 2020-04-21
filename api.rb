3c5cb1ba1ee6a3b7aff37575446e3a4acbd6d519
Michael Brandt <michaelbrandt5@gmail.com>
2020-04-20 19:13:58 -0600

    move routing for /all_tiles
    https://github.com/tobymao/18xx/pull/109#discussion_r411096556

--- api.rb
+++ api.rb
@@ -106,6 +106,10 @@ class Api < Roda
     on 'api' do |hr|
       hr.hash_routes :api
     end
+
+    on 'all_tiles' do
+      render(app_route: 'all_tiles')
+    end
   end
 
   route do |r|
@@ -126,10 +130,6 @@ class Api < Roda
     r.on 'game', Integer do |id|
       r.halt 404 unless (game = Game[id])
       render(game_data: game.to_h(include_actions: true))
-    end
-
-    r.on 'all_tiles' do
-      render(app_route: 'all_tiles')
     end
   end
 
