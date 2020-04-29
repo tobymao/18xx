d0b0db946f1d03a2aa12e0a8206b9a0a4f62b6b4
Jonathan Coveney <jcoveney@gmail.com>
2020-04-29 20:14:28 +0800

    Add spreadsheet tab with a spreadsheet view of the game state

---
+++ assets/js/view/spreadsheet.rb
@@ -0,0 +1,126 @@
+# frozen_string_literal: true
+
+module View
+  class Spreadsheet < Snabberb::Component
+    needs :game
+
+    def render
+      props = {
+         style: {
+          margin: '1rem 0 1.5rem 0',
+          'text-align': 'center'
+        }
+      }
+
+      h(:table, props, [
+        *render_title,
+        *render_corporations,
+        render_player_cash,
+        render_player_privates,
+        render_player_worth,
+        render_player_certs,
+      ])
+
+      #TODO consider adding OR information (could do both company OR revenue and player change in value)
+      #TODO consider adding train availability
+
+    end
+
+    def render_title
+      [
+         h(:tr, [
+          h(:th, { style: { width: '20px' } }, ''),
+          h(:th, { attrs: { colspan: @game.players.length } }, 'Players'),
+          h(:th, { attrs: { colspan: 2 } }, 'Bank shares'),
+          h(:th, { attrs: { colspan: 2 } }, 'Prices'),
+          h(:th, { attrs: { colspan: 4 } }, 'Company'),
+          h(:th, { style: { width: '20px' } }, ''),
+        ]),
+        h(:tr, [
+          h(:th, { style: { width: '20px' } }, ''),
+          *@game.players.map { |p| h(:th, p.name) },
+          h(:th, 'IPO'),
+          h(:th, 'Market'),
+          h(:th, 'Par'),
+          h(:th, 'Current'),
+          h(:th, 'Cash'),
+          h(:th, 'Trains'),
+          h(:th, 'Tokens'),
+          h(:th, 'Privates'),
+          h(:th, { style: { width: '20px' } }, ''),
+        ])
+      ]
+    end
+
+    def render_corporations
+      @game.corporations.map { |c| render_corporation(c) }
+    end
+
+    def render_corporation(corporation)
+      corporation_color =
+        {
+          style: {
+            background: corporation.color,
+            color: '#ffffff'
+          }
+        }
+      props = { style: {} }
+      if not corporation.floated?
+        props[:style]['background-color'] = 'rgba(220,220,220,0.4)'
+      end
+      h(:tr, props, [
+        h(:td, corporation_color, corporation.name),
+        *@game.players.map { |p|
+          h(:td, p.num_shares_of(corporation).to_s + (corporation.president?(p) ? '*' : ''))
+        },
+        h(:td, corporation.num_shares_of(corporation)),
+        h(:td, @game.share_pool.num_shares_of(corporation)),
+        h(:td, corporation.par_price ? corporation.par_price.price : ''),
+        h(:td, corporation.share_price ? corporation.share_price.price : ''),
+        h(:td, corporation.cash),
+        h(:td, corporation.trains.map { |t| t.name }),
+        h(:td, "#{corporation.tokens.map { |t| t.used? ? 0 : 1 }.sum}/#{corporation.tokens.length}"),
+        render_companies(corporation),
+        h(:td, corporation_color, corporation.name),
+      ])
+    end
+
+    def render_companies(entity)
+      h(:td, entity.companies.map { |c| company_short_name(c.name) }.join(','))
+    end
+
+    #TODO we need a better way of referring to companies...numerical index?
+    #     we could just make an ad hoc index and show it on the page
+    def company_short_name(name)
+      name.gsub('-', ' ').split(' ').map { |w| w[0] }.join
+    end
+
+    def render_player_privates
+      h(:tr, [
+        h(:td, 'Privates'),
+        *@game.players.map { |p| render_companies(p) }
+      ])
+    end
+
+    def render_player_cash
+      h(:tr, [
+        h(:td, 'Cash'),
+        *@game.players.map { |p| h(:td, p.cash) }
+      ])
+    end
+
+    def render_player_worth
+      h(:tr, [
+        h(:td, 'Worth'),
+        *@game.players.map { |p| h(:td, p.value) }
+      ])
+    end
+
+    def render_player_certs
+      h(:tr, [
+        h(:td, 'Certs'),
+        *@game.players.map { |p| h(:td, p.num_certs) }
+      ])
+    end
+  end
+end
