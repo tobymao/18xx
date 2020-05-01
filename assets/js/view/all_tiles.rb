4252cc8cc2c6dc95ced8833b1de74bb2fecd88a8
Michael Brandt <michaelbrandt5@gmail.com>
2020-04-30 22:29:56 -0600

    /all_tiles page enhancements
    - show tile manifest for each game
    - show all map hexes for each game; when a particular "tile" is used by too many
      hexes, truncate its name
    - show all hexes with private blockers and corporation reservations
    - if a tile is listed in a game's manifest but is not defined in Engine::Tile,
      render a blank tile with a "TODO" label (e.g., tiles 53 and 61 for 1830)
    - a lot of terrain hexes are stil rendered repeatedly, e.g., the 120 cost
      mountain in 1830; this could be fixed by defining a 'mtn120' tile in
      Engine::Tile and updating the game definition to refer to that name instead of
      a tile code
    
    update map hex view to show all hexes used in a game's map
    
    when a single "tile" is used by many hexes, truncate the name to fit in the box;
    would be nice to have some way to see the full list, punting on that for now
    
    Screenshots of sections for 18Chesapeake and 1889 - https://imgur.com/a/oNb10kv

--- assets/js/view/all_tiles.rb
+++ assets/js/view/all_tiles.rb
@@ -2,6 +2,7 @@
 
 require_tree 'engine'
 
+require 'view/tile_manifest'
 require 'view/tiles'
 
 module View
@@ -17,7 +18,7 @@ module View
     def render
       h('div#tiles', [
           h('div#all_tiles', [
-              h(:h1, 'Track Tiles and Generic Map Hexes'),
+              h(:h1, 'Generic Map Hexes and All Track Tiles'),
               *TILE_IDS.map { |t| render_tile_block(t) }
             ]),
 
@@ -32,42 +33,103 @@ module View
       companies = game_class::COMPANIES.map { |c| Engine::Company.new(**c) }
       corporations = game_class::CORPORATIONS.map { |c| Engine::Corporation.new(**c) }
 
-      rendered_tiles = game_hexes.map do |color, hexes|
+      map_tiles = game_hexes.map do |color, hexes|
         hexes.map do |coords, tile_string|
           coords.map.with_index do |coord, _index|
-            next if TILE_IDS.include?(tile_string)
-
-            tile = Engine::Tile.from_code(
-              coord,
-              color,
-              tile_string,
-              location_name: location_names[coord]
-            )
+            tile =
+              if TILE_IDS.include?(tile_string)
+                Engine::Tile.for(tile_string)
+              else
+                Engine::Tile.from_code(
+                  coord,
+                  color,
+                  tile_string,
+                  location_name: location_names[coord]
+                )
+              end
 
             # add private companies that block tile lays on this hex
             blocker = companies.find do |c|
               c.abilities(:blocks_hex)&.dig(:hex) == coord
             end
-            tile.add_blocker!(blocker) unless blocker.nil?
+            unless blocker.nil?
+              tile.add_blocker!(blocker)
+              # name it with the coord to distinguish this tile from its
+              # standard archetype that doesn't have a blocker
+              tile.name = coord
+            end
 
             # reserve corporation home spots
             corporations.select { |c| c.coordinates == coord }.each do |c|
               tile.cities.first.add_reservation!(c.name)
+
+              # name it with the coord to distinguish this tile from its
+              # standard archetype that doesn't have a blocker
+              tile.name = coord
             end
 
-            render_tile_block(
-              tile.name,
-              tile: tile,
-              location_name: tile.location_name
-            )
+            [coord, tile]
           end.compact
         end
-      end.flatten
+      end.flatten.each_slice(2).to_a.to_h
 
-      h("div#map_hexes_#{game_class.title}", [
-        h(:h1, "#{game_class.title} Map Hexes"),
-        *rendered_tiles,
-      ])
+      # get mapping of tile -> all coordinates using that tile (for starting map
+      # hex "tiles")
+      tile_to_coords = {}
+      map_tiles.each do |coord, tile|
+        tile_key = tile_to_coords.keys.find { |k| k&.name == tile.name }
+        if tile_key.nil?
+          tile_to_coords[tile] = [coord]
+        else
+          tile_to_coords[tile_key] << coord
+        end
+      end
+
+      # truncate "names" (list of hexes with this tile)
+      map_hexes = tile_to_coords.map do |tile, coords|
+        name = coords.join(',')
+        if name.size > 13
+          name = "#{name.slice(0,10)}..."
+        end
+        tile.name = name
+        tile
+      end
+
+      rendered_map_hexes = map_hexes.sort.map do |tile|
+        render_tile_block(
+          tile.name,
+          tile: tile,
+          location_name: tile.location_name
+        )
+      end
+
+      tiles =  game_class::TILES.flat_map do |name, num|
+        num.times.map do |index|
+          begin
+            Engine::Tile.for(name, index: index)
+          rescue Engine::GameError
+            # use "TODO" tiles for when a game has a tile in its TILES that is
+            # not yet defined in Engine::TILES
+            Engine::Tile.from_code(name, 'white', 'l=TODO', index: index)
+          end
+        end
+      end
+
+      rendered_tiles = tiles.sort.group_by(&:name).map do |name, tiles|
+        render_tile_block(name, tile: tiles.first, num: tiles.size)
+      end
+
+      h("div#hexes_and_tiles_#{game_class.title}", [
+          h(:h1, "#{game_class.title}"),
+          h("div#map_hexes_#{game_class.title}", [
+              h(:h2, "#{game_class.title} Map Hexes"),
+              *rendered_map_hexes,
+            ]),
+          h("div#game_tiles_#{game_class.title}", [
+              h(:h2, "#{game_class.title} Tile Manifest"),
+              *rendered_tiles,
+            ])
+        ])
     end
   end
 end
