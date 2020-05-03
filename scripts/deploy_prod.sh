a2de75fe6706b9964fa6c7327e8760bba9a567ae
Michael Brandt <michaelbrandt5@gmail.com>
2020-05-03 13:44:33 -0600

    add `set -e` to deploy_prod.sh to stop and exit with failure when error encountered

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 docker-compose run rack rake precompile
 scp public/assets/main.js public/assets/main.js.gz deploy@18xx:~/18xx/public/assets/
 ssh -l deploy 18xx <<EOF
 cd ~/18xx/
+set -e
 git pull
 make prod_up_b_d
 EOF
