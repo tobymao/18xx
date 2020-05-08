b23dd13b83b940d847c5a713fd24caa36db06644
Dan Schmidt <dfan@dfan.org>
2020-05-07 21:16:23 -0400

    Update order of remaining corps when force-selling (fixes #315)

--- lib/engine/round/operating.rb
+++ lib/engine/round/operating.rb
@@ -411,6 +411,7 @@ module Engine
 
       def sell_shares(shares)
         sell_and_change_price(shares, @share_pool, @stock_market)
+        recalculate_order
       end
 
       def buy_company(company, price)
@@ -504,6 +505,13 @@ module Engine
         end
 
         @log << "-- Event: #{rusted_trains.uniq.join(', ')} trains rust --" if rusted_trains.any?
+      end
+
+      def recalculate_order
+        # Selling shares may have caused the corporations that haven't operated yet
+        # to change order. Re-sort only them.
+        index = @entities.find_index(@current_entity) + 1
+        @entities[index..-1] = @entities[index..-1].sort! if index < @entities.size - 1
       end
     end
   end
