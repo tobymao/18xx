9d32dbfec0cad8ff1b328b248493fafa36cf0486
Toby Mao <toby.mao@gmail.com>
2020-05-07 21:25:46 -0700

    new integration tests

--- spec/lib/engine/games/g_1889_spec.rb
+++ spec/lib/engine/games/g_1889_spec.rb
@@ -24,17 +24,44 @@ module Engine
 
     context 'full game' do
       RESULTS = {
+        68 => {
+          'Kruizey' => 326,
+          'RobbieT' => 180,
+          'bugscheese' => 723,
+          'takeoutweight' => 702,
+        },
+        164 => {
+          'Anvil' => 7258,
+          'Gamergeek65' => 6885,
+        },
         233 => {
           'dionhut' => 7844,
           'hhlodesign' => 7655,
           'raj' => 8050,
+        },
+        247 => {
+          'fdinh' => 1094,
+          'gugvib' => 1148,
+          'marco4884' => 1089,
+          'vecchioleone' => 305,
+        },
+        314 => {
+          'Rebus' => 1134,
+          'johnhawkhaines' => 260,
+          'scottredracecar' => 1473,
+        },
+        319 => {
+          'Avemo3' => 6586,
+          'Hushed' => 3511,
+          'Skanadron' => 5351,
         },
       }.freeze
 
       RESULTS.each do |game_id, result|
         it "#{game_id} matches result exactly" do
           data = JSON.parse(File.read("spec/fixtures/1889/#{game_id}.json"))
-          expect(subject.class.new(data['players'], actions: data['actions']).result).to eq(result)
+          players = data['players'].map { |p| p['name'] }
+          expect(subject.class.new(players, actions: data['actions']).result).to eq(result)
         end
       end
     end
