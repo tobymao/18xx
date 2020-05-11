16a2d0c1fd3f983c3dd2648c97b6d12ee7cda963
Michael Brandt <michaelbrandt5@gmail.com>
2020-05-11 12:55:11 -0600

    tile page: render multiple hexes/tiles w/ +

--- assets/app/view/tiles_page.rb
+++ assets/app/view/tiles_page.rb
@@ -23,44 +23,47 @@ module View
     def render
       dest = @route.match(ROUTE_FORMAT)[1]
 
-      err_msg = "Bad tile dest: \"#{dest}\"; should be \"all\", <game_title>, "\
-                '<tile_name>, <game_title>/<hex_coord>, or '\
-                '<game_title>/<tile_name>'
+      begin
+        # all common hexes/tiles
+        if dest == 'all'
+          h('div#tiles', [
+              h('div#all_tiles', [
+                  h(:h1, 'Generic Map Hexes and Common Track Tiles'),
+                  *TILE_IDS.map { |t| render_tile_block(t) }
+                ]),
 
-      # all common hexes/tiles
-      if dest == 'all'
-        h('div#tiles', [
-            h('div#all_tiles', [
-                h(:h1, 'Generic Map Hexes and Common Track Tiles'),
-                *TILE_IDS.map { |t| render_tile_block(t) }
-              ]),
+            ])
 
-          ])
+        # everything for one game
+        elsif Engine::GAMES_BY_TITLE.keys.include?(dest)
+          game_class = Engine::GAMES_BY_TITLE[dest]
+          h('div#tiles', [
+              map_hexes_and_tile_manifest_for(game_class)
+            ])
 
-      # everything for one game
-      elsif Engine::GAMES_BY_TITLE.keys.include?(dest)
-        game_class = Engine::GAMES_BY_TITLE[dest]
-        h('div#tiles', [
-            map_hexes_and_tile_manifest_for(game_class)
-          ])
+        # hexes/tiles from a specific game
+        elsif dest.include?('/')
+          game_title, dest = dest.split('/')
+          hex_or_tile_ids = dest.split('+')
+          rendered = hex_or_tile_ids.map { |id| render_individual_tile(game_title, id) }
+          h('div#tiles', rendered)
 
-      # just one tile
-      elsif TILE_IDS.include?(dest)
-        render_tile_block(dest, scale: 3.0)
-
-      # just one hex/tile from a specific game
-      elsif dest.include?('/')
-        game_title, dest = dest.split('/')
-        begin
-          render_individual_tile(game_title, dest)
-        rescue StandardError
-          h(:p, err_msg)
+        # common tile(s)
+        else
+          tile_ids = dest.split('+')
+          rendered = tile_ids.map { |id| render_tile_block(id, scale: 3.0) }
+          h('div#tiles', rendered)
         end
+      rescue StandardError => e
+        err_msg = "Bad tile dest: \"#{dest}\"; should be \"all\", <game_title>, "\
+                  '<tile_name>, <game_title>/<hex_coord>, or '\
+                  '<game_title>/<tile_name>. Multiple tile_names can be given, '\
+                  'separated by "+"'
 
-      # no match from prior conditionals, something must be wrong with the given
-      # route
-      else
-        h(:p, err_msg)
+        h(:div, [
+            h(:p, err_msg),
+            h(:p, "#{e.class.name}: #{e.message}"),
+          ])
       end
     end
 
