484446908f7d87f43308aba78cbdc149cd514e58
Jen Freeman <jen.a.freeman@googlemail.com>
2020-05-15 08:59:02 +0100

    Add required metadata to allow pin versions to keep their state correct

--- assets/app/view/actionable.rb
+++ assets/app/view/actionable.rb
@@ -21,17 +21,27 @@ module View
       @game_data[:actions] << action.to_h
       store(:game_data, @game_data, skip: true)
 
+      if @game.finished
+        @game_data[:result] = @game.result
+        @game_data[:status] = 'finished'
+      else
+        @game_data[:result] = {}
+        @game_data[:status] = 'active'
+      end
+
       if @game_data[:mode] == :hotseat
-        if @game.finished
-          @game_data[:result] = @game.result
-          @game_data[:status] = 'finished'
-        else
-          @game_data[:result] = {}
-          @game_data[:status] = 'active'
-        end
         Lib::Storage[@game_data[:id]] = @game_data
       else
-        @connection.safe_post("/game/#{@game_data['id']}/action", action.to_h)
+        json=action.to_h
+        meta={
+          'game_result': @game_data[:result],
+          'game_status': @game_data[:status],
+          'active_players': game.active_players.map(&:name),
+          'turn': game.turn,
+          'round': game.round.name
+          }
+        json['meta']=meta
+        @connection.safe_post("/game/#{@game_data['id']}/action", json)
       end
 
       store(:game, game)
