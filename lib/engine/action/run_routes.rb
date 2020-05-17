e59b2a5a7d502c9e1691babb11fcc799ce594e51
Toby Mao <toby.mao@gmail.com>
2020-05-17 12:16:55 -0700

    don't save hexes and revenue bad merge

--- lib/engine/action/run_routes.rb
+++ lib/engine/action/run_routes.rb
@@ -45,8 +45,6 @@ module Engine
         routes = @routes.map do |route|
           {
             'train' => route.train.id,
-            'hexes' => route.hexes.map(&:id),
-            'revenue' => route.revenue,
             'connections' => route.connections.map(&:id),
           }
         end
