19d8b2838f7d875db3c7bfa725be265e384506da
Toby Mao <toby.mao@gmail.com>
2020-05-20 08:36:29 -0700

    don't show finished games in your games section

--- models/game.rb
+++ models/game.rb
@@ -50,6 +50,7 @@ class Game < Base
       FROM games g
       JOIN user_games ug
         ON g.id = ug.id
+      WHERE g.status != 'finished'
       ORDER BY g.created_at DESC
       LIMIT #{QUERY_LIMIT}
       OFFSET :personal_offset * #{QUERY_LIMIT - 1}
@@ -64,7 +65,7 @@ class Game < Base
     UNION
     #{USER_STATUS_QUERY % { status: 'active' }}
     UNION
-    #{USER_STATUS_QUERY % { status: 'finished' }}
+    #{STATUS_QUERY % { status: 'finished' }}
   SQL
 
   LOGGED_OUT_QUERY = <<~SQL
