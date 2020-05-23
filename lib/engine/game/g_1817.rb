19f60e619002fd98f54955c92849f2924818f098
Christopher Giroir <kelsin@valefor.com>
2020-05-22 22:55:18 -0700

    feat: color definitions for other 3 games

--- lib/engine/game/g_1817.rb
+++ lib/engine/game/g_1817.rb
@@ -6,6 +6,27 @@ require_relative 'base'
 module Engine
   module Game
     class G1817 < Base
+      register_colors(black: '#0a0a0a',
+                      blue: '#0a70b3',
+                      brightGreen: '#7bb137',
+                      brown: '#881a1e',
+                      gold: '#e09001',
+                      gray: '#9a9a9d',
+                      green: '#008f4f',
+                      lavender: '#baa4cb',
+                      lightBlue: '#37b2e2',
+                      lightBrown: '#b58168',
+                      lime: '#bdbd00',
+                      navy: '#004d95',
+                      natural: '#fbf4de',
+                      orange: '#eb6f0e',
+                      pink: '#ec767c',
+                      red: '#dd0030',
+                      turquoise: '#235758',
+                      violet: '#4d2674',
+                      white: '#ffffff',
+                      yellow: '#fcea18')
+
       load_from_json(Config::Game::G1817::JSON)
     end
   end
