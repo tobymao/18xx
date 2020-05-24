41138367e05cd291b508e5847ba69145d9671c53
Toby Mao <toby.mao@gmail.com>
2020-05-23 20:21:19 -0700

    don't return diesels if there's no bonus route fixes #488

--- lib/engine/part/revenue_center.rb
+++ lib/engine/part/revenue_center.rb
@@ -18,7 +18,7 @@ module Engine
       end
 
       def route_revenue(phase, train)
-        return @revenue[:diesel] if train.name.upcase == 'D'
+        return @revenue[:diesel] if train.name.upcase == 'D' && @revenue[:diesel]
 
         phase.tiles.reverse.each { |color| return @revenue[color] if @revenue[color] }
       end
