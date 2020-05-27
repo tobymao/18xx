c45150efb2f7dea9ba8d283b2e3993aa9598f13a
Toby Mao <toby.mao@gmail.com>
2020-05-27 11:16:34 -0700

    fix chessie emails

--- assets/app/mail/turn.rb
+++ assets/app/mail/turn.rb
@@ -8,8 +8,9 @@ class Turn < Snabberb::Component
   needs :game_url
 
   def render
-    @game = Engine::Game::G1889.new(
+    @game = Engine::GAMES_BY_TITLE[@game_data['title']].new(
       @game_data['players'].map { |p| p['name'] },
+      id: @game_data['id'],
       actions: @game_data['actions'],
     )
 
