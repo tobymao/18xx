077ffdaaab6f42c16b390fe534386f852d1afd10
Toby Mao <toby.mao@gmail.com>
2020-05-29 23:05:23 -0700

    remove offset for personal games fixes #356

--- assets/app/view/home.rb
+++ assets/app/view/home.rb
@@ -23,11 +23,9 @@ module View
       end
       grouped = other_games.group_by { |game| game['status'] }
 
-      last = your_games.last
       # Ready, then active, then unstarted, then completed
       your_games.sort_by! do |game|
         [
-          game == last && your_games.size > GameRow::LIMIT ? 1 : 0,
           user_is_acting?(@user, game) ? -game['id'] : 0,
           game['status'] == 'active' ? -game['id'] : 0,
           game['status'] == 'new' ? -game['id'] : 0,
