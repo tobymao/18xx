f45e16a9f3dc48127b6080916c7ec9764c1ce0f6
yzemaze <git@yzemaze.de>
2020-06-03 22:46:13 +0200

    COLOR moved and inlined

--- assets/app/view/user.rb
+++ assets/app/view/user.rb
@@ -109,7 +109,7 @@ module View
         ),
       ]
 
-      color_squares = Lib::Theme::COLOR.map do |color, _value|
+      color_squares = ['white', 'yellow', 'green', 'brown', 'gray', 'red', 'blue'].map do |color, _value|
         h("div.color-square.#{color}", style: { 'background': Lib::Theme.const_get(theme)[color] || 'transparent' })
       end
 
