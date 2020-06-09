c19bc3eec4846018c0ed33c45910e99baafaea0e
Scott Petersen <scott@Scotts-MacBook-Air.local>
2020-06-09 14:57:24 -0500

    remove special power code

--- lib/engine/game/g_18_newengland.rb
+++ lib/engine/game/g_18_newengland.rb
@@ -16,33 +16,6 @@ module Engine
 
       load_from_json(Config::Game::G18NewEngland::JSON)
 
-      def check_special_tile_lay(action, company)
-        return if company.closed? || action.entity == company
-        return unless (ability = company.abilities(:tile_lay))
-
-        hexes = ability[:hexes]
-        return unless hexes.include?(action.hex.id)
-
-        company.remove_ability(:tile_lay)
-        @log << "#{company.name} loses the ability to lay #{hexes}"
-      end
-
-      def columbia
-        @companies.find { |company| company.name == 'Columbia - Philadelphia Railroad' }
-      end
-
-      def baltimore
-        @companies.find { |company| company.name == 'Baltimore and Susquehanna Railroad' }
-      end
-
-      def cornelius
-        @cornelius ||= @companies.find { |company| company.name == 'Cornelius Vanderbilt' }
-      end
-
-      def or_set_finished
-        depot.export! if %w[2 3 4].include?(@depot.upcoming.first.name)
-      end
-
       def stock_round
         Round::Stock.new(@players, game: self, sell_buy_order: :sell_buy)
       end
