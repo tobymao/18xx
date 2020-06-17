3ce344f828047fa029c0498f27e7cdca7e0d643a
Toby Mao <toby.mao@gmail.com>
2020-06-17 15:51:12 -0700

    use same id

--- lib/engine/game/g_1846.rb
+++ lib/engine/game/g_1846.rb
@@ -50,8 +50,14 @@ module Engine
 
       def init_companies(players)
         super + @players.size.times.map do |i|
-          name = "Pass (#{i + 1})"
-          Company.new(sym: name, name: name, value: 0, desc: "Choose this card if you don't want to purchase any of the offered companies this round")
+          id = i + 1
+
+          Company.new(
+            sym: id,
+            name: "Pass (#{id})",
+            value: 0,
+            desc: "Choose this card if you don't want to purchase any of the offered companies this round",
+          )
         end
       end
 
