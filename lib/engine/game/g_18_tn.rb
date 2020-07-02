5571ebb8e84b5b8dec8065ed3d46f59e199a5e1a
scottredracecar <scott@redracecar.com>
2020-07-01 22:08:05 -0500

    add mark derrick games and logos (#960)
    * add games and logos
    
    * fix logos
    
    Co-authored-by: scottredracecar <scott@Scotts-MacBook-Air.local>

---
+++ lib/engine/game/g_18_tn.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../config/game/g_18_tn'
+require_relative 'base'
+
+module Engine
+  module Game
+    class G18TN < Base
+      load_from_json(Config::Game::G18TN::JSON)
+
+      GAME_LOCATION = 'Tennessee, USA'
+      GAME_RULES_URL = 'http://dl.deepthoughtgames.com/18TN-Rules.pdf'
+      GAME_DESIGNER = 'Mark Derrick'
+    end
+  end
+end
