8045038d4c802e7ec0ca9b461ea06492bf86e088
Jen Freeman <jen.a.freeman@googlemail.com>
2020-07-02 08:48:17 +0100

    Tidy up player order

--- lib/engine/round/g_1846/draft.rb
+++ lib/engine/round/g_1846/draft.rb
@@ -6,17 +6,12 @@ module Engine
   module Round
     module G1846
       class Draft < Auction
-        def initialize(game, steps)
-          super
-          @index = @entities.size - 1
-        end
-
         def name
           'Draft Round'
         end
 
-        def next_index!
-          @index = (@index - 1) % @entities.size
+        def select_entities
+          @game.players.reverse
         end
       end
     end
