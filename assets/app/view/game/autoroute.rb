9aca853c613e15c32219a10cb2740f1984afd3a8
Nick Heiner <nheiner@netflix.com>
2020-07-04 13:02:19 -0700

    Adding a second test

--- assets/app/view/game/autoroute.rb
+++ assets/app/view/game/autoroute.rb
@@ -3,7 +3,11 @@ module View
     class Autoroute
       def self.calculate(game, corporationId)
         corp = game.corporation_by_id(corporationId)
-        corp.trains
+        trains = corp.trains
+
+        if trains.empty?
+          return []
+        end
       end
     end
   end
--- spec/assets/app/view/game/autoroute_spec.rb
+++ spec/assets/app/view/game/autoroute_spec.rb
@@ -6,8 +6,18 @@ module View
   module Game
     describe '#calculate' do
       let(:game) { Engine::GAMES_BY_TITLE['1889'].new(%w[player1 player2]) }
+      before :each do
+        game.corporations.each do |corp|
+          corp.trains.clear
+        end
+      end
 
       it 'No tiles and no trains' do
+        expect(Autoroute.calculate(game, 'IR')).to eq([])
+      end
+
+      it '1 train and no tiles' do
+        game.corporation_by_id('IR').trains.push(game.train_by_id('2-1'))
         expect(Autoroute.calculate(game, 'IR')).to eq([])
       end
     end
