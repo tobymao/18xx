84459381ed9af02a6310a30add8bc1ef04b212ff
Nick Heiner <nheiner@netflix.com>
2020-07-04 13:23:00 -0700

    Adding another test, but it does not pass

--- spec/assets/app/view/game/autoroute_spec.rb
+++ spec/assets/app/view/game/autoroute_spec.rb
@@ -1,6 +1,7 @@
 require './spec/spec_helper'
 require 'view/game/autoroute'
 require 'engine'
+require 'engine/route'
 
 module View
   module Game
@@ -17,8 +18,20 @@ module View
       end
 
       it '1 train and no tiles' do
-        game.corporation_by_id('IR').trains.push(game.train_by_id('2-1'))
+        game.corporation_by_id('IR').trains << game.train_by_id('2-1')
         expect(Autoroute.calculate(game, 'IR')).to eq([])
+      end
+      
+      it '1 train and tile 46 at E2' do
+        train = game.train_by_id('2-1');
+        game.corporation_by_id('IR').trains << train
+        game.hex_by_id('E2').lay(game.tile_by_id('5-0').rotate!(4))
+
+        expected = Engine::Route.new(
+          game, game.phase, game.train_by_id(train), connection_hexes: [game.hex_by_id('E2').connections[0]]
+        )
+
+        expect(Autoroute.calculate(game, 'IR')).to eq([expected])
       end
     end
   end
