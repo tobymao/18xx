745828afc0c865900aedbeb0418fdac0d540427e
Jen Freeman <jen.a.freeman@googlemail.com>
2020-07-19 21:11:43 +0100

    Fix undo (by blocking on message), and also show the spreadsheet at game end

---
+++ assets/app/view/game/game_end.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'view/game/pass_button'
+require 'view/game/undo_button'
+require 'view/game/redo_button'
+
+module View
+  module Game
+    class GameEnd < Snabberb::Component
+      include Actionable
+
+      def render
+        h(:div,[
+          h(Game::UndoAndPass, pass: false),
+          h(Game::Map, game: @game, opacity: 1.0),
+          h(Game::Spreadsheet, game: @game),
+        ])
+      end
+    end
+  end
+end
