0c55aadba2de37b0ddce27ae40d53bd96e56e322
yzemaze <33390595+yzemaze@users.noreply.github.com>
2020-07-20 07:20:40 +0200

    Multi revenue with user defined bg colors (#1108)
    * user defined bg colors
    
    * prune bg_coloring
    
    * string => sym
    
    * require & include lib/settings

--- assets/app/view/game/game_info.rb
+++ assets/app/view/game/game_info.rb
@@ -49,12 +49,13 @@ module View
         current_phase = @game.phase.current
         rows = @game.phase.phases.map do |phase|
           phase_color = Array(phase[:tiles]).last
-          phase_props = { style: {} }
-          if Part::MultiRevenue::COLOR.include?(phase_color)
-            bg_color = color_for(phase_color)
-            phase_props[:style][:backgroundColor] = bg_color
-            phase_props[:style][:color] = contrast_on(bg_color)
-          end
+          bg_color = color_for(phase_color)
+          phase_props = {
+            style: {
+              backgroundColor: bg_color,
+              color: contrast_on(bg_color),
+            },
+          }
 
           event_text = []
           event_text << 'Can Buy Companies' if phase[:buy_companies]
