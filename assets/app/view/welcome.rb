9eb0da1c61f6e4ad9195412baa6da63128838230
Jen Freeman <jen.a.freeman@googlemail.com>
2020-07-26 18:20:22 +0100

    Update text to suggest 89 or chesapeake for new players, select 89 by default

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -55,8 +55,9 @@ module View
 
     def render_introduction
       message = <<~MESSAGE
-        <p>18xx.games is a website where you can play async or real-time 18xx games (based on the system originally devised by the brilliant Francis Tresham)! Right now only 1889 and 18Chesapeake are implemented
-        but I'm planning on doing many more in the future.</p>
+        <p>18xx.games is a website where you can play async or real-time 18xx games (based on the system originally devised by the brilliant Francis Tresham)!
+        <p>Right now, 1889 and 18Chesapeake are fully implemented, with 1836Jr30 being in beta but I'm planning on doing many more in the future.
+        If you are new to 18xx games then 1889 or 18Chesapeake are good games to begin with.</p>
 
         <p>You can play locally with hot seat mode without an account. If you want to play multiplayer, you'll need to create an account.</p>
 
