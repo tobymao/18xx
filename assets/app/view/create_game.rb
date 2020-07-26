a0ff379ab2077c1ca36e05a28b04462c409bede7
Jen Freeman <jen.a.freeman@googlemail.com>
2020-07-26 18:21:21 +0100

    Rubocop

--- assets/app/view/create_game.rb
+++ assets/app/view/create_game.rb
@@ -41,8 +41,8 @@ module View
 
       description = []
       description += [h(:a, { attrs: { href: '/signup' } }, 'Signup'), ' or ',
-                     h(:a, { attrs: { href: '/login' } }, 'login'), ' to play multiplayer.'] unless @user
-      description << h(:div, "If you are new to 18xx games then 1889 or 18Chesapeake are good games to begin with.")
+                      h(:a, { attrs: { href: '/login' } }, 'login'), ' to play multiplayer.'] unless @user
+      description << h(:div, 'If you are new to 18xx games then 1889 or 18Chesapeake are good games to begin with.')
       render_form('Create New Game', inputs, description)
     end
 
@@ -60,7 +60,7 @@ module View
 
         attrs[:selected] = 'selected' if game.title == '1889'
 
-        h(:option, { attrs:  attrs }, title)
+        h(:option, { attrs: attrs }, title)
       end
 
       limit_range = lambda do
