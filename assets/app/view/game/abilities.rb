458596dcff89a650fb68f6f181c3c8d161405390
yzemaze <git@yzemaze.de>
2020-07-27 09:11:52 +0200

    cleaner rendering on mobile name truncation revised => longer company names button positioning, paddings & margins

--- assets/app/view/game/abilities.rb
+++ assets/app/view/game/abilities.rb
@@ -8,16 +8,15 @@ module View
       needs :selected_company, default: nil, store: true
       needs :show_other_abilities, default: false, store: true
 
-      ABILITIES = %i[tile_lay teleport assign_hexes assign_corporation token exchange].freeze
-
       def render
         companies = @game.companies.select { |company| !company.closed? && actions_for(company).any? }
         return h(:div) if companies.empty? || @game.round.current_entity.company?
 
         current, others = companies.partition { |company| @game.current_entity.owner == company.owner.owner }
 
+        props = { style: { marginRight: '1rem' } }
         children = [
-          h('div.inline.margined.bold', 'Abilities:'),
+          h('h3.inline', props, 'Abilities:'),
           *render_companies(current),
         ]
 
@@ -29,16 +28,27 @@ module View
             store(:show_other_abilities, !@show_other_abilities)
           end
 
-          children << h('button.button', { on: { click: toggle_show } }, @show_other_abilities ? 'Hide' : 'Show')
+          props = {
+            style: { margin: '0.5rem 0.5rem 0 0' },
+            on: { click: toggle_show },
+          }
+          children << h('button.button', props, @show_other_abilities ? 'Hide' : 'Show Others')
           children.concat(render_companies(others)) if @show_other_abilities
         end
 
         if companies.include?(@selected_company)
-          children << h(:div, { style: { margin: '0.5rem 0' } }, @selected_company.desc)
+          props = {
+            style: {
+              maxWidth: '80rem',
+              margin: '0.3rem 0 0.5rem 0',
+            },
+          }
+          children << h(:div, props, @selected_company.desc)
           children.concat(render_actions)
         end
 
-        h(:div, children)
+        props = { style: { marginBottom: '0.5rem' } }
+        h(:div, props, children)
       end
 
       def render_companies(companies)
@@ -50,18 +60,26 @@ module View
             style: {
               cursor: 'pointer',
               display: 'inline-block',
-              padding: '0.5rem',
+              padding: '0.5rem 1rem 0 0',
             },
           }
           props[:style][:textDecoration] = 'underline' if @selected_company == company
 
           company_name = company.name
-          company_name = company_name[0..15] + '...' if company_name.size > 15
-
           owner_name = company.owner.id
-          owner_name = owner_name[0..10] + '...' if owner_name.size > 10
+          c_size = company_name.size
+          o_size = owner_name.size
+          limit = 36
+          if c_size + o_size > limit
+            if o_size < 5
+              company_name = company_name[0..(limit - 7)] + '…' if c_size > (limit - 3)
+            else
+              company_name = company_name[0..(limit - 17)] + '…' if c_size > (limit - 13)
+              owner_name = owner_name[0..9] + '…' if o_size > 13
+            end
+          end
 
-          h(:div, props, "#{company_name} (#{owner_name})")
+          h(:a, props, "#{company_name} (#{owner_name})")
         end.compact
       end
 
