8278ad8d42b984c55fc82c19160ab6a545322f43
Jen Freeman <jen.a.freeman@googlemail.com>
2020-07-31 18:03:31 +0100

    Allow custom tile rendering

--- assets/app/view/tiles_page.rb
+++ assets/app/view/tiles_page.rb
@@ -51,6 +51,17 @@ module View
           ])
 
       # hexes/tiles from a specific game
+      elsif dest == 'custom'
+        location_name = Lib::Params['n']
+        tile = Engine::Tile.from_code(
+          'custom',
+          'yellow',
+          hexes_or_tiles,
+          location_name: location_name
+        )
+        rendered = render_tile_blocks('custom', layout: layout, tile: tile)
+        h('div#tiles', rendered)
+
       elsif hexes_or_tiles
         game_title = dest
         hex_or_tile_ids = hexes_or_tiles.split('+')
