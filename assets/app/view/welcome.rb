84b4dc9f2106104da30496c14800aa84f2a2b486
tobymao <toby.mao@gmail.com>
2020-08-04 21:01:53 -0700

    welcoem message

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -15,6 +15,7 @@ module View
 
     def render_notification
       message = <<~MESSAGE
+        <p>The 2 player variant has now been implement for 18Chesapeake!.</p>
         <p>All microsoft email domains are blocking 18xx.games. msn, hotmail, live, etc. So you will not be receiving anything.</p>
         <p>1882 and 18AL are both available for alpha testing and 1846 is now in beta.</p>
         <p>Please file <a href='https://github.com/tobymao/18xx/issues'>issues and ideas</a> on
