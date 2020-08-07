53bc4b0df96c0d113b490b32bacfaa9a2f38577c
Michael Brandt <michaelbrandt5@gmail.com>
2020-08-06 23:38:43 -0600

    Fix order of log messages for corp in receivership
    put "Sharepool operates ___" first

--- lib/engine/round/operating.rb
+++ lib/engine/round/operating.rb
@@ -58,8 +58,8 @@ module Engine
         end
         entity.trains.each { |train| train.operated = false }
         @game.place_home_token(entity) if @home_token_timing == :operate
-        skip_steps
         @game.log << "#{entity.owner.name} operates #{entity.name}" unless finished?
+        skip_steps
         next_entity! if finished?
       end
 
