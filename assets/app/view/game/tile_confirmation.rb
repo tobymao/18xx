58cacfb19c9d241b8aeaedff66d72f656f6b44cc
yzemaze <33390595+yzemaze@users.noreply.github.com>
2020-08-12 22:06:12 +0200

    cancel/confirm buttons revised (#1432)

--- assets/app/view/game/tile_confirmation.rb
+++ assets/app/view/game/tile_confirmation.rb
@@ -1,44 +1,58 @@
 # frozen_string_literal: true
 
+require 'lib/settings'
 require 'view/game/actionable'
 
 module View
   module Game
     class TileConfirmation < Snabberb::Component
       include Actionable
+      include Lib::Settings
 
       needs :tile_selector, store: true
 
       def render
+        button_style = {
+          display: 'inline-block',
+          cursor: 'pointer',
+          fontSize: '35px',
+          color: '#FFFFFF',
+          filter: 'drop-shadow(3px 3px 2px #888)',
+          padding: '6px 2px 8px 2px',
+        }
+
         confirm = {
-          props: { innerHTML: '☑' },
+          props: { innerHTML: '✔' },
           style: {
-            position: 'absolute',
-            cursor: 'pointer',
-            left: '-26px',
-            top: '-50px',
-            color: 'black',
-            fontSize: '35px',
+            backgroundColor: default_for(:green),
+            **button_style,
           },
           on: { click: -> { lay_tile } },
         }
 
-        delete = {
-          props: { innerHTML: '☒' },
+        cancel = {
+          props: { innerHTML: '✖' },
           style: {
-            position: 'absolute',
-            cursor: 'pointer',
-            left: '6px',
-            top: '-50px',
-            color: 'black',
-            fontSize: '35px',
+            backgroundColor: default_for(:red),
+            **button_style,
           },
           on: { click: -> { store(:tile_selector, nil) } },
         }
 
-        h(:div, [
-          h(:div, confirm),
-          h(:div, delete),
+        div_props = {
+          style: {
+            display: 'grid',
+            gridAutoFlow: 'column',
+            gridGap: '5px',
+            position: 'absolute',
+            left: '-38px',
+            top: '-75px',
+          },
+        }
+
+        h(:div, div_props, [
+          h('button.no_margin', cancel),
+          h('button.no_margin', confirm),
         ])
       end
 
