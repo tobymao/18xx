f5083d7998048f1cb60a836bb3bf34d0623ce19f
Dan Schmidt <dfan@dfan.org>
2020-08-15 13:00:03 -0400

    Restore correct priority deal behavior (fixes #1207)

--- lib/engine/round/stock.rb
+++ lib/engine/round/stock.rb
@@ -21,7 +21,8 @@ module Engine
         start_entity
       end
 
-      def after_process(_action)
+      def after_process(action)
+        @last_to_act = action.entity unless action.pass?
         return if active_step
 
         next_entity!
