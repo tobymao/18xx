3d00ad4175f247d84770a68093672608ca9fd688
perwestling <per.o.westling@gmail.com>
2020-08-17 21:15:12 +0200

    18GA: Fix tile upgrades
    Ensure tile manifest show allowed upgrade from normal to
    labeled tiles. (4 cities - 2 from yellow, 2 from green)

--- lib/engine/game/g_18_ga.rb
+++ lib/engine/game/g_18_ga.rb
@@ -23,6 +23,11 @@ module Engine
 
       def setup
         setup_company_price_50_to_150_percent
+
+        @green_aug_tile ||= @tiles.find { |t| t.name == '453a' }
+        @green_s_tile ||= @tiles.find { |t| t.name == '454a' }
+        @brown_b_tile ||= @tiles.find { |t| t.name == '457a' }
+        @brown_m_tile ||= @tiles.find { |t| t.name == '458a' }
       end
 
       def operating_round(round_num)
@@ -31,13 +36,31 @@ module Engine
           Step::DiscardTrain,
           Step::BuyCompany,
           Step::HomeToken,
-          Step::Track,
+          Step::G18GA::Track,
           Step::Token,
           Step::Route,
           Step::Dividend,
           Step::SingleDepotTrainBuyBeforePhase4,
           [Step::BuyCompany, blocks: true],
         ], round_num: round_num)
+      end
+
+      YELLOW_CITY_TILES = %w[5 6 57].freeze
+      GREEN_CITY_TILES = %w[14 15].freeze
+
+      def all_potential_upgrades(tile)
+        upgrades = super
+        puts("Tile is #{tile.name} and its hex is #{tile.hex&.name}")
+
+        # Need only to add more potential tiles if tile manifest (non-matching labels)
+        return upgrades if tile.hex&.name != 'A1'
+
+        upgrades << @green_aug_tile if @green_aug_tile && YELLOW_CITY_TILES.include?(tile.name)
+        upgrades << @green_s_tile if @green_s_tile && YELLOW_CITY_TILES.include?(tile.name)
+        upgrades << @brown_b_tile if @brown_b_tile && GREEN_CITY_TILES.include?(tile.name)
+        upgrades << @brown_m_tile if @brown_m_tile && GREEN_CITY_TILES.include?(tile.name)
+
+        upgrades
       end
     end
   end
