5644ae4aff62285a374bbb06bba4027167d52c22
perwestling <per.o.westling@gmail.com>
2020-08-29 20:04:06 +0200

    Fixed review comment

--- lib/engine/ability/token.rb
+++ lib/engine/ability/token.rb
@@ -7,7 +7,7 @@ module Engine
     class Token < Base
       attr_reader :hexes, :price, :teleport_price, :extra, :from_owner
 
-      def setup(hexes:, price:, teleport_price: nil, extra: nil, from_owner: false)
+      def setup(hexes:, price:, teleport_price: nil, extra: nil, from_owner: nil)
         @hexes = hexes
         @price = price
         @teleport_price = teleport_price
