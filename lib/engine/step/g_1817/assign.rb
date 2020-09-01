ba39b953849374ca482673864e0341b82f555c77
Jen Freeman <jen.a.freeman@googlemail.com>
2020-09-01 22:01:58 +0100

    Rubocop

--- lib/engine/step/g_1817/assign.rb
+++ lib/engine/step/g_1817/assign.rb
@@ -9,7 +9,6 @@ module Engine
         def process_assign(action)
           company = action.entity
           target = action.target
-          hexes = company.abilities(:assign_hexes)&.hexes
 
           unless (ability = company.abilities(:assign_hexes))
             @game.game_error("Could not assign #{company.name} to #{target.name}; :assign_hexes ability not found")
@@ -17,12 +16,11 @@ module Engine
 
           case company.id
           when 'UBC', 'OBC'
-              assignable_hexes = ability.hexes.map { |h| @game.hex_by_id(h) }
-              id = 'bridge'
-              @game.game_error("Bridge already on #{target.name}") if target.assigned?(id)
-              target.assign!(id)
-              ability.use!
-              @log << "#{company.name} builds bridge on #{target.name}"
+            id = 'bridge'
+            @game.game_error("Bridge already on #{target.name}") if target.assigned?(id)
+            target.assign!(id)
+            ability.use!
+            @log << "#{company.name} builds bridge on #{target.name}"
           end
         end
       end
