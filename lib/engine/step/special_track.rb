3eb4e5a7ffd8810acfb5f310f92a1692cf046bc3
perwestling <per.o.westling@gmail.com>
2020-09-05 00:59:31 +0200

    Enforce that extra tile lays is actually part of consecutive tile lay
    Fixes #1583
    
    An extra tile lay (that is, tile_lay ability, with when='track)
    must be preceeded and/or followed by a tile lay action
    or be done during a Track or TrackAndToken step.
    
    Only exception is that Pass or BuyCompany is ignored for this check.
    Undo and Redo is also handled so that canceled actions are ignored
    for the check.
    
    Corrected two spec test games where the extra tile lay was done
    outside of the consecutive tile laying. The correction was to
    move the extra tile lay action so it became legal.

--- lib/engine/step/special_track.rb
+++ lib/engine/step/special_track.rb
@@ -29,6 +29,10 @@ module Engine
       end
 
       def process_lay_tile(action)
+        if ability(action.entity)&.when == 'track' && !within_consecutive_lay?
+          @game.game_error('Can only make extra tile lay as consecutive tile lay')
+        end
+
         lay_tile(action)
         check_connect(action)
         ability(action.entity).use!
@@ -77,6 +81,57 @@ module Engine
 
       def setup
         @company = nil
+      end
+
+      private
+
+      IGNORABLE_ACTIONS = %w[BuyCompany Pass].freeze
+
+      def within_consecutive_lay?
+        return true if @game.round.active_step.respond_to?(:process_lay_tile)
+
+        unresolved_undo_actions = 0
+        unresolved_redo_actions = 0
+        @game.actions.reverse_each do |a|
+          demodulized = demodulize(a)
+
+          if demodulized == 'Redo'
+            # Will skip previous undo
+            unresolved_redo_actions += 1
+            next
+          end
+
+          if demodulized == 'Undo'
+            # This can either be canceled by a later Redo
+            # or be a cancelation of a previous action
+            if unresolved_redo_actions.positive?
+              unresolved_redo_actions -= 1
+            else
+              unresolved_undo_actions += 1
+            end
+            next
+          end
+
+          if unresolved_undo_actions.positive?
+            # This action was undone - ignore it
+            unresolved_redo_actions -= 1
+            next
+          end
+
+          if IGNORABLE_ACTIONS.include?(demodulized)
+            # We can ignore this action as it does not really break
+            # the consecutiveness of tile lay
+            next
+          end
+
+          # Now we are down to the latest performed action
+          return demodulized == 'LayTile'
+        end
+        false # Should never get here really
+      end
+
+      def demodulize(action)
+        action.class.name.split('::').last
       end
     end
   end
