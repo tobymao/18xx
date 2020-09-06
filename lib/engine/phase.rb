bd0365a280df7e2d9c232b4f115f662ec32a8ff7
Doug Moore <lordscarlet@idledreams.net>
2020-09-06 00:53:56 -0400

    Use existing check for rusted trains (fixes #1575)

--- lib/engine/phase.rb
+++ lib/engine/phase.rb
@@ -92,17 +92,7 @@ module Engine
     def rust_trains!(train, entity)
       obsolete_trains = []
       rusted_trains = []
-      rusted_corporation_trains = {}
-
-      @game.corporations.each do |c|
-        c.trains.each do |t|
-          should_rust = t.rusts_on == train.sym || (t.obsolete_on == train.sym && @depot.discarded.include?(t))
-          next unless should_rust
-
-          rusted_corporation_trains[t.owner.name] = 0 if !rusted_corporation_trains.has_key?(t.owner.name)
-          rusted_corporation_trains[t.owner.name] +=1
-        end
-      end
+      owners = []
 
       @game.trains.each do |t|
         next if t.obsolete || t.obsolete_on != train.sym
@@ -113,17 +103,18 @@ module Engine
 
       @game.trains.each do |t|
         next if t.rusted
-        
+
         should_rust = t.rusts_on == train.sym || (t.obsolete_on == train.sym && @depot.discarded.include?(t))
         next unless should_rust
 
         rusted_trains << t.name
+        owners << t.owner.name
         entity.rusted_self = true if entity && entity == t.owner
         t.rust!
       end
 
       @log << "-- Event: #{obsolete_trains.uniq.join(', ')} trains are obsolete --" if obsolete_trains.any?
-      @log << "-- Event: #{rusted_trains.uniq.join(', ')} trains rust (#{rusted_corporation_trains.map{|c,t| "#{c} x#{t}"}.join(', ')}) --" if rusted_trains.any?       
+      @log << "-- Event: #{rusted_trains.uniq.join(', ')} trains rust (#{owners.group_by(&:itself).map{|c,t| "#{c} x#{t.count}"}.join(', ')}) --" if rusted_trains.any?       
     end
 
     def next!
