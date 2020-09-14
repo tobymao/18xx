1669a0678a0ba7146ba5db121cbe951f50b6278b
Michael Brandt <michaelbrandt5@gmail.com>
2020-09-13 20:16:40 -0600

    1846 - better hiding of stuff during draft (#1656)
    * 1846 - better hiding of stuff during draft
    
    - Add new method `hiding?` to return true if `@block_show` is true; this fixes
      hiding breaking when using the history feature [Fixes #1585]
    
    - Add attr `pre_process_pass` to steps so that active step can have some
      behavior set; other buttons created within the Auction view update
      `store(:hidden)` before they carry on with any action processing, but the Pass
      button is created outside of that, so the Pass button cannot update anything
      in the Auction view; the Auction view sets a lambda that updates the store and
      then cleans itself up. This fixes the issue where in 1846, if the draft is
      down to one company, a player can click "Show Player" and then pass, and then
      they are shown the info about the next player. [Fixes #1474]
    
    * move before_process_pass stuff out of engine, only into views

--- assets/app/view/game/undo_and_pass.rb
+++ assets/app/view/game/undo_and_pass.rb
@@ -9,13 +9,15 @@ module View
     class UndoAndPass < Snabberb::Component
       include Actionable
 
+      needs :before_process_pass, store: true
+
       def render
         children = []
         entity = @game.round.current_entity
         current_actions = @game.round.actions_for(entity)
         children << h(UndoButton) if @game.undo_possible
         children << h(RedoButton) if @game.redo_possible
-        children << h(PassButton) if current_actions.include?('pass')
+        children << h(PassButton, before_process_pass: @before_process_pass) if current_actions.include?('pass')
         h(:div, children)
       end
     end
