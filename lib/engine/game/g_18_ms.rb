846f237cb4e07fea544bdb1447353b51b62e3d10
perwestling <per.o.westling@gmail.com>
2020-09-16 13:12:38 +0200

    18MS: Set to alpha
    Updated welcome page to include 18MS.
    Sorted the mentioned 18xx in alphabetical order.

--- lib/engine/game/g_18_ms.rb
+++ lib/engine/game/g_18_ms.rb
@@ -11,6 +11,8 @@ module Engine
 
       load_from_json(Config::Game::G18MS::JSON)
 
+      DEV_STAGE = :alpha
+
       GAME_LOCATION = 'Mississippi, USA'
       GAME_RULES_URL = 'https://boardgamegeek.com/thread/2461999/article/35755723#35755723'
       GAME_DESIGNER = 'Mark Derrick'
