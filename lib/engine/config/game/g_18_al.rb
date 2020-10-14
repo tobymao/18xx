f9354f97f004e360e5c30833b0854889653972b2
Robert Ryan Driskel <rdriskel@dealerinspire.com>
2020-10-14 13:16:16 -0600

    Update 18AL and 18Mex to use multiplier attribute

--- lib/engine/config/game/g_18_al.rb
+++ lib/engine/config/game/g_18_al.rb
@@ -554,7 +554,8 @@ module Engine
                   "offboard"
                ],
                "pay":4,
-               "visit":4
+               "visit":4,
+               "multiplier":2
             },
             {
                "nodes":[
