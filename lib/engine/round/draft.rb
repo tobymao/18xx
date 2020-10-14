9c11f8952bf0da157168fe0e95a141d9cc25b584
Michael Brandt <michaelbrandt5@gmail.com>
2020-10-13 21:20:54 -0600

    18 Los Angeles - snake order for draft, priority order for stock (#1842)
    - priority order: by cash, set at beginning of stock round
    
    - snake order for draft
    
    also a couple of small fixes:
        - allowed to pass on first turn of draft
        - log removed private companies

--- lib/engine/round/draft.rb
+++ lib/engine/round/draft.rb
@@ -5,17 +5,37 @@ require_relative 'base'
 module Engine
   module Round
     class Draft < Base
+      def initialize(game, steps, **opts)
+        @reverse_order = opts[:reverse_order] || false
+        @snake_order = opts[:snake_order] || false
+        @snaking_up = true
+
+        super
+      end
+
       def name
         'Draft Round'
       end
 
       def select_entities
-        @game.players.reverse
+        @reverse_order ? @game.players.reverse : @game.players
       end
 
       def setup
         @steps.each(&:unpass!)
         @steps.each(&:setup)
+      end
+
+      def next_entity_index!
+        return super unless @snake_order
+
+        if (@snaking_up && @entity_index == (@entities.size - 1)) ||
+           (!@snaking_up && @entity_index.zero?)
+          @snaking_up = !@snaking_up
+        else
+          plus_or_minus = @snaking_up ? :+ : :-
+          @entity_index = @entity_index.send(plus_or_minus, 1) % @entities.size
+        end
       end
     end
   end
--- lib/engine/step/g_18_los_angeles/draft_distribution.rb
+++ lib/engine/step/g_18_los_angeles/draft_distribution.rb
@@ -6,11 +6,21 @@ module Engine
   module Step
     module G18LosAngeles
       class DraftDistribution < G1846::Draft2pDistribution
-        def process_pass(action)
-          return super unless only_one_company?
+        def actions(entity)
+          return [] if finished?
 
+          entity == current_entity ? ACTIONS_WITH_PASS : []
+        end
+
+        def process_bid(_action)
+          entities.each(&:unpass!)
+          super
+        end
+
+        def process_pass(action)
           @log << "#{action.entity.name} passes"
           @round.next_entity_index!
+          @round.next_entity_index! if current_entity == action.entity
           action.entity.pass!
         end
       end
