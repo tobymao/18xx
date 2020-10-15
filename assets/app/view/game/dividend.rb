b8359d34847ffb180b64b10703432b33558c8cd1
Robert Ryan Driskel <rdriskel@dealerinspire.com>
2020-10-15 15:15:19 -0600

    Cleaner view string generation

--- assets/app/view/game/dividend.rb
+++ assets/app/view/game/dividend.rb
@@ -36,14 +36,14 @@ module View
             cleanup
           end
           button = h('td.no_padding', [h(:button, { style: { margin: '0.2rem 0' }, on: { click: click } }, text)])
-          direction = ''
-          if option[:share_direction]
-            Array(option[:share_direction]).each_with_index do |dir, i|
-              direction += "#{option[:share_times][i]} #{dir} "
+          direction =
+            if option[:share_direction]
+              Array(option[:share_direction]).map.with_index {
+                |dir, i| "#{option[:share_times][i]} #{dir}"
+              }.join(', ')
+            else
+              'None'
             end
-          else
-            direction = 'None'
-          end
 
           props = { style: { paddingRight: '1rem' } }
           h(:tr, [
