647e3f67d4c8cda776d72dff4d47b6ce40146c68
Jennifer Freeman <jenf@users.noreply.github.com>
2020-10-23 22:45:17 +0100

    Sort corporations in stock rounds. (#1994)
    For 1817 the amount of corporations gets confusing
    
    Sort those that have been IPOed by their running order
    Then others leave in symbol order
    
    Done in base class to game so that other games (18CO and 1849 etc) can override.
    
    This does have the slight impact to the UI that the position can change dynamically,
    but this makes it clearer the change in stock price.

--- assets/app/view/game/round/stock.rb
+++ assets/app/view/game/round/stock.rb
@@ -57,7 +57,7 @@ module View
             },
           }
 
-          @game.corporations.map do |corporation|
+          @game.sorted_corporations.map do |corporation|
             next if @auctioning_corporation && @auctioning_corporation != corporation
 
             children = []
