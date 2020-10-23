7bba8580c4c9fdccb7873dbc4b25ca9a2f6d8399
Jennifer Freeman <jenf@users.noreply.github.com>
2020-10-23 22:01:15 +0100

    1817: Convert Token purchase bug fixes (#1992)
    * Allow taking more loans than necessary
    * Move taking loans after the treasury shares (fixes #1984)
    * Buy tokens and liquidate in one place

--- lib/engine/round/g_1817/merger.rb
+++ lib/engine/round/g_1817/merger.rb
@@ -27,9 +27,37 @@ module Engine
           return if action.free?
           return if active_step
 
+          purchase_tokens(@converted) if @tokens_needed
+          liquidate!(@converted) if @tokens_needed
           @converted = nil
+
           @game.players.each(&:unpass!)
           next_entity!
+        end
+
+        def purchase_tokens(corporation)
+          return unless token_cost.positive?
+          return if needs_money?(corporation)
+
+          corporation.spend(token_cost, @game.bank)
+          tokens_needed.times { corporation.tokens << Engine::Token.new(corporation, price: 0) }
+          @log << "#{corporation.name} pays #{@game.format_currency(token_cost)}"\
+            " for #{tokens_needed} token#{tokens_needed > 1 ? 's' : ''}"
+          @tokens_needed = nil
+        end
+
+        def needs_money?(corporation)
+          tokens_needed && token_cost > corporation.cash
+        end
+
+        def token_cost
+          (tokens_needed || 0) * 50
+        end
+
+        def liquidate!(corporation)
+          @game.liquidate!(corporation)
+          @log << "#{corporation.name} cannot purchase required tokens and liquidates"
+          @tokens_needed = nil
         end
 
         def next_entity!
--- lib/engine/step/g_1817/conversion.rb
+++ lib/engine/step/g_1817/conversion.rb
@@ -16,38 +16,12 @@ module Engine
           actions = []
           actions << 'convert' if [2, 5].include?(entity.total_shares)
           actions << 'merge' if mergeable(entity).any?
-          actions << 'take_loan' if @tokens_needed && @game.can_take_loan?(entity)
           actions << 'pass' if actions.any?
           actions
         end
 
-        def pass_description
-          if needs_money?(current_entity)
-            'Liquidate Corporation'
-          elsif current_actions.include?('take_loan')
-            'Pass (Loans)'
-          else
-            super
-          end
-        end
-
         def description
           'Convert or Merge Corporation'
-        end
-
-        def process_take_loan(action)
-          corporation = action.entity
-          @game.take_loan(corporation, action.loan)
-          purchase_tokens(corporation) unless @game.can_take_loan?(corporation)
-        end
-
-        def process_pass(action)
-          corporation = action.entity
-
-          liquidate!(corporation) if needs_money?(corporation)
-          purchase_tokens(corporation) if @tokens_needed
-
-          super
         end
 
         def process_convert(action)
@@ -59,7 +33,7 @@ module Engine
 
           tokens = corporation.tokens.size
 
-          @tokens_needed =
+          @round.tokens_needed =
             if after == 5
               tokens < 8 ? 1 : 0
             elsif after == 10
@@ -68,8 +42,6 @@ module Engine
               0
             end
 
-          liquidate!(corporation) if needs_money?(corporation) && !@game.can_take_loan?(corporation)
-          purchase_tokens(corporation)
           @round.converted = corporation
         end
 
@@ -150,24 +122,6 @@ module Engine
           super unless entity.share_price.liquidation?
         end
 
-        def liquidate!(corporation)
-          @game.liquidate!(corporation)
-          @log << "#{corporation.name} cannot purchase required tokens and liquidates"
-          @tokens_needed = nil
-        end
-
-        def purchase_tokens(corporation)
-          return unless token_cost.positive?
-          return if needs_money?(corporation)
-
-          corporation.spend(token_cost, @game.bank)
-          @tokens_needed.times { corporation.tokens << Engine::Token.new(corporation, price: 0) }
-          @log << "#{corporation.name} pays #{@game.format_currency(token_cost)}"\
-            " for #{@tokens_needed} token#{@tokens_needed > 1 ? 's' : ''}"
-          @tokens_needed = nil
-          pass!
-        end
-
         def mergeable(corporation)
           return [] if !corporation.floated? || !corporation.share_price.normal_movement?
 
@@ -191,21 +145,8 @@ module Engine
         def round_state
           {
             converted: nil,
+            tokens_needed: nil,
           }
-        end
-
-        def setup
-          @tokens_needed = nil
-        end
-
-        private
-
-        def needs_money?(corporation)
-          @tokens_needed && token_cost > corporation.cash
-        end
-
-        def token_cost
-          (@tokens_needed || 0) * 50
         end
       end
     end
