8ede9bcd8d94b80ba2df791140a1d0121b9bf23a
Michael Brandt <michaelbrandt5@gmail.com>
2020-10-22 22:05:09 -0600

    always include game settings in api response

--- models/game.rb
+++ models/game.rb
@@ -103,13 +103,7 @@ class Game < Base
 
   def to_h(include_actions: false, player: nil)
     actions_h = include_actions ? actions.map(&:to_h) : []
-
-    settings_h =
-      if include_actions
-        settings.to_h
-      else
-        { optional_rules_selected: settings['optional_rules_selected'] || [] }
-      end
+    settings_h = settings.to_h
 
     # Move user settings and hide from other players
     user_settings_h = settings_h.dig('players', player)
