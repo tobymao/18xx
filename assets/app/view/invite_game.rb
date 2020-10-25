e57465bcb92f6345f3d0c0aaef262e099671f9b6
Daniel Sousa <github@daniel.sousa.me>
2020-10-25 17:36:51 +0000

    Update invite_game.rb

--- assets/app/view/invite_game.rb
+++ assets/app/view/invite_game.rb
@@ -10,12 +10,12 @@ module View
     needs :user
 
     def render
-      children = [h(:h2, {}, 'This game has not started yet')]
-      if !@user
-        children << h(:h3, {}, 'You need to login before joining a game:')
-        children << h(View::User, type: :login)
+      children = [h(:h2, 'This game has not started yet')]
+      if @user
+        children << h(:div, [h(GameCard, gdata: @game, user: @user)])
       else
-        children << h(:div, {}, [h(GameCard, gdata: @game, user: @user)])
+        children << h(:h3, 'You need to login before joining a game:')
+        children << h(View::User, type: :login)
       end
 
       children << h(:a, { attrs: { href: '/' } }, 'Return home')
