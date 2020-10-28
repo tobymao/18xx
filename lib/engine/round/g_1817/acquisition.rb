7b6a5e3ef86d2773298e2cc90cbb069046c2f095
Michael Brandt <michaelbrandt5@gmail.com>
2020-10-27 22:39:13 -0600

    Further cleanup for Round::G1817::Acquisition#select_entities

--- lib/engine/round/g_1817/acquisition.rb
+++ lib/engine/round/g_1817/acquisition.rb
@@ -19,15 +19,14 @@ module Engine
 
         def select_entities
           # Things that are offered up for acquisition, sale etc
-          @offering =  @game
-                      .corporations
-                      .select do |corp|
-                        corp.floated? && !corp.share_price.acquisition? || @game.stock_prices_start_merger[corp].acquisition?
-                      end
-                      .sort.reverse
-                      .select do |corp|
-                        !corp.share_price.acquisition? || @game.stock_prices_start_merger[corp].acquisition?
-                      end
+          @offering = @game
+                        .corporations
+                        .select do |corp|
+                          corp.floated? &&
+                            (!corp.share_price.acquisition? ||
+                             @game.stock_prices_start_merger[corp].acquisition?)
+                        end
+                        .sort.reverse
           @game.players.select { |p| p.presidencies.any? }
         end
 
