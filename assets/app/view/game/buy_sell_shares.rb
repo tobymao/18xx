f74c3caec370b32c5a52a5aefd9dd2b4a3c4e94e
perwestling <per.o.westling@gmail.com>
2020-11-01 01:51:50 +0100

    In case different percentages is in market, separate buttons (#2128)
    This is currently only for 18MEX but it is general code that will
    be used as soon as there are different percentage shares in the
    share pool.
    
    In case there is just one type of shares the behavior will not be changed.
    
    In case there are more the one type of percentage in the share pool
    the percentage of the market share will be shown on the Buy/Exchange button.
    This is also presented even if only one of the types are buyable.
    
    So, using 18MEX, and Buy, as an example:
    
    * If 5% and 10% are in market pool, and player can buy both
      Show: "Buy 5% Market Share" "Buy 10% Market Share"
    
    * If 5% and 10% are in market pool, but player can only buy 5%
      Show: "Buy 5% Market Share"
    
    * If just 5% or 10% shares are in market pool
      Show "Buy Market Share"
    
    * And this means - if not 18MEX, but still Buy
      Show "Buy Market Share"
    
    * For Exchange a similar behavior
    
    This fixes #2097

--- assets/app/view/game/buy_sell_shares.rb
+++ assets/app/view/game/buy_sell_shares.rb
@@ -16,7 +16,7 @@ module View
         current_entity = step.current_entity
 
         ipo_share = @corporation.shares[0]
-        pool_share = @game.share_pool.shares_by_corporation[@corporation][0]
+        pool_shares = @game.share_pool.shares_by_corporation[@corporation].group_by(&:percent).values.collect(&:first)
 
         children = []
 
@@ -29,13 +29,14 @@ module View
             )
           end
 
-          if step.can_buy?(current_entity, pool_share)
-            children << h(
-              :button,
-              { on: { click: -> { buy_share(current_entity, pool_share) } } },
-              'Buy Market Share',
-            )
-          end
+          # Put up one buy button for each buyable percentage share type in market.
+          # In case there are more than one type of percentages in market (e.g. 18MEX), show percentage type on button.
+          pool_shares
+            .select { |share| step.can_buy?(current_entity, share) }
+            .each do |share|
+              text = pool_shares.size > 1 ? "Buy #{share.percent}% Market Share" : 'Buy Market Share'
+              children << h(:button, { on: { click: -> { buy_share(current_entity, share) } } }, text)
+            end
         end
 
         if step.current_actions.include?('short') && step.can_short?(current_entity, @corporation)
@@ -63,9 +64,14 @@ module View
                             "#{prefix} an #{@game.class::IPO_NAME} share")
             end
 
-            if ability.from.include?(:market) && step.can_gain?(company.owner, pool_share, exchange: true)
-              children << h(:button, { on: { click: -> { buy_share(company, pool_share) } } },
-                            "#{prefix} a Market share")
+            next unless ability.from.include?(:market)
+
+            # Put up one exchange button for each exchangable percentage share type in market.
+            pool_shares
+              .select { |share| step.can_gain?(company.owner, share, exchange: true) }
+              .each do |share|
+              text = pool_shares.size > 1 ? "#{prefix} a #{share.percent}% Market Share" : "#{prefix} a Market Share"
+              children << h(:button, { on: { click: -> { buy_share(company, share) } } }, text)
             end
           end
         end
