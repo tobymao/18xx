bef5947fa0b23b2023fdd17e39674a167bad85d7
Daniel Pierce <moofed@gmail.com>
2020-11-06 09:59:53 -0500

    total_shares instead of 10

--- assets/app/view/game/par.rb
+++ assets/app/view/game/par.rb
@@ -33,8 +33,8 @@ module View
             },
             on: { click: par },
           }
-          purchasable_shares = [(entity.cash / share_price.price).to_i, @corporation.max_ownership_percent / 10].min
-          at_limit = purchasable_shares * 10 >= @corporation.max_ownership_percent
+          purchasable_shares = [(entity.cash / share_price.price).to_i, @corporation.max_ownership_percent / @corporation.total_shares].min
+          at_limit = purchasable_shares * @corporation.total_shares >= @corporation.max_ownership_percent
           flags = at_limit ? ' L' : ''
           text = "#{@game.format_currency(share_price.price)} (#{purchasable_shares}#{flags})"
           h('button.small.par_price', props, text)
