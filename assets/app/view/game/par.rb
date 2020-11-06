56fe9989d5c7031459732592d3ca2b966419769e
Daniel Pierce <moofed@gmail.com>
2020-11-06 10:02:49 -0500

    rubocop line length

--- assets/app/view/game/par.rb
+++ assets/app/view/game/par.rb
@@ -33,7 +33,8 @@ module View
             },
             on: { click: par },
           }
-          purchasable_shares = [(entity.cash / share_price.price).to_i, @corporation.max_ownership_percent / @corporation.total_shares].min
+          purchasable_shares = [(entity.cash / share_price.price).to_i,
+                                @corporation.max_ownership_percent / @corporation.total_shares].min
           at_limit = purchasable_shares * @corporation.total_shares >= @corporation.max_ownership_percent
           flags = at_limit ? ' L' : ''
           text = "#{@game.format_currency(share_price.price)} (#{purchasable_shares}#{flags})"
