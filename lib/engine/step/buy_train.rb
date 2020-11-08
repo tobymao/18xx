8347ff8ce1e1130243d72832778764d7e04de253
Michael Brandt <michaelbrandt5@gmail.com>
2020-11-07 21:08:02 -0700

    Enforce minimum spend when emergency buying train from a corporation, enforce EBUY_OTHER_VALUE for cashless corporations (#2162)
    * enforce minimum spend when emergency buying train from a corporation
    
    * if shares have been sold to help buy a train, that cash must be used if buying
      from another corporation
    * also fix max spend; if the game allows EMR when cross-buying a train, allow
      president to contribute cash even before shares have been sold
    * don't allow cashless corporations to ebuy from others when EBUY_OTHER_VALUE is false
    * update `validate_all()` in `validate.rb` to take a list of titles
    
    [Fixes #968]
    [Fixes #2230]
    
    https://www.18xx.games/game/3063?action=442
    https://www.18xx.games/game/13894?action=376
    
    * fix passing in optional rules in validate.rb
    
    * code review cleanup

--- lib/engine/step/buy_train.rb
+++ lib/engine/step/buy_train.rb
@@ -38,6 +38,18 @@ module Engine
       end
 
       def process_buy_train(action)
+        if action.train.owned_by_corporation?
+          min, max = spend_minmax(action.entity, action.train)
+          unless (min..max).include?(action.price)
+            @game.game_error("#{action.entity.name} may not spend "\
+                             "#{@game.format_currency(action.price)} on "\
+                             "#{action.train.owner.name}'s #{action.train.name} "\
+                             'train; may only spend between '\
+                             "#{@game.format_currency(min)} and "\
+                             "#{@game.format_currency(max)}.")
+          end
+        end
+
         buy_train_action(action)
         pass! unless can_buy_train?(action.entity)
       end
