2bd2bb7ee5172b65584fb9567f02f4fa60e1dcb7
Michael Brandt <michaelbrandt5@gmail.com>
2020-11-07 19:37:18 -0700

    drop "selected" from `optional_rules_selected`
    migration script to fix game settings for this change:
    
    ```
    require_relative 'validate'
    
    where_args = {
      Sequel.pg_jsonb_op(:settings).has_key?('optional_rules_selected') => true,
    }
    
    Game.where(**where_args).each do |game|
    
      game.settings['optional_rules'] =
        if game.settings.has_key?('pin')
          # keep the old key on pinned games so they work correctly, but still add
          # the new key so their info is displayed on game cards on the home page
          # correctly as well
          game.settings['optional_rules_selected'] || []
        else
          game.settings.delete('optional_rules_selected') || []
        end
    
      game.save
    end
    ```

--- assets/app/mail/turn.rb
+++ assets/app/mail/turn.rb
@@ -14,7 +14,7 @@ class Turn < Snabberb::Component
       @game_data['players'].map { |p| p['name'] },
       id: @game_data['id'],
       actions: @game_data['actions'],
-      optional_rules: @game_data.dig('settings', 'optional_rules_selected') || [],
+      optional_rules: @game_data.dig('settings', 'optional_rules') || [],
     )
 
     store(:game, @game, skip: true)
