8f4aab080ce9405c2f145c31cff01aeff68e8205
Chris Rericha <crericha@gmail.com>
2020-11-08 05:57:24 -0500

    Add tests for coalfields functionality.

--- spec/lib/engine/games/g_1828_spec.rb
+++ spec/lib/engine/games/g_1828_spec.rb
@@ -71,5 +71,68 @@ module Engine
         expect(game.custom_end_game_reached?).to be_truthy
       end
     end
+
+    context 'VA Coalfields' do
+      let(:ic) { game.corporations.find { |corp| corp.name == 'IC' } }
+      let(:co) { game.corporations.find { |corp| corp.name == 'C&O' } }
+      let(:va_tile) { game.hex_by_id('K11').tile }
+
+      it 'should block unless coal marker purchased' do
+        player_1.cash = 10_000
+        stock_market.set_par(ic, stock_market.par_prices.first)
+        5.times { game.share_pool.buy_shares(player_1, ic.shares.first) }
+
+        next_or!
+        game.round.process_action(
+          Engine::Action::LayTile.new(ic, tile: game.tile_by_id('9-1'), hex: game.hex_by_id('J8'), rotation: 1)
+        )
+
+        next_or!
+        expect(game.round.actions_for(ic)).to_not include('buy_special')
+        game.round.process_action(
+          Engine::Action::LayTile.new(ic, tile: game.tile_by_id('8-2'), hex: game.hex_by_id('J10'), rotation: 5)
+        )
+        expect do
+          game.round.process_action(Engine::Action::PlaceToken.new(ic, city: va_tile.cities.first, slot: 0))
+        end.to raise_error GameError
+
+        next_or!
+        expect(game.graph.connected_hexes(ic).include?(game.hex_by_id('J12'))).to be_falsey
+        expect(game.graph.connected_hexes(ic).include?(game.hex_by_id('K13'))).to be_falsey
+        expect(game.round.actions_for(ic)).to include('buy_special')
+
+        game.round.process_action(Engine::Action::BuySpecial.new(ic, item: 'Coal Marker'))
+        expect(game.coal_marker?(ic)).to be_truthy
+        expect(va_tile.icons.count { |icon| icon.name == 'coal' }).to eq(1)
+        expect(game.graph.connected_hexes(ic).include?(game.hex_by_id('J12'))).to be_truthy
+        expect(game.graph.connected_hexes(ic).include?(game.hex_by_id('K13'))).to be_truthy
+
+        game.round.process_action(
+          Engine::Action::LayTile.new(ic, tile: game.tile_by_id('4-0'), hex: game.hex_by_id('K13'), rotation: 1)
+        )
+        expect(va_tile.icons.count { |icon| icon.name == 'coal' }).to eq(2)
+        expect do
+          game.round.process_action(Engine::Action::PlaceToken.new(ic, city: va_tile.cities.first, slot: 0))
+        end.to raise_error GameError
+      end
+
+      it 'should acquire coal marker when laying VA tunnel' do
+        player_1.cash = 10_000
+        stock_market.set_par(co, stock_market.par_prices.first)
+        5.times { game.share_pool.buy_shares(player_1, co.shares.first) }
+
+        next_or!
+        game.round.process_action(
+          Engine::Action::LayTile.new(co, tile: game.tile_by_id('57-0'), hex: game.hex_by_id('K15'), rotation: 1)
+        )
+        expect(game.coal_marker?(co)).to be_falsey
+
+        next_or!
+        game.round.process_action(
+          Engine::Action::LayTile.new(co, tile: game.tile_by_id('4-0'), hex: game.hex_by_id('K13'), rotation: 1)
+        )
+        expect(game.coal_marker?(co)).to be_truthy
+      end
+    end
   end
 end
