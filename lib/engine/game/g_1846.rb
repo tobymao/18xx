e6c4a2d13bf1d0ae033d37461f1d5743e5b2ea8d
Daniel Sousa <git@daniel.sousa.me>
2020-11-15 09:34:18 +0000

    Added option in config to make the cert limit depend on the number of corporations in play (1870 and 1846)

--- lib/engine/game/g_1846.rb
+++ lib/engine/game/g_1846.rb
@@ -104,36 +104,6 @@ module Engine
         two_player? ? 0 : players.size
       end
 
-      def cert_limit
-        num_corps = @corporations.size
-        case @players.size
-        when 2
-          num_corps == 5 ? 19 : 16
-        when 3
-          num_corps == 5 ? 14 : 11
-        when 4
-          case num_corps
-          when 6
-            12
-          when 5
-            10
-          else
-            8
-          end
-        when 5
-          case num_corps
-          when 7
-            11
-          when 6
-            10
-          when 5
-            8
-          else
-            6
-          end
-        end
-      end
-
       def setup
         @turn = setup_turn
 
