tobymao/18xx

commit d04bdd0a2c2eba585461b37b739626ee2819584d

Added option in config to make the cert limit depend on the number of (#2317)
corporations in play (1870 and 1846)

Co-authored-by: Daniel Sousa <git@daniel.sousa.me>

lib/engine/game/g_1846.rb
@@ -104,36 +104,6 @@ module Engine
         two_player? ? 0 : players.size
       end
 
-      def cert_limit
-        num_corps = @corporations.size
-        case @players.size
-        when 2
-          num_corps == 5 ? 19 : 16
-        when 3
-          num_corps == 5 ? 14 : 11
-        when 4
-          case num_corps
-          when 6
-            12
-          when 5
-            10
-          else
-            8
-          end
-        when 5
-          case num_corps
-          when 7
-            11
-          when 6
-            10
-          when 5
-            8
-          else
-            6
-          end
-        end
-      end
-
       def setup
         @turn = setup_turn
 

lib/engine/config/game/g_1846.rb
@@ -20,9 +20,21 @@ module Engine
     "5": 9000
   },
   "certLimit": {
-    "3": 14,
-    "4": 12,
-    "5": 11
+    "3": {
+      "5" : 14,
+      "4" : 11
+    },
+    "4": {
+      "6" : 12,
+      "5" : 10,
+      "4" : 8
+    },
+    "5": {
+      "7" : 11,
+      "6" : 10,
+      "5" : 8,
+      "4" : 6
+    }
   },
   "startingCash": {
     "2": 600,

lib/engine/config/game/g_1870.rb
@@ -13,11 +13,26 @@ module Engine
   "currencyFormatStr": "$%d",
   "bankCash": 12000,
   "certLimit": {
-    "2": 28,
-    "3": 20,
-    "4": 16,
-    "5": 13,
-    "6": 11
+    "2": {
+      "10" : 28,
+      "9" : 24
+    },
+    "3": {
+      "10" : 20,
+      "9" : 17
+    },
+    "4": {
+      "10" : 16,
+      "9" : 14
+    },
+    "5": {
+      "10" : 13,
+      "9" : 11
+    },
+    "6": {
+      "10" : 11,
+      "9" : 9
+    }
   },
   "startingCash": {
     "2": 1050,

lib/engine/game/base.rb
@@ -875,9 +875,8 @@ module Engine
         player.bankrupt = true
         return unless self.class::CERT_LIMIT_CHANGE_ON_BANKRUPTCY
 
-        remaining_players = @players.reject(&:bankrupt).length
         # Assume that games without cert limits at lower player counts retain previous counts (1817 and 2 players)
-        @cert_limit = self.class::CERT_LIMIT[remaining_players] || @cert_limit
+        @cert_limit = init_cert_limit
       end
 
       def tile_lays(_entity)
@@ -1011,7 +1010,10 @@ module Engine
 
       def init_cert_limit
         cert_limit = self.class::CERT_LIMIT
-        cert_limit.is_a?(Hash) ? cert_limit[players.size] : cert_limit
+        cert_limit = cert_limit[players.reject(&:bankrupt).length] if cert_limit.is_a?(Hash)
+        cert_limit = cert_limit.reject { |k, _| k.to_i < @corporations.size }
+                       .min_by(&:first)&.last || cert_limit.first.last if cert_limit.is_a?(Hash)
+        cert_limit || @cert_limit
       end
 
       def init_phase

commit 6becd574394332705ad0ad095d474dd8373d557a

1846: Fixed regression introduced with d04bdd0

lib/engine/game/g_1846.rb
@@ -134,6 +134,8 @@ module Engine
           end
         end
 
+        @cert_limit = init_cert_limit
+
         @companies.each do |company|
           company.min_price = 1
           company.max_price = company.value