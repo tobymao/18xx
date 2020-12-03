51e699d8405841624b0f5a45a65c1a5cfb871bad
Benjamin Scott <1711810+benjaminxscott@users.noreply.github.com>
2020-12-02 21:01:10 -0800

    update config

--- lib/engine/game/g_1848.rb
+++ lib/engine/game/g_1848.rb
@@ -8,20 +8,21 @@ module Engine
     class G1848 < Base
       load_from_json(Config::Game::G1848::JSON)
 
+      DEV_STAGE = :alpha
       GAME_LOCATION = 'Australia'
       GAME_RULES_URL = 'http://ohley.de/english/1848/1848-rules.pdf'
       GAME_DESIGNER = 'Leonhard Orgler and Helmut Ohley'
-      #GAME_PUBLISHER = Publisher::INFO[:oo_games]
+      GAME_PUBLISHER = :oo_games
       #GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/1848'
 
-      # Two tiles can be laid, only one upgrade
+      # Two tiles can be laid at a time, with max one upgrade
+      # TODO - until green, can only build one yellow - is this reflected?
       TILE_LAYS = [{ lay: true, upgrade: true }, { lay: true, upgrade: :not_if_upgraded }].freeze
 
-      #<TODO> Check this
-      #HOME_TOKEN_TIMING = :operating_round
-      
+      HOME_TOKEN_TIMING = :operate
+
       #<TODO> Need to define cert_limit (see g_1846 for reference?)
-      
+
       def init_round
         Round::Auction.new(self, [Step::G1848::DutchAuction])
       end
@@ -42,7 +43,7 @@ module Engine
           [Step::BuyCompany, blocks: true],
         ], round_num: round_num)
       end
-      
+
     end
   end
 end
