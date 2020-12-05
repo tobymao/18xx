9b27b38ed8520f354445bf67d1ff35b143d4285a
R. Ryan Driskel <ryandriskel@gmail.com>
2020-12-05 08:51:29 -0700

    18CO Restrictive Track Lay

--- lib/engine/step/g_18_co/tracker.rb
+++ lib/engine/step/g_18_co/tracker.rb
@@ -24,6 +24,59 @@ module Engine
 
           @log << "#{corporation.name} collects a mine token from #{hex.name}"
         end
+
+        def check_track_restrictions!(entity, old_tile, new_tile)
+          super(entity, old_tile, new_tile)
+
+          return if new_tile.color == :yellow # not an upgrade
+          return if old_tile.city_towns.any? # not plain track
+          return if new_tile.color == :green && new_tile.exits.size == 4 # no new junctions, cheap short circuit
+
+          brand_new_exit_junctions = new_exit_junctions(old_tile, new_tile)
+          return if brand_new_exit_junctions.empty?
+
+          old_connected_paths = prior_connected_paths(entity, new_tile, old_tile)
+          return if all_prior_paths_accessible?(old_tile.paths, old_connected_paths)
+
+          @game.game_error('Must have route to access existing track') if old_connected_paths.empty?
+
+          return if accessible_new_path_creates_new_exit?(brand_new_exit_junctions)
+          return if all_new_paths_have_new_exits?(brand_new_exit_junctions)
+
+          @game.game_error('Must have route to access both ends of at least one path of new track')
+        end
+
+        def new_exit_junctions(old_tile, new_tile)
+          brand_new_paths = new_tile.paths.reject { |path| old_tile.paths.find { |p| path <= p } }
+
+          brand_new_paths.map do |bnp|
+            new_exit_junctions = bnp.exits & old_tile.exits
+            next if new_exit_junctions.empty?
+
+            [bnp, new_exit_junctions]
+          end.compact
+        end
+
+        def prior_connected_paths(entity, new_tile, old_tile)
+          @game.graph.connected_paths(entity).map do |connected_path, _b|
+            next unless connected_path.hex == new_tile.hex
+
+            old_tile.paths.find { |op| op <= connected_path }
+          end.compact
+        end
+
+        def all_prior_paths_accessible?(old_paths, old_connected_paths)
+          old_connected_paths.size == old_paths.size
+        end
+
+        def accessible_new_path_creates_new_exit?(brand_new_exit_junctions)
+          brand_new_exit_junctions.flat_map { |_bnp, new_exit_junctions| new_exit_junctions }.one?
+        end
+
+        def all_new_paths_have_new_exits?(brand_new_exit_junctions)
+          brand_new_exit_junctions.size >=
+            brand_new_exit_junctions.sum { |_bnp, new_exit_junctions| new_exit_junctions.size }
+        end
       end
     end
   end
