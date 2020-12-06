a881ab85226d7a3c66e6064a430f9229f099b837
R. Ryan Driskel <ryandriskel@gmail.com>
2020-12-06 09:59:51 -0700

    18CO Fix Normal Track Lay
    I changed the function signature and forgot to update. Stupid. At least we're not live.

--- lib/engine/step/g_18_co/track.rb
+++ lib/engine/step/g_18_co/track.rb
@@ -12,7 +12,7 @@ module Engine
         def process_lay_tile(action)
           lay_tile_action(action)
           clear_upgrade_icon(action.hex.tile)
-          collect_mines(action)
+          collect_mines(action.entity, action.hex)
 
           pass! unless can_lay_tile?(action.entity)
         end
