74fce4ddfb3816e3e3a850b176b525c63e524e73
R. Ryan Driskel <ryandriskel@gmail.com>
2020-12-15 11:09:07 -0700

    Fix/Refactor Emergency Money + Issuable Shares
    See original PR for a complete list of refactor/change explanations.
    
    The problem with my first PR was emergency_money > can_sell? finished with an "unless" statement, thus the passing case returned nil instead of true. This only affected 18MS as that's the only game with @game.class::EBUY_SELL_MORE_THAN_NEEDED. I have included a new fixture that addresses that path.

--- lib/engine/step/train.rb
+++ lib/engine/step/train.rb
@@ -80,25 +80,27 @@ module Engine
         pass! unless can_buy_train?(entity)
       end
 
-      def can_sell?(_entity, bundle)
+      def can_sell?(entity, bundle)
         return false if @game.class::MUST_SELL_IN_BLOCKS && @corporations_sold.include?(bundle.corporation)
-        return false if must_issue_before_ebuy?(current_entity)
+        return false if current_entity != entity && must_issue_before_ebuy?(current_entity)
 
         super
       end
 
       def process_sell_shares(action)
-        @last_share_sold_price = action.bundle.price_per_share
+        @last_share_sold_price = action.bundle.price_per_share unless action.entity == current_entity
         super
-        @corporations_sold << action.bundle.corporation
+        @corporations_sold << action.bundle.corporation unless action.entity == current_entity
       end
 
       def needed_cash(_entity)
         @game.class::EBUY_DEPOT_TRAIN_MUST_BE_CHEAPEST ? @depot.min_depot_price : @depot.max_depot_price
       end
 
-      def available_cash(player)
-        player.cash + current_entity.cash
+      def available_cash(entity)
+        return current_entity.cash if entity == current_entity
+
+        entity.cash + current_entity.cash
       end
 
       def buyable_trains(entity)
@@ -140,10 +142,6 @@ module Engine
         @last_share_sold_price = nil
         @last_share_issued_price = nil
         @corporations_sold = []
-      end
-
-      def issuable_shares
-        []
       end
 
       def must_issue_before_ebuy?(corporation)
