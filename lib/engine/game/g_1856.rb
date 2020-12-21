e514611447be3c7f14fe60d7a7879a0e4f606d96
ventusignis <mgilzinger@google.com>
2020-12-21 00:21:14 -0500

    Close the port private on use

--- lib/engine/game/g_1856.rb
+++ lib/engine/game/g_1856.rb
@@ -72,7 +72,7 @@ module Engine
 
       def gray_phase?
         @phase.tiles.include?('gray')
-      end     
+      end
 
       def revenue_for(route, stops)
         revenue = super
@@ -292,7 +292,7 @@ module Engine
           Step::G1856::CashCrisis,
           # No exchanges.
           Step::DiscardTrain,
-          Step::Assign,
+          Step::G1856::Assign,
           Step::G1856::Loan,
           Step::SpecialTrack,
           Step::BuyCompany,
---
+++ lib/engine/step/g_1856/assign.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../assign'
+
+module Engine
+  module Step
+    module G1856
+      class Assign < Assign
+        def process_assign(action)
+          company = action.entity
+          target = action.target
+
+          unless (ability = @game.abilities(company, :assign_hexes))
+            @game.game_error("Could not assign #{company.name} to #{target.name}; :assign_hexes ability not found")
+          end
+          case company.id
+          when 'GLSC'
+            target.assign!(company.id)
+            ability.use!
+            @log << "#{company.name} builds port on #{target.name} and closes"
+            company.close!
+          end
+        end
+      end
+    end
+  end
+end
