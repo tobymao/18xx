dee06ae8b8ad8294f607cbdaa2588060535c6f0b
ventusignis <mgilzinger@google.com>
2021-01-01 21:53:13 -0500

    Make 1856 Diesel black so that offboards can change on D, not 6 and add black hex color

--- lib/engine/config/game/g_1856.rb
+++ lib/engine/config/game/g_1856.rb
@@ -619,7 +619,7 @@ module Engine
   ],
   "hexes": {
     "red": {
-      "offboard=revenue:yellow_30|brown_50|gray_60;path=a:4,b:_0;path=a:5,b:_0": [
+      "offboard=revenue:yellow_30|brown_50|black_60;path=a:4,b:_0;path=a:5,b:_0": [
         "A20"
       ],
       "border=edge:4": [
@@ -628,22 +628,22 @@ module Engine
       "offboard=revenue:yellow_30|brown_50;path=a:0,b:_0;path=a:5,b:_0;border=edge:1": [
         "B13"
       ],
-      "offboard=revenue:yellow_30|brown_50|gray_40;path=a:0,b:_0;path=a:5,b:_0;icon=image:port,sticky:1": [
+      "offboard=revenue:yellow_30|brown_50|black_40;path=a:0,b:_0;path=a:5,b:_0;icon=image:port,sticky:1": [
         "H5"
       ],
       "offboard=revenue:yellow_20|brown_30;path=a:0,b:_0;path=a:5,b:_0;icon=image:port,sticky:1": [
         "K2"
       ],
-      "offboard=revenue:yellow_20|brown_30|gray_50,hide:1,groups:Canadian West;path=a:0,b:_0;path=a:1,b:_0;border=edge:5": [
+      "offboard=revenue:yellow_20|brown_30|black_50,hide:1,groups:Canadian West;path=a:0,b:_0;path=a:1,b:_0;border=edge:5": [
         "N1"
       ],
-      "offboard=revenue:yellow_20|brown_30|gray_50,groups:Canadian West;path=a:0,b:_0;path=a:1,b:_0;path=a:5,b:_0;border=edge:2": [
+      "offboard=revenue:yellow_20|brown_30|black_50,groups:Canadian West;path=a:0,b:_0;path=a:1,b:_0;path=a:5,b:_0;border=edge:2": [
         "O2"
       ],
-      "offboard=revenue:yellow_20|brown_30|gray_50,groups:Lower Canada;path=a:1,b:_0;path=a:2,b:_0;border=edge:0": [
+      "offboard=revenue:yellow_20|brown_30|black_50,groups:Lower Canada;path=a:1,b:_0;path=a:2,b:_0;border=edge:0": [
         "Q8"
       ],
-      "offboard=revenue:yellow_20|brown_30|gray_50,hide:1,groups:Lower Canada;path=a:2,b:_0;border=edge:3": [
+      "offboard=revenue:yellow_20|brown_30|black_50,hide:1,groups:Lower Canada;path=a:2,b:_0;border=edge:3": [
         "Q10"
       ],
       "offboard=revenue:yellow_30|brown_40,hide:1,groups:Buffalo;path=a:2,b:_0;border=edge:3": [
@@ -675,7 +675,7 @@ module Engine
       ]
     },
     "gray": {
-      "town=revenue:yellow_30|brown_50|gray_40;path=a:0,b:_0;path=a:4,b:_0;path=a:5,b:_0;icon=image:port,sticky:1": [
+      "town=revenue:yellow_30|brown_50|black_40;path=a:0,b:_0;path=a:4,b:_0;path=a:5,b:_0;icon=image:port,sticky:1": [
         "F9"
       ]
     },
@@ -925,7 +925,8 @@ module Engine
         "yellow",
         "green",
         "brown",
-        "gray"
+        "gray",
+        "black"
         ],
         "status":[
         "fullcap",
