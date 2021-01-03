ff6bab518405ad029237d3e166b7e6dd1ee47399
ventusignis <mgilzinger@google.com>
2021-01-03 13:29:06 -0500

    Fixes 3013

--- lib/engine/step/g_1817/dividend.rb
+++ lib/engine/step/g_1817/dividend.rb
@@ -8,6 +8,8 @@ module Engine
     module G1817
       class Dividend < Dividend
         DIVIDEND_TYPES = %i[payout half withhold].freeze
+        PENULTIMATE_PRICE = 540
+        FINAL_PRICE = 600
         include HalfPay
 
         def half_pay_withhold_amount(entity, revenue)
@@ -21,8 +23,8 @@ module Engine
           return { share_direction: :left, share_times: 1 } unless revenue.positive?
 
           times = 0
-          times = 1 if revenue >= price
-          times = 2 if revenue >= price * 2
+          times = 1 if revenue >= price && price < FINAL_PRICE
+          times = 2 if revenue >= price * 2 && price < PENULTIMATE_PRICE
           if times.positive?
             { share_direction: :right, share_times: times }
           else
