9863e6e37f89bfbdf52706544c02900ee3ee2568
tobymao <toby.mao@gmail.com>
2021-01-09 12:23:21 -0800

    update welcome

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -17,8 +17,8 @@ module View
 
     def render_notification
       message = <<~MESSAGE
+        <p>18Chesapeake: Off The Rails is now in beta with a new stock market.</p>
         <p>You can now click on the log to view history or undo to any point in time!</p>
-        <p>18Chesapeake After Dark has been renamed to be 18Chesapeake Off The Rails.</p>
 
         <p>Please file <a href='https://github.com/tobymao/18xx/issues'>issues and ideas</a> on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>.<br>
