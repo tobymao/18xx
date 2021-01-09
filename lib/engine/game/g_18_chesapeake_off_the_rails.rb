20cb07cc77a9ce58711101febdd00b83af0045f6
scottredracecar <scott@redracecar.com>
2021-01-09 14:09:20 -0600

    Move Off the Rails to Beta

--- lib/engine/game/g_18_chesapeake_off_the_rails.rb
+++ lib/engine/game/g_18_chesapeake_off_the_rails.rb
@@ -8,7 +8,7 @@ module Engine
     class G18ChesapeakeOffTheRails < G18Chesapeake
       load_from_json(Config::Game::G18ChesapeakeOffTheRails::JSON)
 
-      DEV_STAGE = :alpha
+      DEV_STAGE = :beta
 
       GAME_RULES_URL = 'https://docs.google.com/document/d/1HI9HyOoCamBEbuE_HCzr2b86xtRDI_hBI3Sbc53KxyA/edit'
       GAME_DESIGNER = 'Scott Petersen'
