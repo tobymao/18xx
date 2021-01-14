30c9069ebe640e458e1ee964791ccf3de78d31ae
Zwergenpunk <Zwergenpunk@web.de>
2021-01-14 19:56:38 +0100

    Fixed Rubocop

--- lib/engine/step/g_18_cz/buy_train.rb
+++ lib/engine/step/g_18_cz/buy_train.rb
@@ -6,14 +6,13 @@ module Engine
   module Step
     module G18CZ
       class BuyTrain < BuyTrain
-
         def buyable_train_variants(train, entity)
           trains = super
           return [] unless trains.any?
 
-          return trains.select { |item| !!(item[:name] =~ /^[2-5][a-j]$/) } if entity.type == :small
-          return trains.select { |item| !!(item[:name] =~ /^[2-5]\+[2-5][a-j]$/) } if entity.type == :medium
-          return trains.select { |item| !!(item[:name] =~ /^[3-8]E[a-j]?$/) } if entity.type == :large
+          return trains.select { |item| (item[:name] =~ /^[2-5][a-j]$/) } if entity.type == :small
+          return trains.select { |item| (item[:name] =~ /^[2-5]\+[2-5][a-j]$/) } if entity.type == :medium
+          return trains.select { |item| (item[:name] =~ /^[3-8]E[a-j]?$/) } if entity.type == :large
         end
       end
     end
