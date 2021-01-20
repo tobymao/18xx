8dd43bc667701913291942e57323d3f23d09083f
etowle <8570492+etowle@users.noreply.github.com>
2021-01-19 21:11:44 -0800

    18Mag: fix L37 revenue

--- lib/engine/config/game/g_18_mag.rb
+++ lib/engine/config/game/g_18_mag.rb
@@ -186,7 +186,7 @@ module Engine
     "L37": {
       "count": 1,
       "color": "gray",
-      "code": "city=revenue:60,slots:4;path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0;label=B"
+      "code": "city=revenue:80,slots:4;path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0;label=B"
     }
   },
   "market": [
