5f61c1b0e6c6bc0dc71f92c8677275df1033c1d6
yzemaze <git@yzemaze.de>
2021-01-22 16:38:16 +0100

    inner joins, encapsulate queries

--- models/game.rb
+++ models/game.rb
@@ -11,75 +11,92 @@ class Game < Base
   QUERY_LIMIT = 13
   PERSONAL_QUERY_LIMIT = 9
 
-  FILTER_QUERY = <<~SQL
+  USER_GAMES_IDS = <<~SQL
+    SELECT game_id AS id
+    FROM game_users
+    WHERE user_id = :user_id
+  SQL
+
+  FILTERED_GAMES = <<~SQL
     SELECT *
     FROM (SELECT g.*,
         setweight(to_tsvector(g.title), 'A') ||
         setweight(to_tsvector(g.round), 'D') ||
-        setweight(to_tsvector(g.description), 'C') ||
+        setweight(to_tsvector(COALESCE(g.description, '')), 'C') ||
         setweight(to_tsvector(coalesce(string_agg(u.name, ' '))), 'B') as ts_vector
        FROM games g
-       JOIN game_users gu ON g.id = gu.game_id
-       JOIN users u ON u.id = gu.user_id
+       INNER JOIN game_users gu ON g.id = gu.game_id
+       INNER JOIN users u ON u.id = gu.user_id
+       WHERE g.status = :status
        GROUP BY g.id) g_search
-    WHERE g_search.ts_vector @@ to_tsquery('(183:* & (test | local2)) | !Auction')
-    ORDER BY ts_rank(g_search.ts_vector, to_tsquery('(183:* & (test | local2)) | !Auction')) DESC, updated_at DESC;
+    WHERE g_search.ts_vector @@ to_tsquery(:search_string)
+    /* ORDER BY ts_rank(g_search.ts_vector, to_tsquery(:search_string)) DESC, updated_at DESC */
+  SQL
+
+  ALL_GAMES_SEARCH_QUERY = <<~SQL
+    SELECT *
+    FROM (#{FILTERED_GAMES}) filtered_games
+    WHERE NOT COALESCE((settings->>'unlisted')::boolean, false)
+    ORDER BY updated_at DESC
+    LIMIT #{QUERY_LIMIT}
+    OFFSET :page * #{QUERY_LIMIT - 1}
   SQL
 
   ALL_GAMES_QUERY = <<~SQL
     SELECT *
     FROM games
     WHERE status = :status
-      AND (settings->>'unlisted')::boolean = false
+    AND NOT COALESCE((settings->>'unlisted')::boolean, false)
     ORDER BY updated_at DESC
     LIMIT #{QUERY_LIMIT}
     OFFSET :page * #{QUERY_LIMIT - 1}
   SQL
 
-  NON_PERSONAL_GAMES_QUERY = <<~SQL
-    WITH user_games AS (
-      SELECT game_id AS id
-      FROM game_users
-      WHERE user_id = :user_id
-    )
-
+  OTHER_GAMES_SEARCH_QUERY = <<~SQL
+    WITH user_games AS (#{USER_GAMES_IDS})
+    , filtered_games AS (#{FILTERED_GAMES})
     SELECT g.*
-    FROM games g
+    FROM filtered_games g
     LEFT JOIN user_games ug
       ON g.id = ug.id
-    WHERE g.status = :status
-      AND ug.id IS NULL
-      AND (settings->>'unlisted')::boolean = false
-    ORDER BY g.updated_at DESC
+    WHERE ug.id IS NULL
+      AND NOT COALESCE((settings->>'unlisted')::boolean, false)
+    ORDER BY updated_at DESC
     LIMIT #{QUERY_LIMIT}
     OFFSET :page * #{QUERY_LIMIT - 1}
   SQL
 
-  PERSONAL_GAMES_QUERY = <<~SQL
-    WITH user_games AS (
-      SELECT game_id AS id
-      FROM game_users
-      WHERE user_id = :user_id
-    )
-    , filtered_games AS (
-      SELECT id, user_id, description, title, max_players, settings, status, turn, round, acting, result, created_at, updated_at
-      FROM (SELECT g.*,
-          setweight(to_tsvector(g.title), 'A') ||
-          setweight(to_tsvector(g.round), 'D') ||
-          setweight(to_tsvector(g.description), 'C') ||
-          setweight(to_tsvector(coalesce(string_agg(u.name, ' '))), 'B') as ts_vector
-         FROM games g
-         JOIN game_users gu ON g.id = gu.game_id
-         JOIN users u ON u.id = gu.user_id
-         GROUP BY g.id) g_search
-      WHERE g_search.ts_vector @@ to_tsquery(:search_string)
-      ORDER BY ts_rank(g_search.ts_vector, to_tsquery(:search_string)) DESC, updated_at DESC
-      /* WHERE g_search.ts_vector @@ to_tsquery('(183:* & (test | local2)) | !Auction') */
-    )
+  OTHER_GAMES_QUERY = <<~SQL
+    WITH user_games AS (#{USER_GAMES_IDS})
+    SELECT g.*
+    FROM games g
+    LEFT JOIN user_games ug
+      ON g.id = ug.id
+    WHERE ug.id IS NULL
+      AND g.status = :status
+      AND NOT COALESCE((settings->>'unlisted')::boolean, false)
+    ORDER BY updated_at DESC
+    LIMIT #{QUERY_LIMIT}
+    OFFSET :page * #{QUERY_LIMIT - 1}
+  SQL
 
+  PERSONAL_GAMES_SEARCH_QUERY = <<~SQL
+    WITH user_games AS (#{USER_GAMES_IDS})
+    , filtered_games AS (#{FILTERED_GAMES})
     SELECT g.*
     FROM filtered_games g
-    JOIN user_games ug
+    INNER JOIN user_games ug
+      ON g.id = ug.id
+    ORDER BY g.acting && '{:user_id}' DESC, g.updated_at DESC
+    LIMIT #{PERSONAL_QUERY_LIMIT}
+    OFFSET :page * #{PERSONAL_QUERY_LIMIT - 1}
+  SQL
+
+  PERSONAL_GAMES_QUERY = <<~SQL
+    WITH user_games AS (#{USER_GAMES_IDS})
+    SELECT g.*
+    FROM games g
+    INNER JOIN user_games ug
       ON g.id = ug.id
     WHERE g.status = :status
     ORDER BY g.acting && '{:user_id}' DESC, g.updated_at DESC
@@ -92,17 +109,17 @@ class Game < Base
       type: opts['games'] || (user ? 'personal' : 'all'),
       page: opts['p']&.to_i || 0,
       status: opts['status'] || (user ? 'active' : 'new'),
-      search_string: opts['s'] || '1:*',
+      search_string: opts['s'] || nil,
     }
     opts[:user_id] = user.id if user
 
     query =
       if user && opts[:type] == 'personal'
-        PERSONAL_GAMES_QUERY
+        opts[:search_string] ? PERSONAL_GAMES_SEARCH_QUERY : PERSONAL_GAMES_QUERY
       elsif user
-        NON_PERSONAL_GAMES_QUERY
+        opts[:search_string] ? OTHER_GAMES_SEARCH_QUERY : OTHER_GAMES_QUERY
       else
-        ALL_GAMES_QUERY
+        opts[:search_string] ? ALL_GAMES_SEARCH_QUERY : ALL_GAMES_QUERY
       end
 
     fetch(query, **opts,).all
