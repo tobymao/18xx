7f5c2269871e8d3f081cc8bce8cf599851b39472
Zwergenpunk <Zwergenpunk@web.de>
2021-01-23 13:26:39 +0100

    review comments

--- lib/engine/step/g_18_cz/token.rb
+++ lib/engine/step/g_18_cz/token.rb
@@ -7,14 +7,10 @@ module Engine
     module G18CZ
       class Token < Token
         def process_place_token(action)
-          ensure_ate_home_token_is_free(action)
-          super
-        end
-
-        def ensure_ate_home_token_is_free(action)
           raise GameError,
                 'B9 cannot be tokened until ATE places its hometoken' if action.city.tile.hex.coordinates == 'B9' &&
                     action.city.tile.cities.all? { |city| city.tokens.compact.empty? }
+          super
         end
       end
     end
