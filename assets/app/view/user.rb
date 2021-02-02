6d686b497cbc4fac8edfbbcff050aa868021ec79
yzemaze <git@yzemaze.de>
2021-02-02 10:03:21 +0100

    more profile cleanup

--- assets/app/view/user.rb
+++ assets/app/view/user.rb
@@ -42,22 +42,18 @@ module View
       title = 'Profile Settings'
       inputs = [
         render_username,
-        render_notifications(setting_for(:notifications)),
-        render_simple_logos(setting_for(:simple_logos)),
-        h('div#settings__colors', [
+        h('div#settings__options', [
+          render_notifications(setting_for(:notifications)),
+          render_simple_logos(setting_for(:simple_logos)),
           render_logo_color(setting_for(:red_logo)),
-          h(:div, [
-            render_color('Main Background', :bg, color_for(:bg)),
-            render_color('Main Font Color', :font, color_for(:font)),
-          ]),
-          h(:div, [
-            render_color('Alternative Background', :bg2, color_for(:bg2)),
-            render_color('Alternative Font Color', :font2, color_for(:font2)),
-          ]),
-          h(:div, [
-            render_color('Your Turn', :your_turn, color_for(:your_turn)),
-            render_color('Hotseat Game', :hotseat_game, color_for(:hotseat_game)),
-          ]),
+        ]),
+        h('div#settings__colors', { style: { maxWidth: '38rem' } }, [
+          render_color('Main Background', :bg, color_for(:bg)),
+          render_color('Main Font Color', :font, color_for(:font)),
+          render_color('Alternative Background', :bg2, color_for(:bg2)),
+          render_color('Alternative Font Color', :font2, color_for(:font2)),
+          render_color('Your Turn', :your_turn, color_for(:your_turn)),
+          render_color('Hotseat Game', :hotseat_game, color_for(:hotseat_game)),
         ]),
         render_tile_colors,
         render_route_colors,
@@ -141,54 +137,49 @@ module View
     def render_username
       h('div#settings__username', [
         render_input(
-          'User Name:',
+          'User Name',
           id: :name,
           attrs: { value: @user&.dig('name') || '' },
-          container_style: { margin: '0' },
-          input_style: { width: '11rem' },
+          input_style: { width: '10.5rem' },
         ),
       ])
     end
 
     def render_notifications(checked = true)
-      h('div#settings__notifications', [
-        render_input(
-          'Turn and Message Emails',
-          id: :notifications,
-          type: :checkbox,
-          attrs: { checked: checked },
-        ),
-      ])
+      render_input(
+        'Turn and Message Emails',
+        id: :notifications,
+        type: :checkbox,
+        attrs: { checked: checked },
+      )
     end
 
     def render_simple_logos(checked = true)
-      h('div#settings__simple_logos', [
-        render_input(
-          'Simple Corporation Logos',
-          id: :simple_logos,
-          type: :checkbox,
-          attrs: { checked: checked },
-        ),
-      ])
+      render_input(
+        'Simple Corporation Logos',
+        id: :simple_logos,
+        type: :checkbox,
+        attrs: { checked: checked },
+      )
+    end
+
+    def render_logo_color(checked = false)
+      render_input(
+        'Red 18xx.games Logo',
+        id: :red_logo,
+        type: :checkbox,
+        attrs: { checked: checked },
+      )
     end
 
     def render_color(label, id, hex_color, attrs: {})
       render_input(label, id: id, type: :color, attrs: { value: hex_color, **attrs })
     end
 
-    def render_logo_color(red_logo)
-      render_input(
-        'Red 18xx.games Logo',
-        id: :red_logo,
-        type: :checkbox,
-        attrs: { checked: red_logo },
-      )
-    end
-
     def render_tile_colors
       h('div#settings__tiles', [
         h(:h3, 'Map & Tile Colors'),
-        h('div#settings__tiles__buttons', TILE_COLORS.map do |color, _|
+        h(:div, TILE_COLORS.map do |color, _|
           render_color('', color, setting_for(color), attrs: { title: color == 'white' ? 'plain' : color })
         end),
       ])
@@ -254,7 +245,7 @@ module View
         },
       }
 
-      h('div#routes', [
+      h('div#settings__routes', [
         h(:h3, 'Routes, Trains & Players'),
         h(:div, grid_props, [
           h(:div, ''),
