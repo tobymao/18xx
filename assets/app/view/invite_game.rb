315bb1059fc144c357343fe4146d3cf2d998aaab
daniel-sousa-me <github@daniel.sousa.me>
2021-02-02 23:46:17 +0000

    Fix invite page to save data in @game_data instead of @game. Auto refresh

--- assets/app/view/invite_game.rb
+++ assets/app/view/invite_game.rb
@@ -8,17 +8,45 @@ module View
     include GameManager
 
     needs :user
+    needs :refreshing, default: false, store: true
 
     def render
+      game_refresh
+
       children = [h(:h2, 'This game has not started yet')]
       if @user
-        children << h(:div, [h(GameCard, gdata: @game, user: @user)])
+        children << h(:div, [h(GameCard, gdata: @game_data, user: @user)])
       else
         children << h(:h3, 'You need to login before joining a game:')
         children << h(View::User, type: :login)
       end
 
-      h('div', children)
+      destroy = lambda do
+        `clearTimeout(#{@refreshing})`
+        store(:refreshing, nil, skip: true)
+      end
+
+      props = {
+        key: 'invite_page',
+        hook: {
+          destroy: destroy,
+        },
+      }
+
+      h('div', props, children)
+    end
+
+    def game_refresh
+      return if @refreshing
+
+      timeout = %x{
+        setTimeout(function(){
+          self['$get_game'](#{@game_data['id']})
+          self['$store']('refreshing', nil, Opal.hash({skip: true}))
+        }, 10000)
+      }
+
+      store(:refreshing, timeout, skip: true)
     end
   end
 end
