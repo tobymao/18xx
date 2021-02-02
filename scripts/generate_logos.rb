c3819f7f7bbe21ac99632f39c388a97b193df864
tobymao <toby.mao@gmail.com>
2021-02-01 20:35:58 -0800

    add 1877 logos

--- scripts/generate_logos.rb
+++ scripts/generate_logos.rb
@@ -12,23 +12,23 @@ def generate_logos(game_title, simple=false)
   game = game_class.new(players)
 
   # Regenerate to avoid corps that are hidden
-  corporations = game.init_corporations(game.stock_market)
+  corporations = game.send(:init_corporations, game.stock_market)
 
   corporations.each do |corp|
     next if simple && !corp.simple_logo
 
     filename =
       if simple
-        "public/#{corp.simple_logo}.alt.svg"
+        "public#{corp.simple_logo}"
       else
-        "public/#{corp.logo}"
+        "public#{corp.logo}"
       end
 
     if File.exist?(filename)
       puts "File #{filename} already exists"
     else
       svg = <<-SVG
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><circle cx="4" cy="4" r="4" fill="#{corp.color}"/><text x="4" y="5" text-anchor="middle" font-weight="700" font-size="3" font-family="Lato" fill="#{corp.text_color}">#{corp.id}</text></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><circle cx="4" cy="4" r="4" fill="#{corp.color}"/><text x="4" y="5" text-anchor="middle" font-weight="700" font-size="3" font-family="Lato" fill="#{corp.text_color}">#{corp.id.gsub('&', '&amp;')}</text></svg>
       SVG
       File.write(filename, svg)
       puts "Generated #{filename}"
@@ -37,3 +37,5 @@ def generate_logos(game_title, simple=false)
 
   end
 end
+
+generate_logos('1877')
