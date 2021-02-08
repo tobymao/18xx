8b8ae92467f0bdc5c99d315f7bbbd1c543e1cff3
Steve <roseundy@users.noreply.github.com>
2021-02-08 11:50:45 -0700

    move 1860 to production; 18Mag to beta (#3746)

--- lib/engine/game/g_18_mag.rb
+++ lib/engine/game/g_18_mag.rb
@@ -34,7 +34,7 @@ module Engine
       GAME_PUBLISHER = :lonny_games
       GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18Mag'
 
-      DEV_STAGE = :alpha
+      DEV_STAGE = :beta
 
       EBUY_PRES_SWAP = false # allow presidential swaps of other corps when ebuying
       EBUY_OTHER_VALUE = false # allow ebuying other corp trains for up to face
