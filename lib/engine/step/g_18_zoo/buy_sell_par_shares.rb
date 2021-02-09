b9a6113513ad31c849651434d55a4a02e859cd1e
crusso <crusso@hotels.com>
2021-02-09 17:03:41 +0100

    Adding tests. Making the SR BuySell step a blocking when powers can be choose or zoo ticket companies can be sold. Locking 9 and 12 par on green and brown phase. Adding "near_family" logic. Handling companies "Leprechaun pot of gold", "Too much responsibility"

--- lib/engine/step/g_18_zoo/buy_sell_par_shares.rb
+++ lib/engine/step/g_18_zoo/buy_sell_par_shares.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../buy_sell_par_shares'
+require_relative 'sell_company'
+require_relative 'choose_power'
 
 module Engine
   module Step
     module G18ZOO
       class BuySellParShares < BuySellParShares
+        include SellCompany
+        include ChoosePower
+
         def actions(entity)
           return [] unless entity == current_entity
           return ['sell_shares'] if must_sell?(entity)
@@ -15,6 +20,8 @@ module Engine
           actions << 'par' if can_ipo_any?(entity)
           actions << 'buy_company' unless purchasable_unsold_companies.empty?
           actions << 'sell_shares' if can_sell_any?(entity)
+          actions << 'sell_company' if can_sell_any_companies?(entity)
+          actions << 'choose' if choice_available?(entity)
           actions << 'pass' unless actions.empty?
           actions
         end
@@ -27,6 +34,33 @@ module Engine
           super
 
           @game.available_companies.delete(action.company)
+          @game.apply_custom_ability(action.company)
+        end
+
+        def get_par_prices(_entity, _corp)
+          super
+            .reject { |p| p.price == 9 && !@game.phase.tiles.include?(:green) }
+            .reject { |p| p.price == 12 && !@game.phase.tiles.include?(:brown) }
+        end
+
+        def can_buy?(entity, bundle)
+          super && more_than_80_only_from_market(entity, bundle)
+        end
+
+        def more_than_80_only_from_market(entity, bundle)
+          corporation = bundle.corporation
+          ipo_share = corporation.shares[0]
+          is_ipo_share = ipo_share == bundle
+          percent = entity.percent_of(corporation)
+          !is_ipo_share || percent < 80
+        end
+
+        def log_pass(entity)
+          return @log << "#{entity.name} passes" if @current_actions.empty?
+          return if bought? && sold?
+
+          action = bought? ? 'to sell' : 'to buy'
+          @log << "#{entity.name} declines #{action} shares"
         end
 
         private
