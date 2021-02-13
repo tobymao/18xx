23f1163cd8ed7420ba606174ba0371c0fa93e047
yzemaze <git@yzemaze.de>
2021-02-13 14:41:59 +0100

    styling

--- assets/app/view/game/async.rb
+++ assets/app/view/game/async.rb
@@ -10,6 +10,7 @@ module View
 
       def render_content
         children = [
+          h(:h2, 'Programmed Actions'),
           h(:p, 'Programmed actions allows you to preprogram your moves ahead of time.'),
           h(:p, 'On asynchronous games this can shorten a game considerably.'),
           h(:p, 'Please note, these are not to be considered secret from other players and when activated are logged'),
@@ -29,7 +30,13 @@ module View
           children << h(:p, 'No programmed actions are presently available for this game')
         end
 
-        h(:div, children)
+        props = {
+          style: {
+            maxWidth: '40rem',
+          },
+        }
+
+        h(:div, props, children)
       end
 
       def sender
@@ -47,7 +54,7 @@ module View
       end
 
       def render_buy_shares
-        children = [h(:div, { style: { fontWeight: 'bold', margin: '2vmin 0' } }, 'Program buy shares till float')]
+        children = [h(:h3, 'Program buy shares till float')]
         children << h(:div,
                       'Warning! At present this does not take into account what other players do'\
                       ', we suggest not enabling after the first stock round.')
@@ -55,8 +62,7 @@ module View
         # @todo: later this will support buying to a certain percentage
         floatable = @game.corporations.select { |corp| corp.ipoed && !corp.floated? }
         if floatable.empty?
-          children << h(:div, { style: { fontWeight: 'bold' } },
-                        'No corporations are ipoed but not floated, cannot program!')
+          children << h('div.bold', 'No corporations are ipoed but not floated, cannot program!')
         else
           values = floatable.map do |entity|
             h(:option, { attrs: { value: entity.name } }, entity.full_name)
@@ -79,8 +85,7 @@ module View
       end
 
       def render_disable(enabled)
-        children = [h(:div, { style: { fontWeight: 'bold', margin: '2vmin 0' } },
-                      "Disable program '#{enabled.class.print_name}'")]
+        children = [h(:h3, "Disable program '#{enabled.class.print_name}'")]
         children << render_button("Disable '#{enabled.class.print_name}'") { disable }
         children
       end
