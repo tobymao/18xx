2a49f172826975adc9dca2dd5d4294b90fc40d32
yzemaze <git@yzemaze.de>
2021-02-14 21:32:34 +0100

    fix history_link regression due to hotkeys

--- assets/app/view/game/history_controls.rb
+++ assets/app/view/game/history_controls.rb
@@ -20,7 +20,7 @@ module View
         style_extra = { padding: '0.2rem 0.5rem', width: '100%' }
 
         unless cursor&.zero?
-          divs << history_link('|<', 'Start', 'Home', 0, style_extra, true)
+          divs << history_link('|<', 'Start', 0, style_extra, true, 'Home')
 
           last_round =
             if cursor == @game.raw_actions.size
@@ -28,8 +28,8 @@ module View
             else
               @game.round_history[-1]
             end
-          divs << history_link('<<', 'Previous Round', 'PageUp', last_round,
-                               { gridColumnStart: '3', **style_extra }, true) if last_round
+          divs << history_link('<<', 'Previous Round', last_round,
+                               { gridColumnStart: '3', **style_extra }, true, 'PageUp') if last_round
 
           prev_action =
             if @game.exception
@@ -39,18 +39,18 @@ module View
             else
               @num_actions - 1
             end
-          divs << history_link('<', 'Previous Action', 'ArrowLeft', prev_action,
-                               { gridColumnStart: '4', **style_extra }, true)
+          divs << history_link('<', 'Previous Action', prev_action,
+                               { gridColumnStart: '4', **style_extra }, true, 'ArrowLeft')
         end
 
         if cursor && !@game.exception
-          divs << history_link('>', 'Next Action', 'ArrowRight', cursor + 1 < @num_actions ? cursor + 1 : nil,
-                               { gridColumnStart: '5', **style_extra }, true)
+          divs << history_link('>', 'Next Action', cursor + 1 < @num_actions ? cursor + 1 : nil,
+                               { gridColumnStart: '5', **style_extra }, true, 'ArrowRight')
           store(:round_history, @game.round_history, skip: true) unless @round_history
           next_round = @round_history[@game.round_history.size]
-          divs << history_link('>>', 'Next Round', 'PageDown', next_round,
-                               { gridColumnStart: '6', **style_extra }, true) if next_round
-          divs << history_link('>|', 'Current Action', 'End', nil, { gridColumnStart: '7', **style_extra }, true)
+          divs << history_link('>>', 'Next Round', next_round,
+                               { gridColumnStart: '6', **style_extra }, true, 'PageDown') if next_round
+          divs << history_link('>|', 'Current Action', nil, { gridColumnStart: '7', **style_extra }, true, 'End')
         end
 
         props = {
