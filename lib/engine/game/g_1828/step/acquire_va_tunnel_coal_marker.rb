1c6e0047b98ec37b4f02749df52b2e61eea3e894
Chris Rericha <crericha@gmail.com>
2021-02-17 23:13:51 -0500

    1828: Convert to new game bundle format.

--- lib/engine/game/g_1828/step/acquire_va_tunnel_coal_marker.rb
+++ lib/engine/game/g_1828/step/acquire_va_tunnel_coal_marker.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module Engine
-  module Step
+  module Game
     module G1828
       module AcquireVaTunnelCoalMarker
         def process_lay_tile(action)
-          @game.acquire_va_tunnel_coal_marker(action.entity) if action.hex.id == Engine::Game::G1828::VA_TUNNEL_HEX
+          if action.hex.id == Engine::Game::G1828::Game::VA_TUNNEL_HEX
+            @game.acquire_va_tunnel_coal_marker(action.entity)
+          end
 
           super
         end
