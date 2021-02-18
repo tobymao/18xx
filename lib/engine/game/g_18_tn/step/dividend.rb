d7cfb6474f3c1cc8e916b1da5fcda5a5610d643d
R. Ryan Driskel <ryandriskel@gmail.com>
2021-02-17 20:28:26 -0700

    Migrate 18TN to new structure (#3984)
    Migrate 18TN to new structure

---
+++ lib/engine/game/g_18_tn/step/dividend.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/dividend'
+
+module Engine
+  module Game
+    module G18TN
+      module Step
+        class Dividend < Engine::Step::Dividend
+          def process_dividend(action)
+            super
+
+            return unless (civil_war = @game.abilities(action.entity, :civil_war))
+
+            @log << "#{action.entity.name} resolves Civil War"
+            civil_war.use!
+          end
+
+          def change_share_price(entity, payout)
+            return super unless civil_war_effect_with_single_train?(entity)
+
+            @log << "#{entity.name}'s share price unchanged - Civil War event, owning just one train (see rule §5.1)"
+          end
+
+          def dividend_options(entity)
+            return super unless civil_war_effect_with_single_train?(entity)
+
+            { withhold: { corporation: 0, per_share: 0, divs_to_corporation: 0 } }
+          end
+
+          def log_run_payout(entity, kind, revenue, action, payout)
+            return super unless civil_war_effect_with_single_train?(entity)
+
+            @log << "#{entity.name}'s run is ignored due to Civil War"
+          end
+
+          def dividend_types
+            return super unless civil_war_effect_with_single_train?(@game.current_entity)
+
+            [:withhold]
+          end
+
+          private
+
+          def civil_war_effect_with_single_train?(corporation)
+            corporation.trains.one? && Array(@game.abilities(corporation, :civil_war)).any?
+          end
+        end
+      end
+    end
+  end
+end
