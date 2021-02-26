7543f1a6eb45e62cefa4c1dfa81bea6843a990e6
Michael Brandt <michaelbrandt5@gmail.com>
2021-02-25 19:14:13 -0700

    Clean up for moving games to new bundle structure
    * remove player_range method, just use the constant
    * remove move_game script/rake task
    * remove Game's load_from_json
    * clean up logic using old game structure
    * fix path for 18FL operating round
    
    [Fixes #3956]

--- assets/app/view/tiles_page.rb
+++ assets/app/view/tiles_page.rb
@@ -106,7 +106,7 @@ module View
       game_class = load_game_class(game_title)
       return [] unless game_class
 
-      players = Engine.player_range(game_class).max.times.map { |n| "Player #{n + 1}" }
+      players = game_class::PLAYER_RANGE.max.times.map { |n| "Player #{n + 1}" }
       game = game_class.new(players)
 
       id, rotation = hex_or_tile_id.split('-')
@@ -135,7 +135,7 @@ module View
     end
 
     def map_hexes_and_tile_manifest_for(game_class)
-      players = Engine.player_range(game_class).max.times.map { |n| "Player #{n + 1}" }
+      players = game_class::PLAYER_RANGE.max.times.map { |n| "Player #{n + 1}" }
       game = game_class.new(players)
 
       # map_tiles: hash; key is hex ID, value is the Tile there
