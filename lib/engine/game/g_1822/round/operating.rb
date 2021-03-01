bb4cf417f0e3d138592ff701fdf2174fcd2c2440
Magnus Fält <magnus@enfs.se>
2021-03-01 14:21:40 +0100

    [1822] Cert limit and corporation reorder

---
+++ lib/engine/game/g_1822/round/operating.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../../../round/operating'
+
+module Engine
+  module Game
+    module G1822
+      module Round
+        class Operating < Engine::Round::Operating
+          def recalculate_order
+            # Selling shares may have caused the major corporations that haven't operated yet
+            # to change order.
+            index = @entity_index + 1
+            if index < @entities.size - 1
+              # Find the first major corporation after current operating entity.
+              index += @entities[index..-1].find_index { |c| c.type == :major } || 0
+              @entities[index..-1] = @entities[index..-1].sort
+            end
+
+            @entities.pop while @entities.last&.corporation? && @entities.size > index
+          end
+        end
+      end
+    end
+  end
+end
