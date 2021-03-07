befdf54402d0c0c44ebce7be15bb23ea7eb7e550
Chris Rericha <crericha@gmail.com>
2021-03-07 18:05:35 -0500

    Change general case to OO tile layer deciding token locations. Override for 1828, where the token owner decides. #4040

--- lib/engine/game/g_1828/step/track.rb
+++ lib/engine/game/g_1828/step/track.rb
@@ -10,11 +10,16 @@ module Engine
         class Track < Engine::Step::Track
           include AcquireVaTunnelCoalMarker
 
-          def update_token!(action, _entity, tile, _old_tile)
-            # Nothing to update if blocking tokens have been laid
-            return if action.hex.id == 'E15' && @game.blocking_token?(tile.cities.flat_map(&:tokens).find(&:itself))
+          def update_token!(action, entity, tile, old_tile)
+            if action.hex.id == 'E15' && (token = tile.cities.flat_map(&:tokens).find(&:itself))
+              # If there are blocking tokens in both cities, no decisions to be made
+              return if @game.blocking_token?(token)
 
-            super
+              # Otherwise, the token owner gets to decide the token location
+              entity = token.corporation
+            end
+
+            super(action, entity, tile, old_tile)
           end
         end
       end
