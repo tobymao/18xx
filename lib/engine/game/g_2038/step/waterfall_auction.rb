07e1fc68b64c6c1bc7955ae1f52e94c3d106e8ac
jason <jason@johnson-net.ch>
2021-03-08 23:33:16 +0100

    temp fix for TSI handling

--- lib/engine/game/g_2038/step/waterfall_auction.rb
+++ lib/engine/game/g_2038/step/waterfall_auction.rb
@@ -10,6 +10,23 @@ module Engine
           def buy_company(player, company, price)
             super
 
+            # TODO: We should probably just remove the action from ST
+            # TODO: and do the below without it, but we need to find
+            # TODO: out how to get the president shares first. :)
+
+            if company.sym == 'ST'
+              @round.companies_pending_par.delete(company)
+              share_price = @game.stock_market.par_prices.find { |pp| pp.price == 100 }
+
+              @game.abilities(company, :shares).shares.each do |share|
+                next unless share.president
+
+                @game.stock_market.set_par(share.corporation, share_price)
+                @game.share_pool.buy_shares(player, share.corporation.shares.first, exchange: :free)
+                @game.after_par(share.corporation)
+              end
+            end
+
             return unless company.instance_of?(G2038::Company)
 
             company.close!   # remove our wrapper which was added in super.buy_company
