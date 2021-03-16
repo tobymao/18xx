147b766bb31a8b2cbf9be12bd99440e5f9fb7990
daniel-sousa-me <github@daniel.sousa.me>
2021-03-16 04:46:24 +0000

    1870: Fixes that affect Ruby, but not Opal

--- lib/engine/game/g_1870/game.rb
+++ lib/engine/game/g_1870/game.rb
@@ -828,11 +828,13 @@ module Engine
           destination_stop.route_revenue(route.phase, route.train)
         end
 
-        def sell_shares_and_change_price(bundle, _allow_president_change: true, _swap: nil)
+        # rubocop:disable Lint/UnusedMethodArgument
+        def sell_shares_and_change_price(bundle, allow_president_change: true, swap: nil)
           @round.sell_queue << bundle
 
           @share_pool.sell_shares(bundle)
         end
+        # rubocop:enable Lint/UnusedMethodArgument
 
         def legal_tile_rotation?(_entity, hex, tile)
           return true unless abilities(river_company, :blocks_partition)
