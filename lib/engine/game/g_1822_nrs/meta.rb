dc118bdd7e7632d975b2ca844952dc57aafddfff
Magnus Fält <magnus@enfs.se>
2021-03-19 08:19:38 +0100

    [1822NRS] Updated variant

--- lib/engine/game/g_1822_nrs/meta.rb
+++ lib/engine/game/g_1822_nrs/meta.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../meta'
+require_relative '../g_1822/meta'
 
 module Engine
   module Game
@@ -18,6 +19,7 @@ module Engine
         GAME_PUBLISHER = :all_aboard_games
         GAME_RULES_URL = 'https://boardgamegeek.com/filepage/219065/1822-railways-great-britain-rules'
         GAME_TITLE = '1822NRS'
+        GAME_IS_VARIANT_OF = G1822::Meta
 
         PLAYER_RANGE = [3, 7].freeze
       end
