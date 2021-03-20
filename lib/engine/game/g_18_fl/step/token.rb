bd98d2dbc03856f4d5a3fc983edad4443805e8c0
ventusignis <mgilzinger@google.com>
2021-03-19 20:20:29 -0400

    Use node.walk for 18FL token cost calculation

--- lib/engine/game/g_18_fl/step/token.rb
+++ lib/engine/game/g_18_fl/step/token.rb
@@ -8,6 +8,7 @@ module Engine
       module Step
         class Token < Engine::Step::Token
           ACTIONS = %w[place_token hex_token pass].freeze
+          INFINITE_DISTANCE = 99_999
 
           def actions(entity)
             return [] unless entity == current_entity
@@ -62,69 +63,23 @@ module Engine
           end
 
           def token_cost_override(entity, city_hex, _slot, token, _tokener)
-            hex = city_hex.respond_to?(:city?) ? city_hex.hex : city_hex
-            token.price * distance_to_station(entity, hex)
+            node = city_hex.respond_to?(:city?) ? city_hex : city_hex.tile.towns.first
+            min_distance = INFINITE_DISTANCE
+            goal_cities = entity.tokens.select(&:city).map(&:city)
+
+            node.walk(corporation: entity) do |path, visited_paths|
+              if goal_cities.include?(path.city)
+                # minus one to account for the tokened hex getting included in the count
+                distance = visited_paths.select { |_k, v| v }.keys.map(&:hex).uniq.size - 1
+                min_distance = [min_distance, distance].min
+              end
+            end
+
+            token.price * min_distance
           end
 
           def tokened(hex, entity)
             hex.tile.icons.any? { |i| i.name == entity.id }
-          end
-
-          # How far is the start_hex from one of the corporation's stations?
-          # This is by track, not as the crow flies.
-          # start is either a city or hex (plain track or town)
-          def distance_to_station(corporation, start)
-            goal_hexes = corporation.tokens.select(&:city).map { |t| [t.city.hex, t.city] }
-            distance = 0
-            visited_hexes = []
-            start_hexes = if start.is_a?(Engine::Part::City)
-                            [[start.hex, start]]
-                          else # Hex
-                            [[start, start.tile.cities.first]]
-                          end
-            until start_hexes.empty?
-              return distance unless (start_hexes & goal_hexes).empty?
-
-              distance += 1
-              hexes_to_visit = start_hexes
-              start_hexes = []
-              hexes_to_visit.each do |hex_c|
-                visited_hexes << hex_c
-                valid_neighbors(hex_c).each do |e, neighbor|
-                  # Ignore this neighbor if they don't connect to each other
-                  next unless hex_c.first.tile.exits.include?(e) && neighbor.tile.exits.include?((e + 3) % 6)
-
-                  neighbor_cities((e + 3) % 6, neighbor).each do |city|
-                    neighbor_hc = [neighbor, city]
-                    # Don't revisit hexes
-                    return distance if goal_hexes.include?(neighbor_hc)
-                    next if visited_hexes.include?(neighbor_hc) || hexes_to_visit.include?(neighbor_hc)
-
-                    start_hexes << neighbor_hc unless neighbor_hc[1]&.blocks?(corporation)
-                  end
-                end
-              end
-            end
-            # Didn't return early, couldn't find a station connected.
-            # This shouldn't happen? If this is called the spot is tokenable
-            # and if the spot is tokenable it is visible from an existing station?
-            raise GameError, 'Distance is uncalculable'
-          end
-
-          # Which of the hex-city neighbors are reachable directly?
-          def valid_neighbors(hex_c)
-            return hex_c.first.neighbors unless hex_c.first.tile.cities.count > 1
-
-            valid_edges = hex_c.last.exits
-            hex_c.first.neighbors.select { |edge, _| valid_edges.include?(edge) }
-          end
-
-          # What cities in this hex can we reach from this incoming edge?
-          def neighbor_cities(incoming_edge, hex)
-            return [nil] if hex.tile.cities.empty?
-
-            reachable_cities = hex.tile.cities.select { |c| c.exits.include?(incoming_edge) }
-            reachable_cities.empty? ? [nil] : reachable_cities
           end
         end
       end
