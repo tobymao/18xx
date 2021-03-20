d67449421547ff8c939046d77ff9913a8ef95859
Magnus Fält <magnus@enfs.se>
2021-03-20 11:05:11 +0100

    [1822] Destination token

--- lib/engine/game/g_1822/game.rb
+++ lib/engine/game/g_1822/game.rb
@@ -3733,8 +3733,7 @@ module Engine
 
         def place_destination_token(entity, hex, token)
           city = hex.tile.cities.first
-          cheater_slot = city.slots
-          city.place_token(entity, token, free: true, check_tokenable: false, cheater: cheater_slot)
+          city.place_token(entity, token, free: true, check_tokenable: false, cheater: city.slots)
           hex.tile.icons.reject! { |icon| icon.name == "#{entity.id}_destination" }
 
           ability = entity.all_abilities.find { |a| a.type == :destination }
