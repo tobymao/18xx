d7452c422ea0808beba8502a4f94ef009740ec19
jason <jason@johnson-net.ch>
2021-03-24 12:33:07 +0100

    properly handle purchase of minors

--- lib/engine/game/g_2038/step/waterfall_auction.rb
+++ lib/engine/game/g_2038/step/waterfall_auction.rb
@@ -11,12 +11,25 @@ module Engine
             super
           end
 
+          def may_purchase?(company)
+            return false unless super
+            return true if @purchasing_first_minor
+            is_minor = @game.minors.find { |m| m.id == company.id }
+            return !is_minor
+          end
+
           def min_bid(company)
             return unless company
             return company.min_bid if may_purchase?(company)
     
             high_bid = highest_bid(company)
             high_bid ? high_bid.price + min_increment : company.min_bid
+          end
+
+          def placement_bid(bid)
+            @purchasing_first_minor = bid.company && bid.company == @companies.first
+            super
+            @purchasing_first_minor = false
           end
         end
       end
