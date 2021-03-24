86080c608bad8406614e3c85f661357a20bb728d
jason <jason@johnson-net.ch>
2021-03-24 11:30:53 +0100

    min bid is actually company price

--- lib/engine/game/g_2038/step/waterfall_auction.rb
+++ lib/engine/game/g_2038/step/waterfall_auction.rb
@@ -10,6 +10,14 @@ module Engine
           def all_passed!
             super
           end
+
+          def min_bid(company)
+            return unless company
+            return company.min_bid if may_purchase?(company)
+    
+            high_bid = highest_bid(company)
+            high_bid ? high_bid.price + min_increment : company.min_bid
+          end
         end
       end
     end
