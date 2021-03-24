51edb26723fb4d0fec5228a36d1030368df29dae
jason <jason@johnson-net.ch>
2021-03-24 14:18:46 +0100

    fixed bug with label name during auction

--- lib/engine/game/g_2038/step/waterfall_auction.rb
+++ lib/engine/game/g_2038/step/waterfall_auction.rb
@@ -7,26 +7,24 @@ module Engine
     module G2038
       module Step
         class WaterfallAuction < Engine::Step::WaterfallAuction
-          def all_passed!
-            super
-          end
 
           def may_purchase?(company)
             return false unless super
             return true if @purchasing_first_minor
-            return !is_minor?(company)
+
+            !is_minor?(company)
           end
 
           def min_bid(company)
             return unless company
             return company.min_bid if may_purchase?(company)
-    
+
             high_bid = highest_bid(company)
             high_bid ? high_bid.price + min_increment : company.min_bid
           end
 
-          def bid_str(company)            
-            company && is_minor?(company) && company == @companies.first ? 'Buy' : 'Place Bid'
+          def bid_str(company)
+            !auctioning && company && is_minor?(company) && company == @companies.first ? 'Buy' : 'Place Bid'
           end
 
           def placement_bid(bid)
