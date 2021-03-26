5bef66dcd1ecec1d276b7f000ed545422fb78337
Michael Brandt <michaelbrandt5@gmail.com>
2021-03-26 09:38:16 -0600

    split out map and entities for 1846 and 18LA (#4771)
    * also move tiles used only by 1846 out of common tile config into 1846's game
      class
    * change 18LA's tiles to inherit from 1846's and override the differences
    * move CERT_LIMIT out of 2p and LA code, all into 1846 base code
    * remove constants from 1846 2p Meta that can be inherited from 1846
    * move `include` statements to top of game classes (TODO: enable the cop
      Layout/ClassStructure)

--- lib/engine/game/g_1846_two_player_variant/meta.rb
+++ lib/engine/game/g_1846_two_player_variant/meta.rb
@@ -8,21 +8,19 @@ module Engine
     module G1846TwoPlayerVariant
       module Meta
         include Game::Meta
+        include G1846::Meta
 
-        DEV_STAGE = :production
         DEPENDS_ON = '1846'
 
-        GAME_DESIGNER = 'Thomas Lehmann'
-        GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/1846'
-        GAME_LOCATION = 'Midwest, USA'
-        GAME_PUBLISHER = %i[gmt_games golden_spike].freeze
+        GAME_ALIASES = ['1846 2p'].freeze
+        GAME_IS_VARIANT_OF = G1846::Meta
         GAME_RULES_URL = {
-          '1846 Rules' => 'https://s3-us-west-2.amazonaws.com/gmtwebsiteassets/1846/1846-RULES-GMT.pdf',
+          '1846 Rules' => G1846::Meta::GAME_RULES_URL,
           '1846 2p Variant Rules' => 'https://boardgamegeek.com/thread/1616729/draft-2-player-1846-rules-game-designer',
         }.freeze
+        GAME_SUBTITLE = nil
         GAME_TITLE = '1846 2p Variant'
-        GAME_IS_VARIANT_OF = G1846::Meta
-        GAME_ALIASES = ['1846 2p'].freeze
+        GAME_VARIANTS = [].freeze
 
         PLAYER_RANGE = [2, 2].freeze
       end
