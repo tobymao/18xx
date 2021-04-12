b84f870e7e0a2b50a6acf15f90970ec7225426ad
crusso <crusso@expediagroup.com>
2021-04-12 18:14:51 +0200

    Ensure Whatsup block "new-phase" bonus if done on first train of new phas

--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -399,6 +399,24 @@ module Engine
           expect(game.process_action(action).exception).to be_a(GameError)
         end
       end
+
+      describe 18 do
+        it 'buying a new train after whatsup (on first train on new phase) must not give "new-phase" bonus' do
+          game = game_at_action(game_file)
+          corporation = game.corporation_by_id('GI')
+          action = {
+            'type' => 'buy_train',
+            'entity' => 'GI',
+            'entity_type' => 'corporation',
+            'train' => '3S-1',
+            'price' => 12,
+            'variant' => '3S',
+          }
+          expect(corporation.share_price.price).to eq(7)
+          game.process_action(action)
+          expect(corporation.share_price.price).to eq(8)
+        end
+      end
     end
   end
 end
