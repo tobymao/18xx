60fb43cce19117da8d4ae6a6ecbac0c2aa2bebcd
Chris Rericha <crericha@gmail.com>
2021-04-27 03:51:53 -0400

    18NY: New step for stagecoach token exchange choice.

---
+++ lib/engine/game/g_18_ny/step/stagecoach_exchange.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/base'
+
+module Engine
+  module Game
+    module G18NY
+      module Step
+        class StagecoachExchange < Engine::Step::Base
+
+          def description
+            'Exchange Token for Stagecoach Token'
+          end
+
+          def actions(entity)
+            return %w[choose pass] if active?
+
+            []
+          end
+
+          def active?
+            return false if @game.stagecoach_token.nil?
+            return false if @passed && !@game.privates_closed
+
+            corporation = @game.stagecoach_token&.corporation
+            # Remove token if privates are closed and it can't be exchanged
+            if @game.privates_closed && (corporation.nil? || !corporation.next_token)
+              remove_stagecoach_token
+            end
+
+            (@game.stagecoach_token && @game.privates_closed) ||
+              (corporation == @game.round.current_operator && corporation.next_token)
+          end
+
+          def active_entities
+            [@game.stagecoach_token.corporation]
+          end
+
+          def choice_available?(entity)
+            entity == @game.stagecoach_token.corporation
+          end
+
+          def choice_name
+            'Exchange Stagecoach token'
+          end
+
+          def choices
+            %w[Exchange]
+          end
+
+          def process_choose(action)
+            @game.stagecoach_token.swap!(action.entity.next_token)
+            remove_stagecoach_token
+            pass!
+          end
+
+          def process_pass(action)
+            remove_stagecoach_token if @game.privates_closed
+            super
+          end
+
+          def remove_stagecoach_token
+            @game.stagecoach_token.destroy!
+            @game.stagecoach_token = nil
+          end
+        end
+      end
+    end
+  end
+end
