924a0ea843899a016b62f0964a76090d5499b502
roseundy <steve@roseundy.net>
2021-05-02 12:52:41 -0600

    18Mag: move to production

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -17,7 +17,7 @@ module View
 
     def render_notification
       message = <<~MESSAGE
-        <p>18Ireland is now in alpha.</p>
+        <p>18Ireland is now in alpha. 18Mag is now in production.</p>
         <p>Please submit problem reports and make suggestions for improvements on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>.</p>
         <p>The <a href='https://github.com/tobymao/18xx/wiki'>18xx.games Wiki</a> has rules, maps,
