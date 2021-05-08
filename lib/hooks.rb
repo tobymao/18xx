700b485ba248470c2c8030ed1f10171127eacf01
tobymao <toby.mao@gmail.com>
2021-05-08 13:45:55 -0700

    fix

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -8,7 +8,7 @@ module Hooks
   def self.send(user, message)
     return unless ENV['RACK_ENV'] == 'production'
 
-    uri = URI.parse(user.settings['webhook_url'] || ENV['SLACK_WEBOOK_URL'])
+    uri = URI.parse(user.settings['webhook_url'] || ENV['SLACK_WEBHOOK_URL'])
     req = Net::HTTP::Post.new(uri)
     req.content_type = 'application/json'
 
