ee8f8aef118c78f5436af80063eaa8fbd5f3d41b
Chris Shaffer <ChrisTheCat@users.noreply.github.com>
2021-05-10 06:00:31 -0700

    Link to official English language rules

--- lib/engine/game/g_18_mag/meta.rb
+++ lib/engine/game/g_18_mag/meta.rb
@@ -15,7 +15,7 @@ module Engine
         GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18Mag'
         GAME_LOCATION = 'Hungary'
         GAME_PUBLISHER = :lonny_games
-        GAME_RULES_URL = 'https://www.lonny.at/games/18magyarorsz%C3%A1g/'
+        GAME_RULES_URL = 'https://boardgamegeek.com/filepage/222188/18mag-rules-english'
 
         PLAYER_RANGE = [2, 6].freeze
       end
