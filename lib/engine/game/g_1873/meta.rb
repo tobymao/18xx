dea436828b1c2cf366c038ed53e7f412c6d80aa9
Steve <roseundy@users.noreply.github.com>
2021-05-12 19:34:00 -0600

    1873: take to beta (#5324)

--- lib/engine/game/g_1873/meta.rb
+++ lib/engine/game/g_1873/meta.rb
@@ -8,7 +8,7 @@ module Engine
       module Meta
         include Game::Meta
 
-        DEV_STAGE = :alpha
+        DEV_STAGE = :beta
 
         GAME_DESIGNER = 'Klaus Kiermeier'
         GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/Harzbahn-1873'
