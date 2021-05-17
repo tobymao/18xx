1b42174291de1b15b9c6e08267a5a26e3331fef9
roseundy <steve@roseundy.net>
2021-05-17 14:03:01 -0600

    1862: take to alpha

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -18,7 +18,7 @@ module View
     def render_notification
       message = <<~MESSAGE
         <p>We now support Slack/Discord notifications. Learn how to use them <a href='https://github.com/tobymao/18xx/wiki/Notifications'>here</a>.</p>
-        <p>18Ireland and 18ZOO are now in alpha. 18FL, Hazbahn 1873 and 1870 are now in beta.</p>
+        <p>1862, 18Ireland and 18ZOO are now in alpha. 18FL, Hazbahn 1873 and 1870 are now in beta.</p>
         <p>Prototype games are now more clearly labelled, 1849: Kingdom of the Two Sicilies is production.</p>
         <p>Please submit problem reports and make suggestions for improvements on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>.</p>
