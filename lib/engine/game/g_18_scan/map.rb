04280469e1bf2ce857ed9c8f9503d811d80d3fe3
philcampeau <philcampeau@gmail.com>
2021-05-24 15:16:39 -0400

    Update map.rb

--- lib/engine/game/g_18_scan/map.rb
+++ lib/engine/game/g_18_scan/map.rb
@@ -19,8 +19,7 @@ module Engine
               'count' => 1,
               'color' => 'green',
               'code' =>
-              'city=revenue:50,loc:center;path=a:0,b:_2;'\
-              'path=a:_2,b:_1;path=a:_0,b:_1;label=COP',
+              'city=revenue:50;path=a:0,b:_0;path=a:2,b:_0;path=a:4,b:_0;label=COP',
             },
           '141' => 3,
           '142' => 3,
@@ -33,8 +32,8 @@ module Engine
               'count' => 1,
               'color' => 'yellow',
               'code' =>
-              'city=revenue:30,loc:center;town=revenue:10,loc:0;path=a:0,b:_2;'\
-              'path=a:_2,b:_1;path=a:_0,b:_1;label=COP;upgrade=cost:40',
+              'city=revenue:30,loc:center;town=revenue:10,loc:0;path=a:0,b:_1;'\
+              'path=a:_1,b:_0;label=COP;upgrade=cost:40',
             },
           '544' => 3,
           '545' => 3,
@@ -62,7 +61,7 @@ module Engine
           'E4' => 'Kristiansand',
           'F5' => 'Götenborg',
           'G4' => 'Oslo',
-          'G8' => 'Norrköping',
+          'G6' => 'Norrköping',
           'J5' => 'Gävle',
           'K2' => 'Trondheim',
           'K6' => 'Stockholm',
@@ -92,7 +91,7 @@ module Engine
             ['Q8'] => 'city=revenue:yellow_30|green_50|brown_80,slots:1;path=a:2,b:_0,terminal:1',
             ['T1'] => 'town=revenue:yellow_10|green_50|brown_10;path=a:0,b:_0;path=a:1,b:_0',
           },
-          lightBlue: {
+          blue: {
             ['L7'] => 'path=a:2,b:3,track:narrow',
           },
           white: {
@@ -100,19 +99,19 @@ module Engine
             %w[F1 H1 J1 N1 P1 G2 I2 M2 O2 Q2 F3 H3 J3] => 'upgrade=cost:60,terrain:mountain',
             %w[G6 J5 L3 P7 R3] => 'town=revenue:0',
             %w[G4 N7 O6 S2] => 'city=revenue:0',
-            %w[K2 R1] => 'city=revenue:0;upgrade=cost:60,terrain:mountain',
-            %w[F5 M6] => 'city=revenue:0;border=edge:1,type:impassable;border=edge:2,type:impassable',
-            ['C6'] => 'city=revenue:0;town=revenue:0;upgrade=cost:40,terrain:water',
+            ['K2'] => 'city=revenue:0;upgrade=cost:60,terrain:mountain',
+            ['M6'] => 'city=revenue:0;border=edge:1,type:impassable;border=edge:2,type:impassable',
+            ['C6'] => 'city=revenue:0;town=revenue:0,loc:1.5;upgrade=cost:40,terrain:water',
             ['D3'] => 'city=revenue:0;border=edge:3,type:impassable',
             ['D5'] => 'city=revenue:0;border=edge:3,type:impassable;border=edge:4,type:impassable;border=edge:5,type:impassable',
             ['D7'] => 'city=revenue:0;upgrade=cost:40,terrain:water',
             ['E2'] => 'city=revenue:0;border=edge:0,type:impassable',
-            ['E4'] => 'town=revenue:0;border=edge:0,type:impassable,border=edge:5,type:impassable',
+            ['E4'] => 'town=revenue:0;border=edge:0,type:impassable;border=edge:5,type:impassable',
             ['F5'] => 'city=revenue:0;border=edge:1,type:impassable;border=edge:2,type:impassable',
-            ['L5'] => 'city=revenue:0;border=edge:4,type:impassable;border=edge:5,type:impassable',
+            ['L5'] => 'border=edge:4,type:impassable;border=edge:5,type:impassable',
             ['M4'] => 'border=edge:5,type:impassable',
             ['N5'] => 'border=edge:1,type:impassable;border=edge:2,type:impassable;border=edge:3,type:impassable',
-            ['O6'] => 'city=revenue:0;border=edge:0,type:impassable;border=edge:5,type:impassable',
+            ['O4'] => 'city=revenue:0;border=edge:0,type:impassable;border=edge:5,type:impassable',
             ['P5'] => 'border=edge:2,type:impassable;border=edge:3,type:impassable',
             ['Q4'] => 'border=edge:0,type:impassable;border=edge:4,type:impassable;border=edge:5,type:impassable',
             ['R1'] => 'city=revenue:0;upgrade=cost:60,terrain:mountain',
@@ -120,7 +119,7 @@ module Engine
             ['S4'] => 'town=revenue:0;border=edge:1,type:impassable',
           },
           yellow: {
-            ['K6'] => 'city=revenue:30;city=revenue:30;path=a:1,b:_0;path=a:2,b:_1;border=edge:4,type:impassable',
+            ['K6'] => 'city=revenue:30,loc:0.5;city=revenue:30,loc:2.5;path=a:1,b:_0;path=a:2,b:_1;border=edge:4,type:impassable',
           },
         }.freeze
         # rubocop:enable Layout/LineLength
