a8ec30e03114e5450c7e365cc64c3a20f18e1911
ventusignis <mgilzinger@google.com>
2021-05-31 10:40:35 -0400

    Fix special token in 1856

--- lib/engine/game/g_1856/step/special_token.rb
+++ lib/engine/game/g_1856/step/special_token.rb
@@ -7,6 +7,11 @@ module Engine
     module G1856
       module Step
         class SpecialToken < Engine::Step::SpecialToken
+          def actions(entity)
+            return [] if entity.company? && entity.owner.player?
+
+            super
+          end
           def process_place_token(action)
             # This is WSRC because no other private can use the special token step
             @game.log << "#{@game.wsrc.name} is used and will close at the end of the operating round"
