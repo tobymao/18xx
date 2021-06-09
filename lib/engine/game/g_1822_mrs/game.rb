177b0bb8540b84a8da0d418a25dc64eb6d024479
TBarchet <65371010+TBarchet@users.noreply.github.com>
2021-06-09 10:10:09 -0400

    1822MRS 2-Player Variant Fix
    Resubmitted compliant code for 1822MRS 2-Player

--- lib/engine/game/g_1822_mrs/game.rb
+++ lib/engine/game/g_1822_mrs/game.rb
@@ -11,7 +11,7 @@ module Engine
 
         BIDDING_BOX_START_MINOR = 'M24'
         BIDDING_BOX_START_MINOR_ADV = 'M14'
-		
+
         CERT_LIMIT = { 2 => 27, 3 => 18, 4 => 14, 5 => 11, 6 => 9, 7 => 8 }.freeze
 
         DESTINATIONS = {
@@ -43,10 +43,10 @@ module Engine
         STARTING_COMPANIES_ADVANCED = %w[P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12
                                          C1 C2 C3 C4 C6 C7 C9 M7 M8 M9 M10 M11 M12 M13 M14 M15
                                          M16 16 M17 M18 M19 M20 M21 M24].freeze
-										 
-		STARTING_COMPANIES_2Player = %w[P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12
-                                         C1 C2 C3 C4 C6 C7 C9 M7 M8 M9 M10 M11 M12 M13 M14 M15
-                                         M16 16 M17 M18 M19 M20 M21 M24].freeze								 
+
+        STARTING_COMPANIES_TWOPLAYER = %w[P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12
+                                          C1 C2 C3 C4 C6 C7 C9 M7 M8 M9 M10 M11 M12 M13 M14 M15
+                                          M16 16 M17 M18 M19 M20 M21 M24].freeze
 
         STARTING_CORPORATIONS = %w[7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 24
                                    LNWR GWR LBSCR SECR MR LYR SWR].freeze
@@ -681,9 +681,10 @@ module Engine
         end
 
         def starting_companies
-		  return self.class::STARTING_COMPANIES_ADVANCED if optional_advanced?
- 		  return self.class::STARTING_COMPANIES_2Player if @players.size == 2
-		  self.class::STARTING_COMPANIES
+          return self.class::STARTING_COMPANIES_ADVANCED if optional_advanced?
+          return self.class::STARTING_COMPANIES_TWOPLAYER if @players.size == 2
+
+          self.class::STARTING_COMPANIES
         end
 
         def optional_advanced?
