198bb95bc2eac0aecef97a4aab610811bb7ab414
scottredracecar <scott@redracecar.com>
2021-06-24 17:51:06 -0500

    1877 update rules link

--- lib/engine/game/g_1877/meta.rb
+++ lib/engine/game/g_1877/meta.rb
@@ -16,7 +16,7 @@ module Engine
         GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/1877'
         GAME_LOCATION = 'Venezuela'
         GAME_PUBLISHER = :all_aboard_games
-        GAME_RULES_URL = 'https://github.com/tobymao/18xx/wiki/1877'
+        GAME_RULES_URL = 'https://docs.google.com/document/d/1HlnW2jPCMcxYisDX_CjNLMkwBz3nmwOQItsNKm13HRc'
 
         PLAYER_RANGE = [2, 7].freeze
         OPTIONAL_RULES = [
