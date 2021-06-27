5594a3e893db708e10a395a553f35ea6d385e8b2
perwestling <per.o.westling@gmail.com>
2021-06-27 12:37:50 +0200

    Change minor check in buy trains actions (fixes #5843)
    By making the check in a method it is easy for titles to override
    method if they want to allow a minor to be able to buy trains.
    
    If instead an override of actions' method it might cause a bug
    if code is forgotten - which is what happended in 1893 (and 1824).
    
    A quick scan through of existing titles did not find any other
    title where minors can buy trains so I have not updated any
    other title.

--- lib/engine/game/g_1893/step/buy_train.rb
+++ lib/engine/game/g_1893/step/buy_train.rb
@@ -7,14 +7,8 @@ module Engine
     module G1893
       module Step
         class BuyTrain < Engine::Step::BuyTrain
-          def actions(entity)
-            return [] if entity != current_entity
-            # TODO: Not sure this is right
-            return %w[sell_shares buy_train] if president_may_contribute?(entity)
-
-            return %w[buy_train pass] if can_buy_train?(entity)
-
-            []
+          def can_entity_buy_train?
+            true
           end
 
           def round_state
