92d1472378d6f3e323191a8c7db7905447e6ce8e
roseundy <steve@roseundy.net>
2021-06-28 10:57:27 -0600

    fixed ternary assignment

--- assets/app/view/game/round/operating.rb
+++ assets/app/view/game/round/operating.rb
@@ -38,8 +38,7 @@ module View
           left << render_mode_button if @step.respond_to?(:mode_enabled?) && @step.mode_enabled?
           left << h(SpecialBuy) if @current_actions.include?('special_buy')
           if @current_actions.include?('run_routes')
-            left << h(RouteSelector) unless @step.conversion?
-            left << h(TrackConversion) if @step.conversion?
+            left << (@step.conversion? ? h(TrackConversion) : h(RouteSelector))
           end
           left << h(Dividend) if @current_actions.include?('dividend')
           left << h(Convert) if @current_actions.include?('convert')
