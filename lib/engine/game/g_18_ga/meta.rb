63ea5d6d4015225f122c8faa54d5b33a0ca92e72
philcampeau <philcampeau@gmail.com>
2021-06-30 20:22:52 -0400

    Create meta.rb

--- lib/engine/game/g_18_ga/meta.rb
+++ lib/engine/game/g_18_ga/meta.rb
@@ -16,6 +16,18 @@ module Engine
         GAME_RULES_URL = 'http://www.18xx.net/18GA/18GAr.txt'
 
         PLAYER_RANGE = [3, 5].freeze
+        OPTIONAL_RULES = [
+          {
+            sym: :soft_rust_t4,
+            short_name: 'Soft rust',
+            desc: '4 trains get one more run after rusting',
+          },
+          {
+            sym: :double_yellow_first_or,
+            short_name: 'Extra yellow',
+            desc: 'Allow corporation to lay 2 yellows its first OR',
+          },
+        ].freeze
       end
     end
   end
