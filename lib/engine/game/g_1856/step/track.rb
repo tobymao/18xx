2804e6ca8064bff6026e33c957b0a265cf275980
ventusignis <mgilzinger@google.com>
2021-07-07 08:37:54 -0400

    Fix 1856 not allowing dits to downgrade

--- lib/engine/game/g_1856/step/track.rb
+++ lib/engine/game/g_1856/step/track.rb
@@ -7,6 +7,41 @@ module Engine
     module G1856
       module Step
         class Track < Engine::Step::Track
+          def check_track_restrictions!(entity, old_tile, new_tile)
+            return if @game.loading || !entity.operator?
+
+            graph = @game.graph_for_entity(entity)
+
+            # Don't check that towns are preserved
+
+            old_paths = old_tile.paths
+            changed_city = false
+            used_new_track = old_paths.empty?
+
+            new_tile.paths.each do |np|
+              next unless graph.connected_paths(entity)[np]
+
+              op = old_paths.find { |path| np <= path }
+              used_new_track = true unless op
+              old_revenues = op&.nodes && op.nodes.map(&:max_revenue).sort
+              new_revenues = np&.nodes && np.nodes.map(&:max_revenue).sort
+              changed_city = true unless old_revenues == new_revenues
+            end
+
+            case @game.class::TRACK_RESTRICTION
+            when :permissive
+              true
+            when :city_permissive
+              raise GameError, 'Must be city tile or use new track' if new_tile.cities.none? && !used_new_track
+            when :restrictive
+              raise GameError, 'Must use new track' unless used_new_track
+            when :semi_restrictive
+              raise GameError, 'Must use new track or change city value' if !used_new_track && !changed_city
+            else
+              raise
+            end
+          end
+
           def legal_tile_rotation?(entity, hex, tile)
             old_paths = hex.tile.paths
             old_ctedges = hex.tile.city_town_edges
