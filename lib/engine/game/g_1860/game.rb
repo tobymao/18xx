8119e04d7e2ba6c4739ce6ce7b97848740d8d74c
roseundy <steve@roseundy.net>
2021-08-05 09:55:53 -0600

    remove superfluous check in 1860

--- lib/engine/game/g_1860/game.rb
+++ lib/engine/game/g_1860/game.rb
@@ -1683,7 +1683,6 @@ module Engine
         # all routes must intersect each other
         def check_intersection(routes)
           actual_routes = routes.reject { |r| r.chains.empty? }
-          return unless actual_routes
 
           # build a map of which routes intersect with each route
           intersects = Hash.new { |h, k| h[k] = [] }
