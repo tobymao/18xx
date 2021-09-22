f35913b7b4f5cfedc2588ef32b339ff11ef42df8
Steve Undy <steve@roseundy.net>
2021-09-21 22:53:54 -0600

    1825: better handling of options

--- lib/engine/game/g_1825/meta.rb
+++ lib/engine/game/g_1825/meta.rb
@@ -119,6 +119,118 @@ module Engine
             desc: 'Do not increase bank size based on number of minors and kits',
           },
         ].freeze
+
+        MUTEX_RULES = [
+          %i[unit_1 unit_2 unit_3 unit_12 unit_23 unit_123],
+          %i[big_bank strict_bank],
+        ].freeze
+
+        def self.check_options(options, num_players)
+          optional_rules = (options || []).map(&:to_sym)
+
+          return if optional_rules.empty? && !num_players
+
+          if optional_rules.empty?
+            case num_players
+            when 2
+              return 'No Unit(s) selected. Will use Unit 3 based on player count'
+            when 3
+              return 'No Unit(s) selected. Will use Unit 2 based on player count'
+            when 4, 5
+              return 'No Unit(s) selected. Will use Unit 1 based on player count'
+            when 6, 7
+              return 'No Unit(s) selected. Will use Units 1+2 based on player count'
+            when 8
+              return 'No Unit(s) selected. Will use Units 1+2+3 based on player count'
+            else
+              return 'No Unit(s) selected. Will use Units 1+2+3 and R1+R2+R3 based on player count'
+            end
+          end
+
+          units = {}
+          kits = {}
+          regionals = {}
+
+          units[1] = true if optional_rules.include?(:unit_1)
+          units[1] = true if optional_rules.include?(:unit_12)
+          units[1] = true if optional_rules.include?(:unit_123)
+
+          units[2] = true if optional_rules.include?(:unit_2)
+          units[2] = true if optional_rules.include?(:unit_12)
+          units[2] = true if optional_rules.include?(:unit_23)
+          units[2] = true if optional_rules.include?(:unit_123)
+
+          units[3] = true if optional_rules.include?(:unit_3)
+          units[3] = true if optional_rules.include?(:unit_23)
+          units[3] = true if optional_rules.include?(:unit_123)
+
+          kits[1] = true if optional_rules.include?(:k1)
+          kits[2] = true if optional_rules.include?(:k2)
+          kits[3] = true if optional_rules.include?(:k3)
+          kits[5] = true if optional_rules.include?(:k5)
+          kits[6] = true if optional_rules.include?(:k6)
+          kits[7] = true if optional_rules.include?(:k7)
+
+          regionals[1] = true if optional_rules.include?(:r1)
+          regionals[2] = true if optional_rules.include?(:r2)
+          regionals[3] = true if optional_rules.include?(:r3)
+
+          if !units[1] && !units[2] && !units[3] && !optional_rules.empty?
+            return 'Must select at least one Unit if using other options'
+          end
+          return 'Cannot combine Units 1 and 3 without Unit 2' if units[1] && !units[2] && units[3]
+          return 'Cannot add Regionals without Unit 1' if !regionals.keys.empty? && !units[1]
+          return 'Cannot add K5 without Unit 2' if kits[5] && !units[2]
+          return 'Cannot add K7 without Unit 1' if kits[7] && !units[1]
+          return 'K2 not supported with just Unit 3' if kits[2] && !units[1] && !units[2] && units[3]
+          return 'K2 not supported without K3' if kits[2] && !kits[3]
+          return 'Cannot use extra Unit 3 trains without Unit 3' if !units[3] && optional_rules.include?(:u3p)
+          return 'Cannot use K1 or K6 with D1' if (kits[1] || kits[6]) && optional_rules.include?(:d1)
+          return 'Cannot use both bank options' if optional_rules.include?(:big_bank) && optional_rules.include?(:strict_bank)
+
+          nil
+        end
+
+        def self.min_players(optional_rules, num_players)
+          optional_rules = (optional_rules || []).map(&:to_sym)
+
+          if optional_rules.empty?
+            return unless num_players
+
+            case num_players
+            when 2, 3, 4, 5
+              return 2
+            when 6, 7
+              return 3
+            else
+              return 4
+            end
+          end
+
+          units = {}
+
+          units[1] = true if optional_rules.include?(:unit_1)
+          units[1] = true if optional_rules.include?(:unit_12)
+          units[1] = true if optional_rules.include?(:unit_123)
+
+          units[2] = true if optional_rules.include?(:unit_2)
+          units[2] = true if optional_rules.include?(:unit_12)
+          units[2] = true if optional_rules.include?(:unit_23)
+          units[2] = true if optional_rules.include?(:unit_123)
+
+          units[3] = true if optional_rules.include?(:unit_3)
+          units[3] = true if optional_rules.include?(:unit_23)
+          units[3] = true if optional_rules.include?(:unit_123)
+
+          case units.keys.sort.map(&:to_s).join
+          when '1', '2', '3'
+            2
+          when '12', '23'
+            3
+          else # all units
+            4
+          end
+        end
       end
     end
   end
