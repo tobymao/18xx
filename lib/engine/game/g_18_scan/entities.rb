0fd4e0f0e227968859a17ccd5ed66aa3848103ab
Aleksandr Vansach <Aleksandr.Vansach@eurochem.ru>
2021-10-06 12:35:20 +0300

    18Scan: add game map

---
+++ lib/engine/game/g_18_scan/entities.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G18Scan
+      module Entities
+        COMPANIES = [].freeze
+
+        CORPORATIONS = [].freeze
+     end
+    end
+  end
+end
--- lib/engine/game/g_18_scan/game.rb
+++ lib/engine/game/g_18_scan/game.rb
@@ -2,12 +2,16 @@
 
 require_relative 'meta'
 require_relative '../base'
+require_relative 'entities'
+require_relative 'map'
 
 module Engine
   module Game
     module G18Scan
       class Game < Game::Base
         include_meta(G18Scan::Meta)
+        include G18Scan::Map
+        include G18Scan::Entities
       end
     end
   end
