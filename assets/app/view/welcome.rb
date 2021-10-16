f13a659137cce0bb4fb7e65c4cde4a1cd5e7d565
tobymao <toby.mao@gmail.com>
2021-10-15 22:18:27 -0700

    welcome

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -17,8 +17,7 @@ module View
 
     def render_notification
       message = <<~MESSAGE
-        <p>1825 is now in alpha. 18VA is now in beta.</p>
-
+        <p>18Tokaido is a new prototype in alpha!</p>
         <p>We now support Slack/Discord notifications. Learn how to use them <a href='https://github.com/tobymao/18xx/wiki/Notifications'>here</a>.</p>
         <p>Please submit problem reports and make suggestions for improvements on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>.</p>
