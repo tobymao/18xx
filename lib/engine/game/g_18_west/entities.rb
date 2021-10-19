a7a8e9014a718b086faf58b9713a08960ae27fce
Douglas Triggs <douglas@triggs.org>
2021-10-19 00:41:14 -0600

    [18West] initial config, tiles, map

---
+++ lib/engine/game/g_18_west/entities.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G18West
+      module Entities
+        COMPANIES = [
+          {
+            name: 'Delete this later',
+            value: 20,
+            revenue: 5,
+            desc: 'D8',
+            sym: 'DELETE',
+            abilities: [{ type: 'blocks_hexes', owner_type: 'player', hexes: ['D8'] }],
+            color: nil,
+          },
+        ].freeze
+
+        CORPORATIONS = [
+
+        ].freeze
+      end
+    end
+  end
+end
