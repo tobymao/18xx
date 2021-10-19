38a5ec140d889b0ad87638a708249e30e0ddf491
Chris Shaffer <ChrisTheCat@users.noreply.github.com>
2021-10-19 07:02:15 -0700

    Updated notification text

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -18,7 +18,7 @@ module View
     def render_notification
       message = <<~MESSAGE
         <p>18Tokaido is a new prototype in alpha!</p>
-        <p>We now support Slack/Discord notifications. Learn how to use them <a href='https://github.com/tobymao/18xx/wiki/Notifications'>here</a>.</p>
+        <p>Learn how to get <a href='https://github.com/tobymao/18xx/wiki/Notifications'>notifications</a> by email, Slack, Discord, and Telegram.</p>
         <p>Please submit problem reports and make suggestions for improvements on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>.</p>
         <p>The <a href='https://github.com/tobymao/18xx/wiki'>18xx.games Wiki</a> has rules, maps,
