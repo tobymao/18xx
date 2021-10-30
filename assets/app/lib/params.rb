49a570c9237ac9739023db780f69a58c336fcd1b
Magnus Kristiansen <37312691+magnusrk-ch@users.noreply.github.com>
2021-10-30 21:43:41 +0200

    Make home page navigation preserve unrelated query params.
    This is needed to support filtering by e.g. game title, since otherwise the filters will be dropped as soon as you paginate.
    
    The immediate effect is that paginating in one section won't reset the other sections to page 0. Also fixes a double render when paginating games on home page.
    
    Introduces a class in Lib::Params for stateful manipulation of search/query params.
    ***

--- assets/app/lib/params.rb
+++ assets/app/lib/params.rb
@@ -3,9 +3,7 @@
 module Lib
   module Params
     def self.[](key)
-      return nil if `typeof URLSearchParams === 'undefined'` # rubocop:disable Lint/LiteralAsCondition
-
-      Native(`(new URLSearchParams(window.location.search)).get(#{key})`)
+      URLSearchParams.new[key]
     end
 
     def self.add(route, k, v)
@@ -13,6 +11,31 @@ module Lib
       return route unless v
 
       "#{route}?#{k}=#{v}"
+    end
+
+    class URLSearchParams
+      attr_reader :unsupported
+
+      def initialize
+        @unsupported = `typeof URLSearchParams === 'undefined'`
+        @native = Native(`new URLSearchParams(window.location.search)`) unless @unsupported
+      end
+
+      def [](key)
+        @native&.get(key)
+      end
+
+      def []=(key, value)
+        if !value || value.to_s.empty?
+          @native&.delete(key)
+        else
+          @native&.set(key, value)
+        end
+      end
+
+      def to_query_string
+        @native&.toString() || ''
+      end
     end
   end
 end
