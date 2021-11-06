9af418726c1bd010b64002e40fa4d1af5962f062
Michael Alexander <outsidepasser@gmail.com>
2021-11-06 17:16:49 -0400

    Give the M18 to the purchaser of the FCM concession

---
+++ lib/engine/game/g_1822_mx/round/stock.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../g_1822/round/stock'
+
+module Engine
+  module Game
+    module G1822MX
+      module Round
+        class Stock < Engine::Game::G1822::Round::Stock
+          def buy_company(bid)
+            super
+            player = bid.entity
+            if bid.company.id == 'C1'
+              company = @game.company_by_id('M18')
+              company.owner = player
+              player.companies << company
+
+              minor = @game.find_corporation(company)
+              minor.reservation_color = :white
+
+              share_price = @game.stock_market.par_prices.find { |pp| pp.price == 50 }
+              @game.stock_market.set_par(minor, share_price)
+              @game.bank.spend(100, minor)
+
+              @game.share_pool.transfer_shares(ShareBundle.new(minor.shares.first), player)
+              @game.after_par(minor)
+
+            end
+          end
+        end
+      end
+    end
+  end
+end
