aa4d89342a0c1a0ba2dc825661d7dd8496f7e79f
Chris Rericha <crericha@gmail.com>
2021-11-20 21:20:10 -0500

    [18NY] Don't double count new connection bonuses

--- lib/engine/game/g_18_ny/step/route.rb
+++ lib/engine/game/g_18_ny/step/route.rb
@@ -9,8 +9,18 @@ module Engine
         class Route < Engine::Step::Route
           def process_run_routes(action)
             super
+
+            connection_bonus = false
             action.routes.flat_map(&:stops).select { |stop| stop.hex.assigned?('connection_bonus') }.each do |stop|
               @game.claim_connection_bonus(action.entity, stop.hex)
+              connection_bonus = true
+            end
+            return unless connection_bonus
+
+            # Recalculate revenue if a connection_bonus was claimed
+            action.routes.each do |route|
+              route.clear_cache!
+              route.revenue
             end
           end
         end
