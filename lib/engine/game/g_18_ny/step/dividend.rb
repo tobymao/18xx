49eab1cc19a95fdbc75858fe24ec76d6bd23d95b
Chris Rericha <crericha@gmail.com>
2021-11-23 01:24:01 -0500

    [18NY] Minors only move forward if they have revenue

--- lib/engine/game/g_18_ny/step/dividend.rb
+++ lib/engine/game/g_18_ny/step/dividend.rb
@@ -14,10 +14,8 @@ module Engine
           include Engine::Step::MinorHalfPay
 
           def share_price_change(entity, revenue = 0)
-            revenue += revenue if entity.type == :minor
-
             price = entity.share_price.price
-            return { share_direction: :right, share_times: 1 } if entity.type == :minor
+            return { share_direction: :right, share_times: 1 } if entity.type == :minor && revenue.positive?
             return { share_direction: :right, share_times: 1 } if revenue >= price
             return { share_direction: :left, share_times: 1 } if revenue < price / 2
 
