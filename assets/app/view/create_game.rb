2be4a27115028a2bdd4a006539c124baa9bda0bc
Michael Brandt <michaelbrandt5@gmail.com>
2021-12-17 12:50:32 -0700

    use "Shikoku 1889" as 1889's canonical title
    * update references in announcements, new player recommendations, and tutorial
    * update game card rendering to use the nearest title, rather than the game
      data's actual title; this allows games currently in the database as '1889'
      to continue to function without change (migration), and be displayed as
      'Shikoku 1889'
    * simplify the location to "Japan" so that the string in the new game dropdown
      is not so repetitive

--- assets/app/view/create_game.rb
+++ assets/app/view/create_game.rb
@@ -69,7 +69,7 @@ module View
           h(:a, { attrs: { href: '/login' } }, 'login'), ' to play multiplayer.'
         ]
       end
-      description << h(:p, 'If you are new to 18xx games then 1889, 18Chesapeake or 18MS are good games to begin with.')
+      description << h(:p, 'If you are new to 18xx games then Shikoku 1889, 18Chesapeake or 18MS are good games to begin with.')
       render_form('Create New Game', inputs, description)
     end
 
