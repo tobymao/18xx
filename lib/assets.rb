395523b358cd192ab0c44d0523c8f5459930d71e
tobymao <toby.mao@gmail.com>
2021-12-24 13:35:22 -0800

    undo tree shaking

--- lib/assets.rb
+++ lib/assets.rb
@@ -238,7 +238,7 @@ class Assets
       file.write(source)
       file.rewind
       now = Time.now
-      source = `esbuild #{file.path} --minify --log-level=error --target=es2019 --tree-shaking=true`
+      source = `esbuild #{file.path} --minify --log-level=error --target=es2019`
       puts "Compressing #{key} - #{Time.now - now}"
     end
 
