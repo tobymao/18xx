7c4c2e0a0777a64778eef8d3af86aa61f73c04cf
Lachlan Kingsford <lachlan@nerdygentleman.com>
2022-01-03 15:29:21 +1100

    18ChEv Add rooms and home stations

--- lib/engine/game/g_18_christmas_eve/game.rb
+++ lib/engine/game/g_18_christmas_eve/game.rb
@@ -18,12 +18,43 @@ module Engine
         # rubocop:disable Layout/LineLength
         HEXES = {
           white: {
-            %w[C8 D11 D3 D7 D9 E4 F5 F11 H11 I2 I4 I8 J5 J7 K4 K12 L1 L3 L9] => '',
-            %w[G8 G10 H7 H9] => 'upgrade=cost:80,terrain:mountain',
-            %w[B9 C4 C12 I12] => 'town=revenue:0',
-            %w[B7 I6 J3] => 'town=revenue:0;town=revenue:0',
-            %w[C10] => 'city=revenue:0;label=DC',
-            %w[B3 D5 E6 E10 F3 F9 G12 K2 K8 K10 L5 L11] => 'city=revenue:0]',
+            # Kitchen
+            %w[D3 E4] => 'frame=color:#CE95C3',
+            %w[B3 F3] => 'frame=color:#CE95C3;city=revenue:0',
+            %w[C4] => 'frame=color:#CE95C3;town=revenue:0',
+            # Dining
+            %w[D5 E6] => 'frame=color:#F7FFB6;city=revenue:0',
+            %w[D7 F5] => 'frame=color:#F7FFB6',
+            # Lounge
+            %w[C8 D9 D11] => 'frame=color:#FFFFFF',
+            %w[B7] => 'frame=color:#FFFFFF;town=revenue:0;town=revenue:0',
+            %w[B9 C12] => 'frame=color:#FFFFFF;town=revenue:0',
+            %w[C10] => 'frame=color:#FFFFFF;city=revenue:0;label=DC',
+            # Foyer
+            %w[F11] => 'frame=color:#7D96CD',
+            %w[E10] => 'frame=color:#7D96CD;city=revenue:0;',
+            # Staircase
+            %w[G8 G10 H7 H9] => 'upgrade=cost:80,terrain:mountain;frame=color:#C4E4CD',
+            # Study
+            %w[G12] => 'frame=color:#BBBBB6;city=revenue:0;',
+            %w[H11] => 'frame=color:#BBBBB6;',
+            %w[I12] => 'frame=color:#BBBBB6;town=revenue:0',
+            # Master
+            %w[K10 L11] => 'frame=color:#E6C4AF;city=revenue:0;',
+            %w[L9 K12] => 'frame=color:#E6C4AF;',
+            # Hall
+            %w[I4 J5 I8 J7] => 'frame=color:#F5846B',
+            %w[I6] => 'frame=color:#F5846B;town=revenue:0;town=revenue:0',
+            %w[K8] => 'frame=color:#F5846B;city=revenue:0;',
+            # Rec Room
+            %w[I2 J1] => 'frame=color:#9FCAA1',
+            %w[J3] => 'frame=color:#9FCAA1;town=revenue:0;town=revenue:0',
+            # Bedroom
+            %w[L1 L3] => 'frame=color:#AB9E7F',
+            %w[K2] => 'frame=color:#AB9E7F;city=revenue:0;',
+            # Bedroom
+            %w[K4] => 'frame=color:#AFDFE0',
+            %w[L5] => 'frame=color:#AFDFE0;city=revenue:0;',
           },
           red: {
             %w[E14] => 'offboard=revenue:yellow_20|green_40|brown_60|gray_80;path=a:2,b:_0;path=a:3,b:_0;path=a:4,b:_0',
@@ -37,8 +68,8 @@ module Engine
             %w[B1] => 'offboard=revenue:yellow_60|brown_20|gray_10,hide:1,groups:Back Door;path=a:0,b:_0;border=edge:1',
           },
           yellow: {
-            %w[E12] => 'city=revenue:30;city=revenue:30;path=a:3,b:_0;path=a:0,b:_1;label=OO',
-            %w[H3] => 'city=revenue:30;city=revenue:30;path=a:2,b:_0;path=a:5,b:_1;label=OO',
+            %w[E12] => 'frame=color:#7D96CD;city=revenue:30;city=revenue:30;path=a:3,b:_0;path=a:0,b:_1;label=OO',
+            %w[H3] => 'frame=color:#F5846B;city=revenue:30;city=revenue:30;path=a:2,b:_0;path=a:5,b:_1;label=OO',
           },
           gray: {
             %w[D13 G4] => 'path=a:4,b:5',
@@ -52,6 +83,101 @@ module Engine
           }
         }.freeze
 
+        CORPORATIONS = [
+          {
+            float_percent: 60,
+            sym: 'PRR',
+            name: 'Pennsylvania Railroad',
+            logo: '18_chesapeake/PRR',
+            simple_logo: '18_chesapeake/PRR.alt',
+            tokens: [0, 40, 60, 80],
+            coordinates: 'L11',
+            color: '#237333',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'PLE',
+            name: 'Pittsburgh and Lake Erie Railroad',
+            logo: '18_chesapeake/PLE',
+            simple_logo: '18_chesapeake/PLE.alt',
+            tokens: [0, 40, 60],
+            coordinates: 'B3',
+            color: :black,
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'SRR',
+            name: 'Strasburg Rail Road',
+            logo: '18_chesapeake/SRR',
+            simple_logo: '18_chesapeake/SRR.alt',
+            tokens: [0, 40],
+            city: 1,
+            coordinates: 'E12',
+            color: '#d81e3e',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'B&O',
+            name: 'Baltimore & Ohio Railroad',
+            logo: '18_chesapeake/BO',
+            simple_logo: '18_chesapeake/BO.alt',
+            tokens: [0, 40, 60],
+            coordinates: 'L5',
+            city: 0,
+            color: '#0189d1',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'C&O',
+            name: 'Chesapeake & Ohio Railroad',
+            logo: '18_chesapeake/CO',
+            simple_logo: '18_chesapeake/CO.alt',
+            tokens: [0, 40, 60, 80],
+            coordinates: 'K2',
+            color: '#a2dced',
+            text_color: 'black',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'LV',
+            name: 'Lehigh Valley Railroad',
+            logo: '18_chesapeake/LV',
+            simple_logo: '18_chesapeake/LV.alt',
+            tokens: [0, 40],
+            coordinates: 'F9',
+            color: '#FFF500',
+            text_color: 'black',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'C&A',
+            name: 'Camden & Amboy Railroad',
+            logo: '18_chesapeake/CA',
+            simple_logo: '18_chesapeake/CA.alt',
+            tokens: [0, 40],
+            coordinates: 'J11',
+            color: '#f48221',
+            reservation_color: nil,
+          },
+          {
+            float_percent: 60,
+            sym: 'N&W',
+            name: 'Norfolk & Western Railway',
+            logo: '18_chesapeake/NW',
+            simple_logo: '18_chesapeake/NW.alt',
+            tokens: [0, 40, 60],
+            coordinates: 'E6',
+            color: '#7b352a',
+            reservation_color: nil,
+          },
+        ].freeze
+
         LOCATION_NAMES = {
           'J11' => 'Basement',
           'E14' => 'Front Door',
@@ -62,6 +188,18 @@ module Engine
           'B1' => 'Back Door',
           'A6' => 'Forgotten Cupboard',
           'A8' => 'Garage',
+          'G8' => 'Staircase',
+          'D3' => 'Kitchen',
+          'D7' => 'Dining',
+          'H11' => 'Study',
+          'C8' => 'Lounge',
+          'L9' => 'Master',
+          'J7' => 'Hall',
+          'K4' => 'Bedroom',
+          'L3' => 'Bedroom',
+          'I2' => 'Rec Room',
+          'F11' => 'Foyer',
+
         }.freeze
 
         def or_set_finished; end
