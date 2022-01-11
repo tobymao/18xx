8ae90a467e66c08db803f026a7d67ff98879ad50
Steve Undy <steve@roseundy.net>
2022-01-11 13:08:56 -0700

    UI: adjust size for number input forms for Firefox

--- assets/app/view/game/round/auction.rb
+++ assets/app/view/game/round/auction.rb
@@ -149,7 +149,7 @@ module View
                       min: @step.min_bid(company),
                       max: @step.max_bid(@current_entity, company),
                       type: 'number',
-                      size: @current_entity.cash.to_s.size,
+                      size: @current_entity.cash.to_s.size + 2,
                     })
 
           buttons = []
@@ -200,7 +200,7 @@ module View
                 min: @step.min_player_bid,
                 max: @step.max_player_bid(@current_entity),
                 type: 'number',
-                size: @current_entity.cash.to_s.size,
+                size: @current_entity.cash.to_s.size + 2,
               })
           h(:div, [
             input,
@@ -276,7 +276,7 @@ module View
                       min: @step.min_bid(minor),
                       max: @step.max_bid(@current_entity, minor),
                       type: 'number',
-                      size: @current_entity.cash.to_s.size,
+                      size: @current_entity.cash.to_s.size + 2,
                     })
 
           [
