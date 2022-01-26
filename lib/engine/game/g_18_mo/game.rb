34afa7873826d729e0143643d23c2d1e9f63a76e
Scott Petersen <mkp@Marys-MacBook-Pro.local>
2022-01-26 08:50:19 -0600

    updates

--- lib/engine/game/g_18_mo/game.rb
+++ lib/engine/game/g_18_mo/game.rb
@@ -13,18 +13,21 @@ module Engine
         include G18MO::Entities
         include G18MO::Map
 
+        STARTING_CASH = { 2 => 600, 3 => 425, 4 => 400, 5 => 385 }.freeze
+        TILE_COST = 21
+
         ORANGE_GROUP = [
-        'Misc Bonus Token A',
-        'Misc Bonus Token B',
-        'Tunnel Blasting Company',
+        'Pool Share',
         'Steamboat Company',
+        'Extra Green Tile',
+        'Revenue Change',
         ].freeze
 
         BLUE_GROUP = [
-        'Misc Track A',
-        'Misc Track B',
-        'Misc Track C',
-        'Misc Track D',
+        'Tunnel Blasting Company',
+        'Ranch Tile',
+        'Train Discount',
+        'Mountain Construction Company',
         ].freeze
 
         GREEN_GROUP = %w[ATSF MKT CBQ RI MP SSW SLSF].freeze
@@ -39,6 +42,16 @@ module Engine
           'SLSF' => 'E9',
         }.freeze
 
+        STEAMBOAT_HEXES = %w[].freeze
+
+        def steamboat
+          @steamboat ||= company_by_id('SC')
+        end
+
+        def block_for_steamboat?
+          false
+        end
+
         LSL_HEXES = %w[].freeze
         LSL_ICON = 'sbl'
         LSL_ID = 'SBL'
@@ -51,8 +64,153 @@ module Engine
 
         MEAT_REVENUE_DESC = 'Citrus'
 
-        def steamboat
-          @steamboat ||= company_by_id('SC')
+        def operating_round(round_num)
+          @round_num = round_num
+          G1846::Round::Operating.new(self, [
+            G1846::Step::Bankrupt,
+            G1846::Step::Assign,
+            Engine::Step::SpecialToken,
+            G1846::Step::SpecialTrack,
+            G1846::Step::BuyCompany,
+            G1846::Step::IssueShares,
+            G1846::Step::TrackAndToken,
+            Engine::Step::Route,
+            G1846::Step::Dividend,
+            Engine::Step::DiscardTrain,
+            Engine::Step::SpecialBuyTrain,
+            G1846::Step::BuyTrain,
+            [G1846::Step::BuyCompany, { blocks: true }],
+          ], round_num: round_num)
+        end
+
+        def stock_round
+          Engine::Round::Stock.new(self, [
+            Engine::Step::DiscardTrain,
+            G1846::Step::Assign,
+            Engine::Step::Exchange,
+            G1846::Step::BuySellParShares,
+          ])
+        end
+
+        PHASES = [
+                {
+                  name: '2',
+                  train_limit: 4,
+                  tiles: [:yellow],
+                  operating_rounds: 2,
+                  status: ['can_buy_companies'],
+                },
+                {
+                  name: '3',
+                  train_limit: 4,
+                  on: '3G',
+                  tiles: %i[yellow green],
+                  operating_rounds: 2,
+                  status: ['can_buy_companies'],
+                },
+                {
+                  name: '4',
+                  train_limit: 4,
+                  on: '4E',
+                  tiles: %i[yellow green],
+                  operating_rounds: 2,
+                  status: ['can_buy_companies'],
+                },
+                {
+                  name: '5',
+                  on: '5',
+                  train_limit: 3,
+                  tiles: %i[yellow green brown],
+                  operating_rounds: 2,
+                },
+                {
+                  name: '6',
+                  on: '6E',
+                  train_limit: 3,
+                  tiles: %i[yellow green brown],
+                  operating_rounds: 2,
+                },
+                {
+                  name: '7',
+                  on: '7',
+                  train_limit: 2,
+                  tiles: %i[yellow green brown gray],
+                  operating_rounds: 2,
+                },
+              ].freeze
+
+        TRAINS = [
+          {
+            name: '2Y',
+            distance: 2,
+            price: 100,
+            obsolete_on: '4E',
+            rusts_on: '7',
+            variants: [
+              {
+                name: '3Y',
+                distance: 3,
+                price: 150,
+              },
+              ],
+          },
+          {
+            name: '3G',
+            distance: 3,
+            price: 180,
+            obsolete_on: '6E',
+            variants: [
+              {
+                name: '4G',
+                distance: 4,
+                price: 240,
+              },
+            ],
+          },
+          {
+            name: '4E',
+            distance: [{ 'nodes' => %w[city offboard town], 'pay' => 4, 'visit' => 99 }],
+            price: 300,
+            obsolete_on: '6E',
+          },
+          {
+            name: '5',
+            distance: 5,
+            price: 450,
+            variants: [
+              {
+                name: '6',
+                distance: 6,
+                price: 540,
+              },
+            ],
+            events: [{ 'type' => 'close_companies' }],
+          },
+          {
+            name: '6E',
+            distance: [{ 'nodes' => %w[city offboard town], 'pay' => 6, 'visit' => 99 }],
+            price: 600,
+          },
+          {
+            name: '7',
+            distance: 7,
+            price: 700,
+            variants: [
+              {
+                name: '8',
+                distance: 8,
+                price: 800,
+              },
+            ],
+            events: [
+              { 'type' => 'remove_markers' },
+              { 'type' => 'remove_reservations' },
+            ],
+          },
+        ].freeze
+
+        def operating_order
+          @minors.select(&:floated?) + @corporations.select(&:floated?).sort
         end
 
         def check_other(route)
@@ -62,6 +220,29 @@ module Engine
             raise GameError, 'Route cannot run to multiple cities in a hex' if visited_hexes[hex]
 
             visited_hexes[hex] = true
+          end
+        end
+
+        # Two lays with one being an upgrade, second tile costs 20
+        TILE_LAYS = [
+          { lay: true, upgrade: true },
+          { lay: true, upgrade: :not_if_upgraded, cost: 20 },
+        ].freeze
+
+        def num_trains(train)
+          num_players = @players.size
+
+          case train[:name]
+          when '2Y'
+            two_player? ? 7 : num_players + 4
+          when '3G'
+            two_player? ? 5 : num_players
+          when '4E', '6E'
+            1
+          when '5'
+            two_player? ? 3 : num_players - 1
+          when '7'
+            two_player? ? 4 : 9
           end
         end
 
--- lib/engine/game/g_18_mo/map.rb
+++ lib/engine/game/g_18_mo/map.rb
@@ -92,51 +92,52 @@ module Engine
             'color' => 'green',
             'code' => 'city=revenue:40;city=revenue:40;city=revenue:40;city=revenue:40;'\
                       'path=a:0,b:_0;path=a:_0,b:2;path=a:3,b:_1;path=a:_1,b:2;path=a:4,b:_2;'\
-                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=Chi',
+                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=StL',
           },
           '299' => {
             'count' => 1,
             'color' => 'brown',
             'code' => 'city=revenue:70;city=revenue:70;city=revenue:70;city=revenue:70;'\
                       'path=a:0,b:_0;path=a:_0,b:2;path=a:3,b:_1;path=a:_1,b:2;path=a:4,b:_2;'\
-                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=Chi',
+                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=StL',
           },
           '300' => {
             'count' => 1,
             'color' => 'gray',
             'code' => 'city=revenue:90;city=revenue:90;city=revenue:90;city=revenue:90;'\
                       'path=a:0,b:_0;path=a:_0,b:2;path=a:3,b:_1;path=a:_1,b:2;path=a:4,b:_2;'\
-                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=Chi',
+                      'path=a:_2,b:2;path=a:5,b:_3;path=a:_3,b:2;label=StL',
           },
           '611' => 4,
           '619' => 3,
+          'X1' => {
+            'count' => 1,
+            'color' => 'yellow',
+            'code' => 'offboard=revenue:yellow_40|brown_50;path=a:0,b:_0;label=R',
+          },
         }.freeze
 
         LOCATION_NAMES = {
           'A7' => 'Topeka',
-          'B8' => 'Lawrence',
-          'B14' => 'Parsons',
+          'C13' => 'Joplin',
           'A15' => 'Dallas',
           'C5' => 'St. Joseph',
           'C7' => 'Kansas City',
-          'C11' => 'Nevada City',
-          'C13' => 'Joplin',
+          'D4' => 'Cameron',
           'D8' => 'Pleasant Hill',
-          'D14' => 'Aurora',
           'E9' => 'Sedalia',
           'E1' => 'Des Moines',
           'E13' => 'Springfield, MO',
           'E15' => 'Branson',
           'E17' => 'Little Rock',
-          'F4' => 'Macon City',
-          'F12' => 'Lebanon',
+          'F4' => 'Chillicothe',
           'G7' => 'Columbia',
           'G9' => 'Jefferson City',
-          'G11' => 'Rolla',
           'H4' => 'Quincy',
           'H6' => 'Hannibal',
+          'H10' => 'Cuba',
+          'H12' => 'Salem',
           'I7' => 'St. Charles',
-          'I11' => 'Bismarck',
           'J2' => 'Aurora',
           'J4' => 'Chapin',
           'J8' => 'St. Louis',
@@ -151,18 +152,18 @@ module Engine
 
         HEXES = {
           white: {
-            %w[A9 A11 B10 B12 C3 C9 C15 D2 D4 E3 L6 L8 L10 J12 K15] => '',
-            %w[B8 C11 C13 D14 E15 I11 K5] => 'city=revenue:0',
-            %w[D16 F14 F16 G13 H12 H14 G15 I13 I15] => 'upgrade=cost:40,terrain:mountain',
-            ['G11'] => 'city=revenue:0;border=edge:3,type:water,cost:20',
-            ['H10'] => 'border=edge:2,type:water,cost:20',
-            %w[J10] => 'border=edge:4,type:water,cost:40',
-            %w[G3] => 'border=edge:4,type:water,cost:40;border=edge:5,type:water,cost:40',
-            %w[D10] => 'border=edge:4,type:water,cost:40;border=edge:5,type:water,cost:20',
+            %w[A9 A11 B8 B10 B12 C3 C9 C11 C15 D2 D14 E3 F12 I11 L6 L8 L10 J12 K15] => '',
+            %w[C13 D4 E15 F4 H10 H12 K5] => 'city=revenue:0',
+            %w[D16 F14 F16 G13 H14 G15 I13 I15] => 'upgrade=cost:40,terrain:mountain',
+            %w[C13] => 'city=revenue:10;path=a:1,b:_0',
+            ['G11'] => 'border=edge:3,type:water,cost:20',
+            ['J10'] => 'border=edge:4,type:water,cost:40',
+            ['G3'] => 'border=edge:4,type:water,cost:40;border=edge:5,type:water,cost:40',
+            ['D10'] => 'border=edge:4,type:water,cost:40;border=edge:5,type:water,cost:20',
             ['G7'] => 'city=revenue:0;border=edge:0,type:water,cost:20;border=edge:1,type:water,cost:20',
             %w[F10 H8] => 'upgrade=cost:40,terrain:water',
-            %w[K11] => 'upgrade=cost:60,terrain:water',
-            ['J6'] => 'border=edge:0,type:impassable;border=edge:1,type:water,cost:40;border=edge:2,type:water,cost:40',
+            ['K11'] => 'upgrade=cost:60,terrain:water',
+            ['J6'] => 'border=edge:0,type:impassable;border=edge:1,type:water,cost:60;border=edge:2,type:water,cost:40',
             ['E9'] => 'city=revenue:0;border=edge:0,type:water,cost:40;border=edge:1,type:water,cost:40',
             ['K13'] => 'city=revenue:0;border=edge:4,type:water,cost:40',
             ['L12'] => 'border=edge:1,type:water,cost:40',
@@ -176,9 +177,8 @@ module Engine
             ['G5'] => 'border=edge:4,type:water,cost:40',
             ['H4'] => 'city=revenue:0;border=edge:0,type:water,cost:40;border=edge:1,type:water,cost:40;'\
                       'border=edge:2,type:water,cost:40',
-            ['H6'] => 'city=revenue:0;border=edge:3,type:water,cost:40;border=edge:4,type:water,cost:40',
             ['I7'] => 'city=revenue:0;border=edge:0,type:water,cost:40;border=edge:3,type:water,cost:40;'\
-                      'border=edge:4,type:water,cost:40',
+                      'border=edge:4,type:water,cost:60',
             ['I9'] => 'border=edge:3,type:water,cost:40',
             ['I3'] => 'border=edge:5,type:water,cost:20',
             ['I5'] => 'border=edge:0,type:water,cost:40;border=edge:1,type:water,cost:40;'\
@@ -191,46 +191,38 @@ module Engine
             ['F8'] => 'border=edge:3,type:water,cost:20;border=edge:4,type:water,cost:20',
             ['J4'] => 'city=revenue:0;border=edge:1,type:water,cost:20;border=edge:2,type:water,cost:20',
             ['C5'] => 'city=revenue:0;border=edge:1,type:water,cost:20',
+            ['J14'] => 'city=revenue:10;path=a:0,b:_0;',
           },
 
           red: {
-            ['A7'] => 'city=revenue:yellow_30|brown_60,slots:2;path=a:0,b:_0;path=a:5,b:_0',
-            ['A15'] => 'offboard=revenue:yellow_30|brown_60;path=a:4,b:_0',
-            ['E1'] => 'offboard=revenue:yellow_30|brown_60;path=a:0,b:_0',
-            ['E17'] => 'offboard=revenue:yellow_30|brown_60;path=a:2,b:_0;path=a:3,b:_0;path=a:4,b:_0',
-            ['J2'] => 'offboard=revenue:yellow_30|brown_60;path=a:0,b:_0',
-            ['K3'] => 'offboard=revenue:yellow_30|brown_60;path=a:0,b:_0;path=a:1,b:_0',
-            ['K17'] => 'offboard=revenue:yellow_30|brown_60;path=a:2,b:_0',
+            ['A7'] => 'city=revenue:yellow_30|brown_40,slots:2,groups:W;icon=image:1846/30;path=a:0,b:_0;path=a:5,b:_0;label=W',
+            ['A15'] => 'offboard=revenue:yellow_30|brown_60,groups:W;icon=image:1846/30;path=a:4,b:_0;label=W;',
+            ['E1'] => 'offboard=revenue:yellow_20|brown_50;path=a:0,b:_0',
+            ['E17'] => 'offboard=revenue:yellow_20|brown_50;path=a:2,b:_0;path=a:3,b:_0;path=a:4,b:_0',
+            ['J2'] => 'offboard=revenue:yellow_30|brown_40,groups:E;icon=image:1846/20;path=a:0,b:_0;label=E',
+            ['K3'] => 'offboard=revenue:yellow_30|brown_40,groups:E;icon=image:1846/20;path=a:0,b:_0;path=a:1,b:_0;label=E',
+            ['K17'] => 'offboard=revenue:yellow_30|brown_60,groups:E;icon=image:1846/30;path=a:2,b:_0;label=E',
 
           },
           yellow: {
-            ['B14'] => 'city=revenue:10;path=a:1,b:_0;path=a:3,b:_0;path=a:4,b:_0',
-
-            %w[F4] => 'city=revenue:20;path=a:2,b:_0;path=a:5,b:_0',
-            %w[F12] => 'city=revenue:20;path=a:2,b:_0;path=a:4,b:_0',
-            %w[C7] => 'city=revenue:40,slots:2;path=a:1,b:_0;path=a:5,b:_0;label=Z;'\
+            ['B14'] => 'path=a:1,b:4',
+            ['C7'] => 'city=revenue:40,slots:2;path=a:1,b:_0;path=a:5,b:_0;label=Z;'\
                       'upgrade=cost:40,terrain:water',
-            %w[E13] => 'city=revenue:40,slots:2;path=a:0,b:_0;path=a:2,b:_0;label=Z;'\
-                       'upgrade=cost:40,terrain:water;',
-            %w[G9] => 'city=revenue:40,slots:2;path=a:2,b:_0;path=a:4,b:_0;label=Z;'\
+            ['E13'] => 'city=revenue:40,slots:2;path=a:0,b:_0;path=a:2,b:_0;path=a:4,b:_0;label=Z',
+
+            ['G9'] => 'city=revenue:40,slots:2;path=a:2,b:_0;path=a:4,b:_0;label=Z;'\
                       'upgrade=cost:40,terrain:water;border=edge:0,type:water,cost:20;'\
                       'border=edge:5,type:water,cost:20;border=edge:3,type:water,cost:20',
+            ['H6'] => 'city=revenue:20;path=a:2,b:_0;border=edge:3,type:water,cost:40;border=edge:4,type:water,cost:40',
             ['J8'] => 'city=revenue:10;city=revenue:10;city=revenue:10;city=revenue:10;path=a:0,b:_0;'\
-                      'path=a:1,b:_1;path=a:2,b:_2;path=a:5,b:_3;label=Chi;'\
+                      'path=a:1,b:_1;path=a:2,b:_2;path=a:5,b:_3;label=StL;'\
                       'upgrade=cost:40,terrain:water;border=edge:3,type:impassable',
-
+            ['J16'] => 'path=a:3,b:5',
           },
-          green: {
 
-            ['J14'] => 'city=revenue:30,slots:2;path=a:0,b:_0;path=a:3,b:_0;path=a:4,b:_0;'\
-                       'path=a:5,b:_0',
-
-          },
           gray: {
             ['K7'] => 'town=revenue:30;path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;path=a:3,b:_0',
             ['L14'] => 'city=revenue:20;path=a:1,b:_0',
-            ['J16'] => 'path=a:3,b:5',
-
           },
         }.freeze
       end
