6422cf7b21f3a57d136b13d13a96401f7d64a31d
Andy Pymont <andypymont@gmail.com>
2022-02-01 23:33:25 +0000

    18GB: Share price increases up to 4x based on dividend paid

--- lib/engine/game/g_18_gb/step/dividend.rb
+++ lib/engine/game/g_18_gb/step/dividend.rb
@@ -10,6 +10,23 @@ module Engine
           def holder_for_corporation(entity)
             return entity
           end
+
+          def share_price_change(entity, revenue)
+            if revenue.positive?
+              curr_price = entity.share_price.price
+              if revenue >= 4 * curr_price
+                { share_direction: :right, share_times: 4 }
+              elsif revenue >= 3 * curr_price
+                { share_direction: :right, share_times: 3 }
+              elsif revenue >= 2 * curr_price
+                { share_direction: :right, share_times: 2 }
+              else
+                { share_direction: :right, share_times: 1 }
+              end
+            else
+              { share_direction: :left, share_times: 1 }
+            end
+          end
         end
       end
     end
