148d5463f7c62218a96ce3f9182c345e10f65fc7
Dan Schmidt <dfan@dfan.org>
2022-02-04 10:25:51 -0500

    18Ireland: show pool limit for minors (fixes #5422)

--- lib/engine/game/g_18_ireland/game.rb
+++ lib/engine/game/g_18_ireland/game.rb
@@ -50,6 +50,8 @@ module Engine
 
         GAME_END_CHECK = { bankrupt: :immediate, stock_market: :current_round, bank: :full_or }.freeze
 
+        MINOR_MARKET_SHARE_LIMIT = 40
+
         MARKET = [
           ['', '62', '68', '76', '84', '92', '100p', '110', '122', '134', '148', '170', '196', '225', '260e'],
           ['', '58', '64', '70', '78', '85p', '94', '102', '112', '124', '136', '150', '172', '198'],
@@ -500,6 +502,10 @@ module Engine
 
           @corporations = corporations
           @show_majors = false
+        end
+
+        def init_share_pool
+          G18Ireland::SharePool.new(self)
         end
 
         def rust(train)
