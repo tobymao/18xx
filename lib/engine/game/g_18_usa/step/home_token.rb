620a7a4e9cd62239cab571516a9e819a1b6f3d7b
Chris Rericha <crericha@gmail.com>
2022-02-06 15:22:10 -0500

    [18USA] Implement free station and resource subsidies. Includes minor refactoring of subsidies.

--- lib/engine/game/g_18_usa/step/home_token.rb
+++ lib/engine/game/g_18_usa/step/home_token.rb
@@ -10,41 +10,23 @@ module Engine
           def round_state
             super.merge(
               {
-                needed_city_subsidy: 0,
+                minimum_city_subsidy: 0,
               }
             )
           end
 
           def process_place_token(action)
-            # If the corporation's location has a subsidy add it
-            corporation = token.corporation
-            hex = action.city.hex
-            subsidy = @game.subsidies_by_hex.delete(hex.coordinates)
-            if subsidy
-              action.city.hex.tile.icons.reject! { |icon| icon.name.include?('subsidy') }
-              subsidy_company = @game.create_company_from_subsidy(subsidy)
-              assign_boomtown_subsidy(hex, corporation) if subsidy_company.id == 'S8'
-              subsidy_company.owner = corporation
-              corporation.companies << subsidy_company
-            end
-            @round.needed_city_subsidy = 0
+            @game.add_subsidy(token.corporation, action.city.hex)
+            @round.minimum_city_subsidy = 0
             super
-          end
-
-          def assign_boomtown_subsidy(hex, corporation)
-            subsidy = @game.company_by_id('S8')
-            subsidy.all_abilities.each do |ability|
-              ability.hexes << hex.id if ability.type == :tile_lay
-              ability.corporation = corporation.id if ability.type == :close
-            end
           end
 
           def available_hex(_entity, hex)
             return false unless super
-            return true if @round.needed_city_subsidy.zero?
+            return true if @round.minimum_city_subsidy.zero?
 
             city_subsidy = (subsidy = @game.subsidies_by_hex[hex.coordinates]) ? subsidy[:value] : 0
-            city_subsidy >= @round.needed_city_subsidy
+            city_subsidy >= @round.minimum_city_subsidy
           end
         end
       end
