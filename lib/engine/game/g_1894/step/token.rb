0f8abc946c934bc06ebf0ca8059995a824dd5da0
Galatolol <galatolol@protonmail.com>
2022-02-06 18:48:46 +0100

    Remove unused code

--- lib/engine/game/g_1894/step/token.rb
+++ lib/engine/game/g_1894/step/token.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 require_relative '../../../step/token'
-require_relative 'token_tracker'
 
 module Engine
   module Game
     module G1894
       module Step
         class Token < Engine::Step::Token
-          include TokenTracker
-
           def place_token(entity, city, token, connected: true, extra_action: false, special_ability: nil)
             return super if city.hex.name != Engine::Game::G1894::Game::ENGLAND_HEX
 
