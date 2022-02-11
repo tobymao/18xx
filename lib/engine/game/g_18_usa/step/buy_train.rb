a5d7bf2a9782b227eb56f9d3eda59d32d16fbeb1
ventusignis <mgilzinger@google.com>
2022-02-11 13:38:44 -0500

    18USA: Cannot buy pullmans from bank during buy trains phase

--- lib/engine/game/g_18_usa/step/buy_train.rb
+++ lib/engine/game/g_18_usa/step/buy_train.rb
@@ -15,6 +15,9 @@ module Engine
 
           def buyable_trains(entity)
             buyable_trains = super
+            # Cannot buy pullmans from the bank/depot? # TODO: Confirm if this is true and if false, remove this line.
+            # https://github.com/tobymao/18xx/issues/7097
+            buyable_trains.reject! { |t| @game.pullman_train?(t) && t.from_depot? }
             # Cannot buy a pullman if you have a pullman
             buyable_trains.reject! { |t| @game.pullman_train?(t) } if entity.runnable_trains&.any? { |t| @game.pullman_train?(t) }
             buyable_trains
