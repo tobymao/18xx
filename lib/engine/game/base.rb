1f2cca0ea2eab0c171bb95bd6f68bb024ed708a9
Bruce Murphy <pack-github@rattus.net>
2022-02-14 02:32:55 +1100

    Refactor out the special case to avoid a labelled city upgrading to plain yellow tiles now it's needed outside 1870

--- lib/engine/game/base.rb
+++ lib/engine/game/base.rb
@@ -1458,6 +1458,9 @@ module Engine
         return false if from.towns.size != to.towns.size
         return false if !from.label && from.cities.size != to.cities.size
 
+        # but don't permit a labelled city to be downgraded to 0 cities.
+        return false if from.label && !from.cities.empty? && to.cities.empty?
+
         # handle case where we are laying a yellow OO tile and want to exclude single-city tiles
         return false if (from.color == :white) && from.label.to_s == 'OO' && from.cities.size != to.cities.size
 
