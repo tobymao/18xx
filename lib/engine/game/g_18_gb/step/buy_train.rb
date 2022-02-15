b59630521222bfcdde829dd14ebdda83f378b8da
Andy Pymont <andypymont@gmail.com>
2022-02-15 13:52:13 +0000

    18GB Mandatory train buying - Train buying is only mandatory if the corporation can afford the _most expensive_ depot train - If a corporation converts to 10-share to raise emergency funds, it must then buy a new train if it can afford it

--- lib/engine/game/g_18_gb/step/buy_train.rb
+++ lib/engine/game/g_18_gb/step/buy_train.rb
@@ -60,15 +60,19 @@ module Engine
             depot_trains = [] if entity.cash < @depot.min_depot_price
 
             other_trains = @depot.other_trains(entity)
-            other_trains = [] if entity.cash.zero?
+            other_trains = [] if entity.cash.zero? || must_buy_new_train?(entity)
             other_trains.reject! { |t| illegal_train_buy?(entity, t) }
 
             depot_trains + other_trains
           end
 
+          def must_buy_new_train?(entity)
+            must_buy_train?(entity) && @round.emergency_converted
+          end
+
           def must_buy_train?(entity)
-            cheapest = @game.depot.min_depot_price
-            entity.trains.empty? && cheapest.positive? && entity.cash > cheapest
+            mandatory = @game.depot.max_depot_price
+            entity.trains.empty? && mandatory.positive? && entity.cash >= mandatory
           end
         end
       end
