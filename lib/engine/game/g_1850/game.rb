db3c6dcfaf12bbfc1e0dbf6ea7c127ba485606bc
philcampeau <philcampeau@gmail.com>
2022-02-16 12:13:36 -0500

    Update game.rb

--- lib/engine/game/g_1850/game.rb
+++ lib/engine/game/g_1850/game.rb
@@ -129,6 +129,32 @@ module Engine
 
           city_by_id('C20-0-0').place_token(neutral, neutral.next_token)
         end
+
+        # Everything below this line is also included in 1870's game.rb file
+
+        CURRENCY_FORMAT_STR = '$%d'
+
+        BANK_CASH = 12_000
+
+        STARTING_CASH = { 2 => 1050, 3 => 700, 4 => 525, 5 => 420, 6 => 350 }.freeze
+
+        CAPITALIZATION = :full
+
+        MUST_SELL_IN_BLOCKS = true
+
+        MARKET = [
+          %w[64y 68 72 76 82 90 100p 110 120 140 160 180 200 225 250 275 300 325 350 375 400],
+          %w[60y 64y 68 72 76 82 90p 100 110 120 140 160 180 200 225 250 275 300 325 350 375],
+          %w[55y 60y 64y 68 72 76 82p 90 100 110 120 140 160 180 200 225 250i 275i 300i 325i 350i],
+          %w[50o 55y 60y 64y 68 72 76p 82 90 100 110 120 140 160i 180i 200i 225i 250i 275i 300i 325i],
+          %w[40b 50o 55y 60y 64 68 72p 76 82 90 100 110i 120i 140i 160i 180i],
+          %w[30b 40o 50o 55y 60y 64 68p 72 76 82 90i 100i 110i],
+          %w[20b 30b 40o 50o 55y 60y 64 68 72 76i 82i],
+          %w[10b 20b 30b 40o 50y 55y 60y 64 68i 72i],
+          %w[0c 10b 20b 30b 40o 50y 55y 60i 64i],
+          %w[0c 0c 10b 20b 30b 40o 50y],
+          %w[0c 0c 0c 10b 20b 30b 40o],
+        ].freeze
       end
     end
   end
