f5e2fd60545366ed64604d4aa522be8f53466944
philcampeau <philcampeau@gmail.com>
2022-02-16 12:14:01 -0500

    Update meta.rb

--- lib/engine/game/g_1850/meta.rb
+++ lib/engine/game/g_1850/meta.rb
@@ -1,24 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../meta'
-require_relative '../g_1870/meta'
+# require_relative '../g_1870/meta'
 
 module Engine
   module Game
     module G1850
       module Meta
         include Game::Meta
-<<<<<<< HEAD
-        include G1870::Meta
-=======
-        # include G1870::Meta
 
         DEV_STAGE = :prealpha
         # DEPENDS_ON = '1870'
->>>>>>> 31f57cf09d934d4dce1615c86f4df8fbe3dba5c0
-
-        DEV_STAGE = :prealpha
-        DEPENDS_ON = '1870'
         GAME_TITLE = '1850'
         GAME_SUBTITLE = 'The MidWest'
         GAME_DESIGNER = 'Bill Dixon'
