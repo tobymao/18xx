c94ae674f41cc4506301b72e272df6abe4b6f6d7
R. Ryan Driskel <ryandriskel@gmail.com>
2022-02-17 07:07:01 -0700

    18MT Wiki Links, Remove impossible tile

--- lib/engine/game/g_18_mt/meta.rb
+++ lib/engine/game/g_18_mt/meta.rb
@@ -14,7 +14,12 @@ module Engine
         GAME_DESIGNER = 'R. Ryan Driskel'
         GAME_LOCATION = 'Montana, USA'
         GAME_TITLE = '18MT: Big Sky Barons'
-        GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18MT'
+        GAME_RULES_URL = {
+          'Wiki Rules Highlights' => 'https://github.com/tobymao/18xx/wiki/18MT:-Big-Sky-Barons',
+          'Contact Ryan on the 18xx Slack' => 'https://app.slack.com/client/T67762LUR/C012K0CNY5C',
+          'Playtest Forum (BoardGameGeek, best place to discuss)' => 'https://boardgamegeek.com/thread/2816504/18mt-big-sky-barons-playtesting',
+        }.freeze
+        GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18MT:-Big-Sky-Barons'
 
         PLAYER_RANGE = [3, 5].freeze
       end
