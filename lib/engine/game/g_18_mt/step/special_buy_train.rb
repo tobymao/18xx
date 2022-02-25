738edd0c49c0f030d41c51b56472f659704455af
R. Ryan Driskel <ryandriskel@gmail.com>
2022-02-25 12:29:25 -0700

    18MT Fix 7293 Only allow special train buy during buy train

--- lib/engine/game/g_18_mt/step/special_buy_train.rb
+++ lib/engine/game/g_18_mt/step/special_buy_train.rb
@@ -10,6 +10,13 @@ module Engine
         class SpecialBuyTrain < Engine::Step::SpecialBuyTrain
           include G18MT::Train
 
+          def actions(entity)
+            return [] unless ability(entity)
+            return [] unless @round.train_buy_available
+
+            ACTIONS
+          end
+
           def buyable_trains(entity)
             trains = @game.abilities(entity, :train_discount, time: ability_timing)&.trains || []
             super.select { |t| trains.include?(t.name) && t.from_depot? }
