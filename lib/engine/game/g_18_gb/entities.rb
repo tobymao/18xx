f64f267b25a60449d7d6db4cb3fa2b1d94f8ed36
Andy Pymont <andypymont@gmail.com>
2022-02-25 21:49:12 +0000

    18GB Add more detail to GN/MC private company descriptions

--- lib/engine/game/g_18_gb/entities.rb
+++ lib/engine/game/g_18_gb/entities.rb
@@ -65,8 +65,12 @@ module Engine
             name: 'Great Northern',
             value: 70,
             revenue: 25,
-            desc: 'The GN allows a corporation to lay a free Station Marker in York (I14). The GN owner may use this ability ' \
-                  'once per game, after the GN has closed, for any corporation which they control.',
+            desc: 'The GN allows a corporation to lay a Station Marker in York (I14). A space is reserved for the GN until the '\
+                  'blue phase, otherwise an empty space must be available in York. The GN owner may use this ability ' \
+                  'once per game, after the GN has closed, for any corporation which they control. The station is free if the ' \
+                  'corporation can trace a route to York, otherwise it costs £50. After laying the marker, the corporation ' \
+                  'also gains the ability to lay a green tile in York as one of its standard tile actions, instead of the ' \
+                  'usual yellow tile, even before green tiles are normally available.',
             sym: 'GN',
             color: nil,
             abilities: [
@@ -232,8 +236,12 @@ module Engine
             name: 'Maryport & Carlisle',
             value: 60,
             revenue: 20,
-            desc: 'The MC allows a corporation to lay a Station Marker in Carlisle (H9). The MC owner may use this ability ' \
-                  'once per game, after the MC has closed, for any corporation which they control.',
+            desc: 'The MC allows a corporation to lay a Station Marker in Carlisle (H9). A space is reserved for the MC until ' \
+                  'the blue phase, otherwise an empty space must be available in Carlisle. The MC owner may use this ability ' \
+                  'once per game, after the MC has closed, for any corporation which they control. The station is free if the ' \
+                  'corporation can trace a route to Carlisle, otherwise it costs £50.  After laying the marker, the ' \
+                  'corporation also gains the ability to lay a green tile in Carlisle as one of its standard tile actions, ' \
+                  'instead of the usual yellow tile, even before green tiles are normally available.',
             sym: 'MC',
             color: nil,
             abilities: [
