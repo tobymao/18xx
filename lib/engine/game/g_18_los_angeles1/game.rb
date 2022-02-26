283b92dd854f2b36bf16eb4781b46fe171b53308
Michael Brandt <michaelbrandt5@gmail.com>
2022-02-25 23:32:26 -0700

    18LA2: map and basic entity changes (primarily config changes)
    only real code change here is in g_18_los_angeles1/game.rb to load the
    "override" entities for the first edition, everything else is config
    
    map changes:
    
    * various icons, terrain, and revenue changes
    * new "LAX" hex at D1
    * some new gray hexes with offboards to nowhere, allowing more flexible track
      upgrades on the map in certain cases
    
    corporation changes:
    
    * some reservations removed/changed
    * PER has no fixed home (choosing home is implemented in a separate commit)
    
    private company changes:
    
    * South Bay Line removed
    * Puente Trolley revenue decreased from $15 to $10
    * Chino Hill Excavation price reduced from $60 to $50, gained ability to also
      discount water terrain (TODO: prevent discounts from stacking)
    * hexes changed for Citrus and Steamship companies
    * 4 new companies added
        * price and revenue implemented, abilities not implemented yet

--- lib/engine/game/g_18_los_angeles1/game.rb
+++ lib/engine/game/g_18_los_angeles1/game.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../g_18_los_angeles/game'
+require_relative 'entities'
+require_relative 'map'
 require_relative 'meta'
 
 module Engine
@@ -8,6 +10,22 @@ module Engine
     module G18LosAngeles1
       class Game < G18LosAngeles::Game
         include_meta(G18LosAngeles1::Meta)
+        include Entities
+        include Map
+
+        def game_companies
+          @game_companies ||=
+            self.class::COMPANIES + (G18LosAngeles::Game::COMPANIES.slice(0, 11).map do |company|
+                                       self.class::COMPANIES_1E[company[:sym]] || company
+                                     end)
+        end
+
+        def game_corporations
+          @game_corporations ||=
+            G18LosAngeles::Game::CORPORATIONS.map do |company|
+              self.class::CORPORATIONS_1E[company[:sym]] || company
+            end
+        end
       end
     end
   end
