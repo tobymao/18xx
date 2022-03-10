tobymao/18xx

commit 6af9cd7cb5fc3dc464c34dfa7c63e78b43e253dd

Fixed incorrect regions_weights for labels.

assets/app/view/game/part/label.rb
@@ -17,7 +17,7 @@ module View
             y: 0,
           },
           pointy: {
-            region_weights: { [5, 6] => 1.0, [7, 12] => 0.25 },
+            region_weights: { [0, 1] => 1.0, [2, 6] => 0.25 },
             x: -65,
             y: 0,
           },
@@ -30,7 +30,7 @@ module View
             y: 0,
           },
           pointy: {
-            region_weights: { [17, 18] => 1.0, [11, 16] => 0.25 },
+            region_weights: { [22, 23] => 1.0, [17, 21] => 0.25 },
             x: 65,
             y: 0,
           },

commit 2b9775dc46b44a66948cbde4d235aa186c60725c

Fixed incorrect region_weights for labels introduced in 6af9cd7cb5fc3dc464c34dfa7c63e78b43e253dd.

assets/app/view/game/part/label.rb
@@ -17,7 +17,7 @@ module View
             y: 0,
           },
           pointy: {
-            region_weights: { [0, 1] => 1.0, [2, 6] => 0.25 },
+            region_weights: { [5, 12] => 1.0, [6] => 0.25 },
             x: -65,
             y: 0,
           },
@@ -30,7 +30,7 @@ module View
             y: 0,
           },
           pointy: {
-            region_weights: { [22, 23] => 1.0, [17, 21] => 0.25 },
+            region_weights: { [11, 18] => 1.0, [17] => 0.25 },
             x: 65,
             y: 0,
           },