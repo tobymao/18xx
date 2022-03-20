b1926526c1a1f3de9e350aa6e25de223df5ecec0
Dan Schmidt <dfan@dfan.org>
2022-03-20 09:22:48 -0400

    Fix spelling of Tallahassee

--- lib/engine/game/g_18_fl/game.rb
+++ lib/engine/game/g_18_fl/game.rb
@@ -145,7 +145,7 @@ module Engine
           'B5' => 'Mobile',
           'B7' => 'Pensacola',
           'B13' => 'Chattahoochee',
-          'B15' => 'Talahassee',
+          'B15' => 'Tallahassee',
           'B19' => 'Lake City',
           'B23' => 'Jacksonville',
           'C14' => 'St. Marks',
@@ -293,7 +293,7 @@ module Engine
 
         COMPANIES = [
           {
-            name: 'Talahassee Railroad',
+            name: 'Tallahassee Railroad',
             value: 0,
             discount: -20,
             revenue: 5,
