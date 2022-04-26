a7f9b5682d1cb1703e5416392d9ae088cd27acd3
Christopher Giroir <kelsin@valefor.com>
2022-04-25 17:15:16 -0700

    fix: attempt to make rounding issues on PEIR better

--- lib/engine/game/g_1871/game.rb
+++ lib/engine/game/g_1871/game.rb
@@ -652,14 +652,16 @@ module Engine
             return
           end
 
-          new_share_percent = (100 / @peir_shares.size).ceil
+          new_share_percent = (100.0 / @peir_shares.size).round(2)
           @peir.forced_share_percent = new_share_percent
-          @log << "PEIR shares are now worth #{new_share_percent}%"
           peir.share_holders.clear
           @peir_shares.each do |share|
             share.percent = new_share_percent
             peir.share_holders[share.owner] ||= 0
             peir.share_holders[share.owner] += new_share_percent
+          end
+          peir.share_holders.each do |owner, amount|
+            @log << "#{owner.name} now owns #{amount}% of the PEIR"
           end
 
           peir.owner = peir_owner
