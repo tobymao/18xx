4ce90aa2d54c0609bec5f2bd57e899fe8ff9f34a
Andy Pymont <andypymont@gmail.com>
2022-04-29 13:12:53 +0100

    1861: Fix bug where nationalised Nikolaev blocked other minors from starting

--- lib/engine/game/g_1861/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_1861/step/buy_sell_par_shares.rb
@@ -8,11 +8,12 @@ module Engine
       module Step
         class BuySellParShares < G1867::Step::BuySellParShares
           def ipo_type(entity)
-            if entity.type == :minor && entity.id != 'N' && !@game.corporation_by_id('N').ipoed
+            return 'No home token locations are available' if entity.type == :major && @game.home_token_locations(entity).empty?
+
+            nikolaev = @game.corporation_by_id('N')
+            if entity.type == :minor && entity.id != 'N' && !(nikolaev.ipoed || nikolaev.closed?)
               return 'Nikolaev must be the first corporation'
             end
-
-            return 'No home token locations are available' if entity.type == :major && @game.home_token_locations(entity).empty?
 
             super
           end
