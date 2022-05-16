33c4325ff2250a63dc7b30758b7da1c85f83f29c
Michael Alexander <outsidepasser@gmail.com>
2022-05-15 23:28:54 -0400

    [1822MX] Removed multi-condition unless

--- lib/engine/game/g_1822_mx/step/track.rb
+++ lib/engine/game/g_1822_mx/step/track.rb
@@ -50,7 +50,7 @@ module Engine
             @log << "Tile placement for NDEM by #{@ndem_tile_layers.first.name}" if current_entity == @game.ndem
             if action.tile.id == 'BC-0'
               tile_lay = get_tile_lay(action.entity)
-              raise GameError, 'Cannot lay a builder cube now' unless tile_lay && tile_lay[:lay]
+              raise GameError, 'Cannot lay a builder cube now' if !tile_lay || !tile_lay[:lay]
 
               @log << "#{action.entity.name} places builder cube on #{action.hex.name}"
               action.hex.tile.icons << Part::Icon.new('../icons/1822_mx/red_cube', 'block')
