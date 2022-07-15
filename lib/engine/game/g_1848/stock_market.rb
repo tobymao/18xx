c2e58456b988f9588b1bb76ae07ece67cff389ba
bena <bena@il.ibm.com>
2022-07-15 07:13:45 +0300

    fix stocl market logic and constant ref

--- lib/engine/game/g_1848/stock_market.rb
+++ lib/engine/game/g_1848/stock_market.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../stock_market'
+require_relative 'game'
 
 module Engine
   module Game
@@ -8,7 +9,7 @@ module Engine
       class StockMarket < Engine::StockMarket
         def move_down(corporation)
           r, c = corporation.share_price.coordinates
-          r += 1 if (r + 1 < @market.size || r + 1 != Game::BOE_ROW) && share_price(r + 1, c)
+          r += 1 if r + 1 < @market.size && r + 1 != G1848::Game::BOE_ROW && share_price(r + 1, c)
           move(corporation, r, c)
         end
       end
