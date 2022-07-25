6979e101b64e233d31aa631b5158e10e0d40458b
bena <bena@il.ibm.com>
2022-07-25 14:54:44 +0300

    move add_neutral_tokens to init_hexes so that it shows up in starting map

--- lib/engine/game/g_1867/game.rb
+++ lib/engine/game/g_1867/game.rb
@@ -1464,21 +1464,21 @@ module Engine
           @final_operating_rounds || super
         end
 
-        def add_neutral_tokens
+        def add_neutral_tokens(hexes)
           @green_tokens = []
           logo = '/logos/1867/neutral.svg'
-          @hexes.each do |hex|
+          hexes.each do |hex|
             case hex.id
             when 'D2'
-              token = Token.new(@national, price: 0, logo: logo, simple_logo: logo, type: :neutral)
+              token = Token.new(national, price: 0, logo: logo, simple_logo: logo, type: :neutral)
               hex.tile.cities.first.exchange_token(token)
               @green_tokens << token
             when 'L12'
-              token = Token.new(@national, price: 0, logo: logo, simple_logo: logo, type: :neutral)
+              token = Token.new(national, price: 0, logo: logo, simple_logo: logo, type: :neutral)
               hex.tile.cities.last.exchange_token(token)
               @green_tokens << token
             when 'F16'
-              hex.tile.cities.first.exchange_token(@national.tokens.first)
+              hex.tile.cities.first.exchange_token(national.tokens.first)
             end
           end
         end
@@ -1515,20 +1515,29 @@ module Engine
           @hidden_company = company_by_id('3')
 
           # CN corporation only exists to hold tokens
-          @national = @corporations.find { |c| c.type == :national }
+          @national = national
           @national.ipoed = true
           @national.shares.clear
           @national.shares_by_corporation[@national].clear
 
           @national_reservations = self.class::NATIONAL_RESERVATIONS.dup
           @corporations.delete(@national)
-          add_neutral_tokens
 
           # Move green and majors out of the normal list
           @corporations, @future_corporations = @corporations.partition do |corporation|
             corporation.type == :minor && !self.class::GREEN_CORPORATIONS.include?(corporation.id)
           end
           @show_majors = false
+        end
+
+        def national
+          @national ||= @corporations.find { |c| c.type == :national }
+        end
+
+        def init_hexes(_companies, corporations)
+          hexes = super
+          add_neutral_tokens(hexes)
+          hexes
         end
 
         def event_green_minors_available!
