678fd7f250c68c3f258ad79e1dfc0346e851f853
perwestling <per.o.westling@gmail.com>
2022-08-02 12:55:25 +0200

    18SJ: Fix IPO bugs (fixes #8128)
    Need to check IPO shares when doing buy and par.

--- lib/engine/game/g_18_sj/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_sj/step/buy_sell_par_shares.rb
@@ -10,6 +10,22 @@ module Engine
           def can_sell?(entity, bundle)
             super && (@game.oscarian_era || bundle.corporation.floated?)
           end
+
+          def can_ipo_any?(entity)
+            !bought? && @game.corporations.any? do |c|
+              @game.can_par?(c, entity) && can_buy?(entity, c.ipo_shares.first&.to_bundle)
+            end
+          end
+
+          # FIXME: copied from 21moon which has "FIXME: move to common location"
+          def can_buy_any_from_ipo?(entity)
+            @game.corporations.each do |corporation|
+              next unless corporation.ipoed
+              return true if can_buy_shares?(entity, corporation.ipo_shares)
+            end
+
+            false
+          end
         end
       end
     end
