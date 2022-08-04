1bbfd79288e22b5499f8247062de56847c38a548
Dan Schmidt <dfan@dfan.org>
2022-08-04 15:10:22 -0400

    [18Ireland] Correct merge vote log message (fixes #8136)

--- lib/engine/game/g_18_ireland/step/merger_common.rb
+++ lib/engine/game/g_18_ireland/step/merger_common.rb
@@ -18,7 +18,11 @@ module Engine
               @round.vote_outcome = :for
               @round.to_vote = []
             elsif @round.to_vote.empty?
-              @game.log << 'Votes are tied and proposal is rejected'
+              @game.log << if @round.votes_against > @round.votes_for
+                             'Majority has voted against merger and proposal is rejected'
+                           else
+                             'Votes are tied and proposal is rejected'
+                           end
               @round.vote_outcome = :against
             elsif @round.votes_available - @round.votes_against < @round.votes_needed
               @game.log << "#{@round.votes_needed} votes in favor cannot be reached and proposal is rejected"
