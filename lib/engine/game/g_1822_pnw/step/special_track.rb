dbc69aad83383d1fb7e9f975df6e784018ee59ea
Michael Alexander <outsidepasser@gmail.com>
2022-08-13 22:04:35 -0400

    [1822PNW] Privates 10 to 13

--- lib/engine/game/g_1822_pnw/step/special_track.rb
+++ lib/engine/game/g_1822_pnw/step/special_track.rb
@@ -7,7 +7,32 @@ module Engine
     module G1822PNW
       module Step
         class SpecialTrack < Engine::Game::G1822::Step::SpecialTrack
+          PORT_TILES = %w[P1-0 P2-0].freeze
+
+          def available_hex(entity, hex)
+            if @game.port_company?(entity)
+              return nil unless abilities(entity).hexes.include?(hex.id)
+              return [hex.tile.exits] if hex.tile.color == :blue && !PORT_TILES.include?(hex.tile.id)
+
+              return nil
+            end
+            if @game.cube_company?(entity)
+              return @game.can_hold_builder_cubes?(hex.tile) && @game.graph.connected_hexes(entity.owner)[hex]
+            end
+
+            super
+          end
+
           def potential_tiles(entity, hex)
+            if @game.port_company?(entity)
+              return [] if PORT_TILES.include?(hex.tile.id)
+
+              tile_ability = abilities(entity)
+              tile = @game.tiles.find { |t| t.name == tile_ability.tiles[0] }
+              return [tile]
+            elsif @game.cube_company?(entity)
+              return @game.can_hold_builder_cubes?(hex.tile) ? [@game.tile_by_id('BC-0')] : []
+            end
             tiles = super
             if @game.can_hold_builder_cubes?(hex.tile)
               cube_tile = @game.tile_by_id('BC-0')
@@ -16,7 +41,8 @@ module Engine
             tiles
           end
 
-          def legal_tile_rotation?(_entity, hex, tile)
+          def legal_tile_rotation?(entity, hex, tile)
+            return hex.tile.paths.any? { |p| p.exits == tile.exits } if @game.port_company?(entity)
             return true if tile.id == 'BC-0'
             return true if @game.legal_leavenworth_tile(hex, tile)
 
@@ -30,7 +56,17 @@ module Engine
           end
 
           def process_lay_tile(action)
-            if @game.company_ability_extra_track?(action.entity) && action.tile.id == 'BC-0'
+            if @game.cube_company?(action.entity)
+              @log << "#{action.entity.name} places builder cube on #{action.hex.name}"
+              action.hex.tile.icons << Part::Icon.new('../icons/1822_mx/red_cube', 'block')
+              ability = abilities(action.entity)
+              ability.use!
+              if ability.count.zero? && ability.closed_when_used_up
+                company = ability.owner
+                @log << "#{company.name} closes"
+                company.close!
+              end
+            elsif @game.company_ability_extra_track?(action.entity) && action.tile.id == 'BC-0'
               @log << "#{action.entity.name} places builder cube on #{action.hex.name}"
               action.hex.tile.icons << Part::Icon.new('../icons/1822_mx/red_cube', 'block')
               ability = abilities(action.entity)
