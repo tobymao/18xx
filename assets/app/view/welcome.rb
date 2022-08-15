6755ade33edc12fde61a94a42dbfde7130eb618a
tobymao <toby.mao@gmail.com>
2022-08-14 18:58:05 -0700

    welcome

--- assets/app/view/welcome.rb
+++ assets/app/view/welcome.rb
@@ -17,8 +17,7 @@ module View
 
     def render_notification
       message = <<~MESSAGE
-        <p><a href="https://github.com/tobymao/18xx/wiki/1848">1848</a> is now in alpha.</p>
-        <p><a href="https://www.kickstarter.com/projects/ambierona/board-game-day?ref=5ripxz">Board Game Day</a> a rhyming children's book by Ambie is now available on Kickstarter!</p>
+        <p><a href="https://www.kickstarter.com/projects/ambierona/board-game-day?ref=5ripxz">Board Game Day</a> a rhyming children's book by Ambie ends in soon! Shikoku: 1889 is featured in it!</p>
         <p>Learn how to get <a href='https://github.com/tobymao/18xx/wiki/Notifications'>notifications</a> by email, Slack, Discord, and Telegram.</p>
         <p>Please submit problem reports and make suggestions for improvements on
         <a href='https://github.com/tobymao/18xx/issues'>GitHub</a>. Join the
