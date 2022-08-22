24f3cca3626a452317eedae7a605de964da2f681
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2022-08-22 21:26:04 +0100

    [1858] Add company/train/market definitions

---
+++ lib/engine/game/g_1858/market.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G1858
+      module Market
+        CURRENCY_FORMAT_STR = '₧%d'
+        BANK_CASH = 12_000
+        CAPITALIZATION = :incremental
+        MUST_SELL_IN_BLOCKS = true
+        SELL_BUY_ORDER = :sell_buy
+        SELL_AFTER = :operate
+        CERT_LIMIT = { 3 => 21, 4 => 16, 5 => 13, 6 => 11 }.freeze
+        STARTING_CASH = { 3 => 500, 4 => 375, 5 => 300, 6 => 250 }.freeze
+
+        MARKET = [
+          %w[0
+             50
+             60
+             65
+             70
+             80
+             90
+             100
+             110
+             120
+             135
+             150
+             165
+             180
+             200
+             220
+             245
+             270
+             300],
+        ].freeze
+      end
+    end
+  end
+end
---
+++ lib/engine/game/g_1858/trains.rb
@@ -0,0 +1,155 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G1858
+      module Trains
+        PHASES = [
+          {
+            name: '2',
+            train_limit: 4,
+            tiles: [:yellow],             # FIXME: narrow gauge tiles not available until phase 3
+            status: ['minor_limit_two'],
+            operating_rounds: 2,
+          },
+          {
+            name: '3',
+            on: %w[4H 2M],
+            train_limit: 4,
+            tiles: %i[yellow green],
+            status: ['minor_limit_two'],
+            operating_rounds: 2,
+          },
+          {
+            name: '4',
+            on: %w[6H 3M],
+            train_limit: 3,
+            tiles: %i[yellow green],
+            status: ['minor_limit_one'],
+            operating_rounds: 2,
+          },
+          {
+            name: '5',
+            on: %w[5E 4M],
+            train_limit: 2,
+            tiles: %i[yellow green brown],
+            status: %w[minor_limit_one normal_formation],
+            operating_rounds: 2,
+          },
+          {
+            name: '6',
+            on: %w[6E 5M],
+            train_limit: 2,
+            tiles: %i[yellow green brown],
+            status: ['normal_formation'],
+            operating_rounds: 2,
+          },
+          {
+            name: '7',
+            on: %w[7E 6M],
+            train_limit: 2,
+            tiles: %i[yellow green brown gray],
+            status: ['normal_formation'],
+            operating_rounds: 2,
+          },
+        ].freeze
+
+        TRAINS = [
+          {
+            name: '2H',
+            obsolete_on: %w[6H 3M],
+            rusts_on: %w[6E 5M],
+            distance: 2,
+            price: 100,
+            num: 6,
+          },
+          {
+            name: '4H',
+            obsolete_on: %w[6E 5M],
+            rusts_on: %w[7E 6M],
+            distance: 4,
+            price: 200,
+            num: 5,
+            variants: [
+              {
+                name: '2M',
+                distance: [{ 'nodes' => %w[city offboard], 'pay' => 2, 'visit' => 2 },
+                           { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                price: 100,
+              },
+            ],
+          },
+          {
+            name: '6H',
+            obsolete_on: %w[7E 6M],
+            rusts_on: %w[5D],
+            distance: 6,
+            price: 300,
+            num: 4,
+            variants: [
+              {
+                name: '3M',
+                distance: [{ 'nodes' => %w[city offboard], 'pay' => 3, 'visit' => 3 },
+                           { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                price: 200,
+              },
+            ],
+          },
+          {
+            name: '5E',
+            distance: [{ 'nodes' => %w[city offboard], 'pay' => 5, 'visit' => 5 },
+                       { 'nodes' => %w[town], 'pay' => 99, 'visit' => 0 }],
+            price: 500,
+            num: 3,
+            variants: [
+              {
+                name: '4M',
+                distance: [{ 'nodes' => %w[city offboard], 'pay' => 4, 'visit' => 4 },
+                           { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                price: 400,
+              },
+            ],
+          },
+          {
+            name: '6E',
+            distance: [{ 'nodes' => %w[city offboard], 'pay' => 6, 'visit' => 6 },
+                       { 'nodes' => %w[town], 'pay' => 99, 'visit' => 0 }],
+            price: 650,
+            num: 2,
+            variants: [
+              {
+                name: '5M',
+                distance: [{ 'nodes' => %w[city offboard], 'pay' => 5, 'visit' => 5 },
+                           { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                price: 550,
+              },
+            ],
+          },
+          {
+            name: '7E',
+            distance: [{ 'nodes' => %w[city offboard], 'pay' => 7, 'visit' => 7 },
+                       { 'nodes' => %w[town], 'pay' => 99, 'visit' => 0 }],
+            price: 800,
+            num: 16,
+            variants: [
+              {
+                name: '6M',
+                distance: [{ 'nodes' => %w[city offboard], 'pay' => 6, 'visit' => 6 },
+                           { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                price: 700,
+              },
+            ],
+          },
+          {
+            name: '5D',
+            distance: [{ 'nodes' => %w[city offboard], 'pay' => 5, 'visit' => 5 },
+                       { 'nodes' => %w[town], 'pay' => 99, 'visit' => 0 }],
+            price: 1_100,
+            num: 8,
+            multiplier: 2,
+          },
+        ].freeze
+      end
+    end
+  end
+end
