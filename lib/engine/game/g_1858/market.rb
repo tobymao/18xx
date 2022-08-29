8bf79da319f91e6ea95b6d9511da3330a9b267e3
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2022-08-29 10:18:25 +0100

    [1858] More compact definition of the stock market

--- lib/engine/game/g_1858/market.rb
+++ lib/engine/game/g_1858/market.rb
@@ -13,27 +13,7 @@ module Engine
         CERT_LIMIT = { 3 => 21, 4 => 16, 5 => 13, 6 => 11 }.freeze
         STARTING_CASH = { 3 => 500, 4 => 375, 5 => 300, 6 => 250 }.freeze
 
-        MARKET = [
-          %w[0c
-             50
-             60
-             65
-             70p
-             80p
-             90p
-             100p
-             110p
-             120p
-             135p
-             150p
-             165
-             180
-             200
-             220
-             245
-             270
-             300],
-        ].freeze
+        MARKET = [%w[0c 50 60 65 70p 80p 90p 100p 110p 120p 135p 150p 165 180 200 220 245 270 300]].freeze
       end
     end
   end
