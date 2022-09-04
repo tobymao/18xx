88cf7cd4f4f88b045a4bb18c512bc777ab9182e4
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2022-09-04 17:48:39 +0100

    [1858] Public companies can split dividends

--- lib/engine/game/g_1858/step/dividend.rb
+++ lib/engine/game/g_1858/step/dividend.rb
@@ -7,8 +7,21 @@ module Engine
     module G1858
       module Step
         class Dividend < Engine::Step::Dividend
+          DIVIDEND_TYPES = %i[payout half withhold].freeze
+
           def rust_obsolete_trains!(entity)
             # Wounded trains are not discarded after running
+          end
+
+          def payout_per_share(entity, revenue)
+            (revenue / entity.total_shares.to_f).floor.to_i
+          end
+
+          def half(entity, revenue)
+            dividend = payout_per_share(entity, revenue / 2)
+            withheld = revenue - (dividend * entity.total_shares)
+            puts "revenue: #{revenue}, shares: #{entity.total_shares}, dividend: #{dividend.to_f}, withheld: #{withheld}"
+            { corporation: withheld, per_share: dividend }
           end
         end
       end
