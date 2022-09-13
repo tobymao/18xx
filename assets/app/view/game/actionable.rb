9c97efe114d90fe16a72a5d56e03cd290342b314
Chris Rericha <crericha@gmail.com>
2022-09-12 22:07:14 -0400

    Set finished_at on the server and fix manually_ended logic.

--- assets/app/view/game/actionable.rb
+++ assets/app/view/game/actionable.rb
@@ -95,12 +95,12 @@ module View
 
         if game.finished
           @game_data[:result] = game.result
-          @game_data[:finished_at] = Time.now.to_i
           @game_data[:manually_ended] = game.manually_ended
           @game_data[:status] = 'finished'
         else
           @game_data[:result] = {}
           @game_data[:status] = 'active'
+          @game_data[:manually_ended] = nil
         end
 
         if hotseat?
@@ -115,7 +115,6 @@ module View
             meta = {
               game_result: @game_data[:result],
               game_status: @game_data[:status],
-              finished_at: @game_data[:finished_at],
               manually_ended: @game_data[:manually_ended],
               active_players: game.active_players_id,
               turn: game.turn,
