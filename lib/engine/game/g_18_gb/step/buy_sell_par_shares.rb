612625a53933850a3c56ee6624ec5282b6322c05
andypymont <andypymont@gmail.com>
2022-09-18 18:24:12 +0100

    18GB: Fix code smell issue in already_president? check in buy_sell_par_shares (remove the !.select.empty? and replace with .include?)

--- lib/engine/game/g_18_gb/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_gb/step/buy_sell_par_shares.rb
@@ -110,7 +110,7 @@ module Engine
           end
 
           def already_president?(corporation)
-            !@round.presidencies.select { |corp| corp == corporation }.empty?
+            @round.presidencies.include?(corporation)
           end
 
           def can_buy_multiple?(entity, corporation, owner)
