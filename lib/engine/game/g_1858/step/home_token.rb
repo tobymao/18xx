5b670e003425b0de8d974d921b3df7bdc0fd15fd
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2022-09-22 23:21:23 +0100

    [1858] Add a bit more info about the game-corrupting undo bug

--- lib/engine/game/g_1858/step/home_token.rb
+++ lib/engine/game/g_1858/step/home_token.rb
@@ -12,6 +12,13 @@ module Engine
             city = action.city
             color = city.tile.color
             token.price = 2 * city.revenue[color] unless color == :white
+
+            # FIXME
+            # There is a bug that is corrupting games, it occurs here when
+            # undoing moves, the city passed as part of the action object did
+            # not have an associated hex. Maybe a bug at a higher level?
+            # Having a company start with a token in Lisboa is a reliable way
+            # of triggering this bug.
             super
           end
         end
