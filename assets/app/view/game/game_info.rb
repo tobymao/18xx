b93b908ef4ac376e729c6f11ca24ffdbf683f1f7
Andrew Zwicky <andrew.zwicky@gmail.com>
2022-10-06 11:14:37 -0500

    Any

--- assets/app/view/game/game_info.rb
+++ assets/app/view/game/game_info.rb
@@ -181,7 +181,7 @@ module View
       def trains
         rust_schedule, obsolete_schedule = rust_obsolete_schedule
 
-        show_obsolete_schedule = !obsolete_schedule.keys.compact.empty?
+        show_obsolete_schedule = obsolete_schedule.keys.any?
         show_upgrade = @depot.upcoming.any? { |train| train.variants.any? { |_k, v| v['discount'] } }
         show_salvage = @depot.trains.any?(&:salvage)
         show_available = @depot.upcoming.any?(&:available_on)
