tobymao/18xx

commit 803e794452c4733f65ccd13583037aeda0f420e5

Extend Engine::Step::BuySellParShareViaBid to handle companies
1858 has auctions of private companies that are performed in a similar
fashion to the minor company auctions in 1861 and 1867. This modifies
the auction code so that, with a few overrides in a subclass, it can be
used for company auctions as well as corporations.

lib/engine/step/buy_sell_par_shares_via_bid.rb
@@ -7,6 +7,7 @@ module Engine
   module Step
     class BuySellParSharesViaBid < BuySellParShares
       # Common code between 1867, 18Ireland, and 1877: Stockholm Tramways for auctioning corporations via bid
+      # Can also be used to auction private companies.
       include Engine::Step::PassableAuction
 
       def actions(entity)
@@ -17,6 +18,10 @@ module Engine
         actions << 'bid' if !bought? && can_bid?(entity)
         actions << 'pass' if actions.any? && !actions.include?('pass') && !must_sell?(entity)
         actions
+      end
+
+      def auctioning_company
+        @auctioning
       end
 
       def auctioning_corporation
@@ -57,15 +62,15 @@ module Engine
       end
 
       def add_bid(action)
-        entity = action.entity
-        corporation = action.corporation
+        player = action.entity
+        entity = action.corporation || action.company
         price = action.price
 
         if @auctioning
-          @log << "#{entity.name} bids #{@game.format_currency(price)} for #{corporation.name}"
+          @log << "#{player.name} bids #{@game.format_currency(price)} for #{entity.name}"
         else
-          @log << "#{entity.name} auctions #{corporation.name} for #{@game.format_currency(price)}"
-          @game.place_home_token(action.corporation) if @game.class::HOME_TOKEN_TIMING == :par
+          @log << "#{player.name} auctions #{entity.name} for #{@game.format_currency(price)}"
+          @game.place_home_token(action.entity) if entity.corporation? && @game.class::HOME_TOKEN_TIMING == :par
         end
         super(action)
 

commit 495ad0c1542274f8d92fb76a647117a8982b8203

Fix breakage in minor auctions for 1861/1867/...
Error introduced in commit 803e79445.

lib/engine/step/buy_sell_par_shares_via_bid.rb
@@ -70,7 +70,7 @@ module Engine
           @log << "#{player.name} bids #{@game.format_currency(price)} for #{entity.name}"
         else
           @log << "#{player.name} auctions #{entity.name} for #{@game.format_currency(price)}"
-          @game.place_home_token(action.entity) if entity.corporation? && @game.class::HOME_TOKEN_TIMING == :par
+          @game.place_home_token(entity) if (@game.class::HOME_TOKEN_TIMING == :par) && !entity.company?
         end
         super(action)
 