fb5968125d6d463ad1481fc1aba37a4d6f94c69e
Michael Brandt <michaelbrandt5@gmail.com>
2022-11-22 16:45:55 -0700

    [1868WY] P7 "Big Boy" private
    this private provides a `+1 +1` token which boosts a train's city and town
    values by one
    
    * represent the token as `[+1 +1]`, and when assigned to a train, represent the
      new number in square brackets to distinguish it; e.g., a `2` train becomes
      `[3+1]`
    * wrap choice buttons in a div so that in the train-buying step, all choices are
      rendered on the same line
        * before: https://i.imgur.com/wJFayNh.png
        * after: https://i.imgur.com/WaSOLlE.png
    * the token can first be assigned when the private is bought (`Choose` step), or
      during the `BuyTrain` step; `ChooseBigBoy` is implemented as a module so to
      easily provide the functionality to both possible steps
    * the token can be re-assigned to a different train during the `BuyTrain` step
    
    [#5011]
    
    empty step assign

--- lib/engine/game/g_1868_wy/step/buy_train.rb
+++ lib/engine/game/g_1868_wy/step/buy_train.rb
@@ -2,6 +2,7 @@
 
 require_relative '../../../step/buy_train'
 require_relative '../skip_coal_and_oil'
+require_relative 'choose_big_boy'
 
 module Engine
   module Game
@@ -9,6 +10,15 @@ module Engine
       module Step
         class BuyTrain < Engine::Step::BuyTrain
           include G1868WY::SkipCoalAndOil
+          include ChooseBigBoy
+
+          def actions(entity)
+            super.concat(choice_actions(entity, cannot_pass: entity.corporation? && must_buy_train?(entity)))
+          end
+
+          def process_choose(action)
+            process_choose_big_boy(action)
+          end
 
           def process_buy_train(action)
             super
