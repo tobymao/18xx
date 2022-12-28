8cb5866421f219f581ed882f50bde77e1d6e756c
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2022-12-28 12:45:48 +0000

    [18BF] Initial import of 18BF code

---
+++ lib/engine/game/g_18_bf/game.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require_relative '../g_1867/game'
+require_relative 'meta'
+require_relative 'entities'
+require_relative 'map'
+require_relative 'market'
+require_relative 'tiles'
+require_relative 'trains'
+
+module Engine
+  module Game
+    module G18BF
+      class Game < G1867::Game
+        include Entities
+        include Map
+        include Market
+        include Tiles
+        include Trains
+
+        include_meta(G18BF::Meta)
+
+        GAME_END_REASONS_TEXT = Base::GAME_END_REASONS_TEXT.merge(
+          custom: 'The first 4+4 or 6G train is purchased.',
+        )
+        GAME_END_CHECK = { custom: :one_more_full_or_set }.freeze
+
+        def setup
+          # TODO: check which bits of this are needed, just cut-n-pasted from 1867.
+          @interest = {}
+          setup_company_price_up_to_face
+
+          @show_majors = false
+          setup_london_hexes
+        end
+
+        def setup_preround
+          # Randomise the privates available and the minor company order.
+          random = Random.new(rand)
+          setup_companies(random)
+          setup_minors(random)
+
+          # Remove companies and corporations that cannot yet be started.
+          @companies, @future_companies = @companies.partition do |company|
+            company.type == :railway
+          end
+          @corporations, @future_corporations = @corporations.partition do |corporation|
+            corporation.type == :minor && corporation.reservation_color == :yellow
+          end
+        end
+
+        def add_neutral_tokens(_hexes)
+          # No green placeholder tokens in 18BF.
+          @green_tokens = []
+        end
+
+        def init_loans
+          @loan_value = 50
+          # 24 minors × 2, 11 majors × 5, 5 systems × 10.
+          Array.new(153) { |id| Loan.new(id, @loan_value) }
+        end
+
+        def new_stock_round
+          case @turn
+          when 1
+            # Ferry companies are now available.
+            ferries, @future_companies = @future_companies.partition { |c| c.type == :ferry }
+            @companies += ferries
+          when 2
+            # Second batch of private companies are available.
+            batch2, @future_corporations = @future_corporations.partition do |corporation|
+              corporation.type == :minor && corporation.reservation_color = :palegreen
+            end
+            @corporations += batch2
+          else
+            # Nothing changes.
+          end
+
+          super
+        end
+
+        def event_minors_batch3!
+            batch3, @future_corporations = @future_corporations.partition do |corporation|
+              corporation.type == :minor && corporation.reservation_color = :green
+            end
+            @corporations += batch3
+        end
+
+        def event_u1_available!
+          u1 = @future_companies.delete { |company| company.sym == 'U1' }
+          @companies << u1
+        end
+
+        def event_u2_available!
+          u2 = @future_companies.delete { |company| company.sym == 'U2' }
+          @companies << u2
+        end
+
+        def event_privates_close!
+          # TODO: implement this.
+        end
+
+        def buyable_bank_owned_companies
+          @companies.select { |company| !company.closed? && !company.owner }
+        end
+
+        def unowned_purchasable_companies(_entity)
+          (@companies + @future_companies).select do |company|
+            !company.closed? && !company.owner
+          end
+        end
+
+        def calculate_interest
+          # Number of loans interest is due on is set before taking loans in that OR
+          @interest.clear
+          @corporations.each { |c| calculate_corporation_interest(c) }
+        end
+      end
+    end
+  end
+end
---
+++ lib/engine/game/g_18_bf/tiles.rb
@@ -0,0 +1,152 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G18BF
+      module Tiles
+        TILES = {
+          # Yellow tiles.
+          '3' => 4,
+          '4' => 12,
+          '5' => 6,
+          '6' => 6,
+          '7' => 'unlimited',
+          '8' => 'unlimited',
+          '9' => 'unlimited',
+          '57' => 6,
+          '58' => 10,
+          '201' => 4,
+          '202' => 4,
+          '621' => 4,
+
+          # Green tiles
+          '14' => 4,
+          '15' => 8,
+          '16' => 2,
+          '17' => 2,
+          '18' => 2,
+          '19' => 2,
+          '20' => 2,
+          '21' => 2,
+          '22' => 2,
+          '23' => 8,
+          '24' => 8,
+          '25' => 6,
+          '26' => 2,
+          '27' => 2,
+          '28' => 2,
+          '29' => 2,
+          '30' => 2,
+          '31' => 2,
+          '87' => 5,
+          '88' => 5,
+          '204' => 5,
+          '207' => 6,
+          '208' => 4,
+          '619' => 4,
+          '622' => 4,
+          '624' => 1,
+          '625' => 1,
+          '626' => 1,
+          'BF1' => {
+            'count' => 2,
+            'color' => 'green',
+            'code' => 'city=revenue:40;city=revenue:40;city=revenue:40;label=BGM;' \
+                      'path=a:0,b:_0;path=a:1,b:_1;path=a:2,b:_2;' \
+                      'path=a:3,b:_0;path=a:4,b:_1;path=a:5,b:_2',
+          },
+          'BF4' => {
+            'count' => 2,
+            'color' => 'green',
+            'code' => 'city=revenue:40;city=revenue:40;city=revenue:40;label=BGM;' \
+                      'path=a:0,b:_0;path=a:1,b:_1;path=a:2,b:_2;' \
+                      'path=a:3,b:_2;path=a:4,b:_1;path=a:5,b:_0',
+          },
+          'BF5' => {
+            'count' => 2,
+            'color' => 'green',
+            'code' => 'city=revenue:40;city=revenue:40;city=revenue:40;label=BGM;' \
+                      'path=a:0,b:_0;path=a:1,b:_1;path=a:2,b:_1;' \
+                      'path=a:3,b:_2;path=a:4,b:_2;path=a:5,b:_0',
+          },
+          'BF9' => {
+            'count' => 1,
+            'color' => 'green',
+            'code' => 'offboard=revenue:50;' \
+                      'path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0',
+          },
+          'BF12' => {
+            'count' => 1,
+            'color' => 'green',
+            'code' => 'city=revenue:40,loc:2.5;city=revenue:40,loc:4;city=revenue:40,loc:5.5;label=L;' \
+                      'path=a:3,b:_0;path=a:4,b:_1;path=a:5,b:_2',
+          },
+
+          # Brown tiles.
+          '39' => 2,
+          '40' => 2,
+          '41' => 2,
+          '42' => 2,
+          '43' => 2,
+          '44' => 2,
+          '45' => 2,
+          '46' => 2,
+          '47' => 2,
+          '63' => 6,
+          '70' => 2,
+          '216' => 2,
+          '448' => 8,
+          '611' => 3,
+          '623' => 4,
+          '801' => 3,
+          '911' => 5,
+          'BF6' => {
+            'count' => 1,
+            'color' => 'brown',
+            'code' => 'city=revenue:50,slots:2;city=revenue:50,slots:1;label=BGM;' \
+                      'path=a:0,b:_1;path=a:1,b:_0;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_1',
+          },
+          'BF7' => {
+            'count' => 1,
+            'color' => 'brown',
+            'code' => 'city=revenue:50,slots:2;city=revenue:50,slots:1;label=BGM;' \
+                      'path=a:0,b:_0;path=a:1,b:_1;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_1;path=a:5,b:_0',
+          },
+          'BF10' => {
+            'count' => 1,
+            'color' => 'brown',
+            'code' => 'offboard=revenue:60;' \
+                      'path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0',
+          },
+          'BF13' => {
+            'count' => 1,
+            'color' => 'brown',
+            'code' => 'city=revenue:50,slots:3;label=L;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0',
+          },
+
+          # Grey tiles.
+          '49' => 1,
+          'BF11' => {
+            'count' => 1,
+            'color' => 'gray',
+            'code' => 'offboard=revenue:70;' \
+                      'path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0',
+          },
+          'BF14' => {
+            'count' => 4,
+            'color' => 'gray',
+            'code' => 'city=revenue:70,slots:3;' \
+                      'path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0;' \
+                      'path=a:3,b:_0;path=a:4,b:_0;path=a:5,b:_0',
+          },
+        }.freeze
+      end
+    end
+  end
+end
