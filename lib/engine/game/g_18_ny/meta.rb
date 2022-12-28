93946a56b6204d0e95df5b6a4000710d05e3d8cd
Chris Rericha <tristipher@gmail.com>
2022-12-28 03:02:02 -0500

    [18NY] To production

--- lib/engine/game/g_18_ny/meta.rb
+++ lib/engine/game/g_18_ny/meta.rb
@@ -8,7 +8,7 @@ module Engine
       module Meta
         include Game::Meta
 
-        DEV_STAGE = :beta
+        DEV_STAGE = :production
 
         GAME_DESIGNER = 'Pierre LeBoeuf'
         GAME_PUBLISHER = :all_aboard_games
