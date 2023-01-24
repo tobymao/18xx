5a694031dcdf04533ff36ac0521f2a989139c219
Chris Rericha <crericha@gmail.com>
2023-01-23 23:58:59 -0500

    [1880] Remove commented out code

--- lib/engine/game/g_1880/step/company_pending_par.rb
+++ lib/engine/game/g_1880/step/company_pending_par.rb
@@ -9,18 +9,6 @@ module Engine
       module Step
         class CompanyPendingPar < Engine::Step::CompanyPendingPar
           include Parrer
-          #           def process_par(action)
-          #             super
-          #             corporation = action.corporation
-          #             @log << "#{corporation.name} selects ABC building permit"
-          #             corporation.building_permits = 'ABC'
-          #           end
-          #
-          #           def auto_actions(entity)
-          #             share = @game.abilities(companies_pending_par.first, :shares).shares.first
-          #             share_price = @game.stock_market.par_prices.find { |pp| pp.price == 100 }
-          #             [Engine::Action::Par.new(entity, corporation: share.corporation, share_price: share_price)]
-          #           end
         end
       end
     end
