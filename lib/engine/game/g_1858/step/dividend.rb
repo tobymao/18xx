tobymao/18xx

commit 18a9c744177a4b88ad7ce80b3957770aa9b78714

[1858] Remove unneeded method
`@game.routes_revenue` now includes revenue from private companies, so
this `total_revenue` method was double-counting the private revenue. It
was only ever used to test for zero revenue, so wouldn't have introduced
any bugs, but this method is no longer needed.

lib/engine/game/g_1858/step/dividend.rb
@@ -11,13 +11,9 @@ module Engine
 
           def actions(entity)
             return [] unless entity.corporation?
-            return [] if total_revenue(entity).zero?
+            return [] if @game.routes_revenue.zero?
 
             ACTIONS
-          end
-
-          def total_revenue(entity)
-            @game.routes_revenue(routes) + entity.companies.sum(&:revenue)
           end
 
           def process_dividend(action)

commit 5af297b2aba19de63c5fe380e1960eade142b672

[1858] Fix bug introduced in commit 18a9c744177a
Fixes issue 61.

lib/engine/game/g_1858/step/dividend.rb
@@ -11,7 +11,7 @@ module Engine
 
           def actions(entity)
             return [] unless entity.corporation?
-            return [] if @game.routes_revenue.zero?
+            return [] if @game.routes_revenue(routes).zero?
 
             ACTIONS
           end