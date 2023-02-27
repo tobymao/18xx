1300c0258406f20e5142cc095993380015b438aa
Chris Rericha <crericha@gmail.com>
2023-02-27 00:32:30 -0500

    Tile confirmation not needed for tile owner

--- assets/app/view/game/tile_confirmation.rb
+++ assets/app/view/game/tile_confirmation.rb
@@ -69,7 +69,9 @@ module View
         blocking_players = (@game.companies + @game.minors + @game.corporations).each_with_object([]) do |company, players|
           next if company.closed?
           next unless (ability = @game.abilities(company, :blocks_hexes_consent))
-          next unless @game.hex_blocked_by_ability?(entity, ability, hex, tile)
+          next unless @game.hex_blocked_by_ability?(@tile_selector.entity, ability, @tile_selector.hex)
+          next unless company == ability.owner
+          next if company.owner == @tile_selector.entity
 
           players << company.owner
         end
