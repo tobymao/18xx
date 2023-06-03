81359942da98d3e4936def813e2540613082343e
Michael Brandt <michaelbrandt5@gmail.com>
2023-06-03 08:34:08 -0600

    Revert "Revert "[1822 family] allow combining :tile_lay private company abilities""
    This reverts commit 94c2c46db5a382dcd75c521781c4298f1f516a66.

--- lib/engine/game/g_1822/step/track.rb
+++ lib/engine/game/g_1822/step/track.rb
@@ -56,7 +56,11 @@ module Engine
             @game.after_lay_tile(action.hex, old_tile, action.tile)
           end
 
-          def available_hex(entity, hex)
+          def available_hex(entities, hex)
+            # entities is sometimes an array for combining private company abilities
+            entities = Array(entities)
+            entity, *_combo_entities = entities
+
             connected = hex_neighbors(entity, hex)
             return nil unless connected
 
