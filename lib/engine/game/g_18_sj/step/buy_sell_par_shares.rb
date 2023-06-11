266cfcc19334ddfa0fd8ac063ceedfa5f370a8ff
perwestling <per.o.westling@gmail.com>
2023-06-11 14:57:44 +0200

    [18SJ] Make it possible to buy multiple "brown" shares from treasury

--- lib/engine/game/g_18_sj/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_sj/step/buy_sell_par_shares.rb
@@ -7,6 +7,21 @@ module Engine
     module G18SJ
       module Step
         class BuySellParShares < Engine::Step::BuySellParShares
+          def can_buy_any?(entity)
+            (can_buy_any_from_market?(entity) ||
+             can_buy_any_from_ipo?(entity) ||
+             can_buy_any_from_treasury?(entity))
+          end
+
+          def can_buy_any_from_treasury?(entity)
+            @game.corporations.each do |corporation|
+              next unless corporation.ipoed
+              return true if can_buy_shares?(entity, corporation.shares)
+            end
+
+            false
+          end
+
           def can_sell?(entity, bundle)
             super && (@game.oscarian_era || bundle.corporation.floated?)
           end
