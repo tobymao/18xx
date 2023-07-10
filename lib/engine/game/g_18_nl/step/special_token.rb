b100fb6e06ae649b6e2541aeda7f08a91dfe61fc
Phil Campeau <philcampeau@gmail.com>
2023-07-10 14:48:27 -0400

    Implements P2 ability

--- lib/engine/game/g_18_nl/step/special_token.rb
+++ lib/engine/game/g_18_nl/step/special_token.rb
@@ -7,15 +7,17 @@ module Engine
     module G18NL
       module Step
         class SpecialToken < Engine::Step::SpecialToken
-          def special_token
-            raise GameError, 'Cannot place station on hex or tile with existing track' unless tile.paths.empty?
-          end
-
           def process_place_token(action)
             entity = action.entity
-            super
-            @game.log << "#{entity.name} closes"
-            entity.close!
+            city = action.city
+            if city.tile.paths.empty?
+              super
+              @game.log << "#{entity.name} closes"
+              entity.close!
+            else
+              # Handle the case when tile.paths is not empty
+              raise GameError, 'Cannot place station on hex with existing track'
+            end
           end
         end
       end
