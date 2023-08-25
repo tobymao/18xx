32c9d68c02794533838e1ffaf6b3d02c16ca42ba
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2023-08-25 18:52:18 +0100

    [18Ardennes] Improved metadata

--- lib/engine/game/g_18_ardennes/meta.rb
+++ lib/engine/game/g_18_ardennes/meta.rb
@@ -10,11 +10,15 @@ module Engine
 
         DEV_STAGE = :prealpha
 
-        GAME_DESIGNER = 'David Hecht'
+        GAME_TITLE = '18Ardennes'
+        GAME_SUBTITLE = 'Rails Through the Ardennes'
+        GAME_DESIGNER = 'David G D Hecht'
         GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18Ardennes'
-        GAME_LOCATION = 'Belguim, the Netherlands, Germany and France'
+        GAME_LOCATION = 'Northwestern Europe'
         GAME_PUBLISHER = :deep_thought_games
         GAME_RULES_URL = 'https://drive.google.com/file/d/1ac2WuCiXRSr9lHpTkkR1yWK0l5cbBu2o/view'
+        GAME_IMPLEMENTER = 'Oliver Burnett-Hall'
+        KEYWORDS = %w[Belgium Netherlands France Germany].freeze
 
         PLAYER_RANGE = [3, 5].freeze
       end
