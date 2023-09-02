6e5a9aaa71dfd152d2c1f629d0617010f95fa0ff
Michael Brandt <michaelbrandt5@gmail.com>
2023-09-02 09:23:29 -0600

    [1868WY] add link to 2-page rules highlights

--- lib/engine/game/g_1868_wy/meta.rb
+++ lib/engine/game/g_1868_wy/meta.rb
@@ -13,7 +13,10 @@ module Engine
         GAME_DESIGNER = 'John Harres'
         GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/1868-Wyoming'
         GAME_PUBLISHER = :mercury
-        GAME_RULES_URL = 'https://boardgamegeek.com/filepage/262791/rules-traxx-mainline-hell-wheels-ks-release'
+        GAME_RULES_URL = {
+          'Rules' => 'https://boardgamegeek.com/filepage/262791/rules-traxx-mainline-hell-wheels-ks-release',
+          'Rules Highlights (2 pages)' => 'https://boardgamegeek.com/filepage/262792/rules-highlights',
+        }.freeze
         GAME_LOCATION = 'Wyoming, USA'
         GAME_TITLE = '1868 Wyoming'
         GAME_FULL_TITLE = '1868: Boom and Bust in the Coal Mines and Oil Fields of Wyoming'
