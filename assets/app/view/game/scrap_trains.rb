e883d7035184ad3bd733892a4326e85e65c58166
Chris Rericha <crericha@gmail.com>
2023-09-04 20:41:07 -0400

    Allow customatization of scrap header text

--- assets/app/view/game/scrap_trains.rb
+++ assets/app/view/game/scrap_trains.rb
@@ -10,12 +10,12 @@ module View
 
       def render
         @corporation ||= @game.round.active_step.current_entity
-        step = @game.round.step_for(@corporation, 'scrap_train')
+        @step = @game.round.step_for(@corporation, 'scrap_train')
 
-        scrappable_trains = step.scrappable_trains(@corporation)
+        scrappable_trains = @step.scrappable_trains(@corporation)
         return nil if scrappable_trains.empty?
 
-        if step.respond_to?(:scrap_trains_button_only?) && step.scrap_trains_button_only?
+        if @step.respond_to?(:scrap_trains_button_only?) && @step.scrap_trains_button_only?
           render_buttons(scrappable_trains)
         else
           render_section(scrappable_trains)
@@ -23,8 +23,8 @@ module View
       end
 
       def render_buttons(scrappable_trains)
-        h(:div, generate_scrap_train_actions(scrappable_trains) do |scrap, train, step|
-          h(:button, { on: { click: scrap } }, step.scrap_info(train))
+        h(:div, generate_scrap_train_actions(scrappable_trains) do |scrap, train|
+          h(:button, { on: { click: scrap } }, @step.scrap_info(train))
         end)
       end
 
@@ -38,21 +38,24 @@ module View
           },
         }
         h(:div,
-          [h(:h3, 'Trains to Scrap'),
+          [h(:h3, scrap_trains_header_text),
            h(:div, div_props, scrap_trains(scrappable_trains))])
       end
 
+      def scrap_trains_header_text
+        @step.respond_to?(:scrap_header_text) ? @step.scrap_header_text : 'Trains to Scrap'
+      end
+
       def scrap_trains(scrappable_trains)
-        generate_scrap_train_actions(scrappable_trains) do |scrap, train, step|
+        generate_scrap_train_actions(scrappable_trains) do |scrap, train|
           [h(:div, train.name),
            h('div.nowrap', train.owner.name),
-           h('div.right', step.scrap_info(train)),
-           h('button.no_margin', { on: { click: scrap } }, step.scrap_button_text(train))]
+           h('div.right', @step.scrap_info(train)),
+           h('button.no_margin', { on: { click: scrap } }, @step.scrap_button_text(train))]
         end
       end
 
       def generate_scrap_train_actions(scrappable_trains)
-        step = @game.round.step_for(@corporation, 'scrap_train')
         scrappable_trains.flat_map do |train|
           scrap = lambda do
             process_action(Engine::Action::ScrapTrain.new(
@@ -60,7 +63,7 @@ module View
               train: train,
             ))
           end
-          yield(scrap, train, step)
+          yield(scrap, train)
         end
       end
     end
