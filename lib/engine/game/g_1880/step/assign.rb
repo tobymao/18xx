e60f6dd032facb636a3d0ad241f056a0ce1beaf3
Benjamin Scott <bschmoker@gmail.com>
2023-11-10 17:07:31 -0800

    [1880] skip P5 assignment if corp already has D

--- lib/engine/game/g_1880/step/assign.rb
+++ lib/engine/game/g_1880/step/assign.rb
@@ -9,13 +9,11 @@ module Engine
         class Assign < Engine::Step::Assign
           ACTIONS_WITH_PASS = %w[assign pass].freeze
           def actions(entity)
-            return [] if entity.player?
+            return [] if entity.player? || current_entity.minor?
             return [] unless @game.abilities(entity, :assign_corporation)
-            return [] if current_entity.minor? ||
-                        (entity == @game.p5 && current_entity.corporation? && current_entity.building_permits&.include?('D'))
 
             return ACTIONS if @game.forced_exchange_rocket? && entity == @game.rocket
-            return ACTIONS_WITH_PASS if p5_block? && current_entity.owner == @game.p5.owner
+            return ACTIONS_WITH_PASS if p5_block?
 
             super
           end
@@ -40,14 +38,13 @@ module Engine
           end
 
           def description
-            @game.forced_exchange_rocket? ? 'Forced Rocket exchange' : 'Assign'
+            @game.forced_exchange_rocket? ? 'Forced Exchange for Rocket of China' : 'Assign Jeme Tien Yow Engineer Office'
           end
 
           def help
             return super unless @game.forced_exchange_rocket?
 
-            'Rocket of China is still open at phase B3. Owner must trade the rocket of china,'\
-              ' pick one of the corporation to gain a 4 train'
+            'Owner of Rocket of China must choose one of their corporations to receive the next 4 train from the depot at no cost'
           end
 
           def blocks?
@@ -55,22 +52,24 @@ module Engine
           end
 
           def p5_block?
-            @game.phase.name[0] == 'D' && !@game.p5.all_abilities.empty?
+            @game.phase.name[0] == 'D' && !@game.p5.all_abilities.empty? && current_entity.corporation? \
+            && current_entity.owner == @game.p5.owner && !current_entity.building_permits&.include?('D')
           end
 
           def process_assign(action)
             return process_assign_rocket(action) if @game.forced_exchange_rocket?
+            raise GameError, "#{current_entity.id} already has a D permit" if current_entity.building_permits&.include?('D')
 
             super
             corporation = action.target
-            @game.log << "#{corporation.name} recieves a D building permit"
+            @game.log << "#{corporation.name} receives a D building permit"
             corporation.building_permits += 'D'
           end
 
           def process_assign_rocket(action)
             buying_corp = action.target
             train = @game.rocket_train || @game.depot.upcoming.first
-            @log << "#{buying_corp.name} exchanges the #{@game.rocket.name} for a #{train.name} train"
+            @log << "#{buying_corp.name} exchanges #{@game.rocket.name} for a free #{train.name} train"
 
             @game.rocket.close!
             @game.buy_train(buying_corp, train, :free)
