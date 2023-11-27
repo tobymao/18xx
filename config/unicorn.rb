ee74069a964d17b5e4ade43d4e0208357703c230
tobymao <toby.mao@gmail.com>
2023-11-27 08:50:35 -0800

    tweak

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,8 +6,8 @@ require 'message_bus'
 PRODUCTION = ENV['RACK_ENV'] == 'production'
 
 listen 9292
-worker_processes PRODUCTION ? 6 : 1
-timeout PRODUCTION ? 10 : 60
+worker_processes PRODUCTION ? 8 : 1
+timeout PRODUCTION ? 20 : 60
 preload_app true
 
 before_fork do |_server, _worker|
