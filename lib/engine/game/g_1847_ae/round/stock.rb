495d9c7271beaf590a0a13536892b05d8d65ba60
Galatolol <galatolol@protonmail.com>
2023-12-02 19:18:51 +0100

    1847AE - fix autoaction bug

--- lib/engine/game/g_1847_ae/round/stock.rb
+++ lib/engine/game/g_1847_ae/round/stock.rb
@@ -9,6 +9,7 @@ module Engine
         class Stock < Engine::Round::Stock
           def setup
             @game.exchange_all_investor_companies! if @game.must_exchange_investor_companies
+            @game.nationalization_actions_this_round = []
 
             super
           end
