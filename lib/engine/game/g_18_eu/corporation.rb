1928fd105d49c8c3213a8ba38939bab646e57844
Andrew Zwicky <andrew.zwicky@gmail.com>
2023-12-15 21:36:49 -0600

    Rubocop

--- lib/engine/game/g_18_eu/corporation.rb
+++ lib/engine/game/g_18_eu/corporation.rb
@@ -9,7 +9,8 @@ module Engine
         def holding_ok?(share_holder, extra_percent = 0)
           # Per the rules, it's OK to temporarily hold over the per-corp limit
           # if the company hasn't operated yet.
-          return true unless self.operated?
+          return true unless operated?
+
           super
         end
       end
