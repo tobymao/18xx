a4632a14eeeebaf9b79c6ebd05aa9ce7973325bf
Andrew Zwicky <andrew.zwicky@gmail.com>
2024-01-21 21:53:39 -0600

    spec updates

--- spec/lib/engine/games/g_1854_spec.rb
+++ spec/lib/engine/games/g_1854_spec.rb
@@ -10,39 +10,59 @@ module Engine
 
     let(:players) { %w[alice ben chuck] }
 
+    def do_action(game, action)
+      game.process_action(action,  add_auto_actions: true).maybe_raise!
+    end
+
     context 'testing 1854' do
       it 'should conduct initial auction' do
-        game = Game::G1854::Game.new(players)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 55),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 60),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 65),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L3'), price: 160), add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 70),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P1'), price: 20),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P3'), price: 70),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 155),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 160),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L1'), price: 150),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 165),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 155),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 160),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 165),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 170),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L4'), price: 150),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 175),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('L6'), price: 150),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P4'), price: 170),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Pass.new(game.current_entity),  add_auto_actions: true)
-        game.process_action(Action::Bid.new(game.current_entity, company: game.company_by_id('P5'), price: 190),  add_auto_actions: true)
-        # binding.pry
-        game.process_action(Action::Par.new(game.current_entity, corporation: game.corporation_by_id('KE'), share_price: game.share_price_by_id("67,5,6")),  add_auto_actions: true)
-        game.process_action(Action::BuyShares.new(game.current_entity, shares: game.share_by_id('KE_2')),  add_auto_actions: true)
+        game = Game::G1854::Game.new(players).maybe_raise!
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 55))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P2'), price: 60))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L3'), price: 160))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P1'), price: 20))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P3'), price: 70))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 155))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 160))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L1'), price: 150))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L2'), price: 165))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 155))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 160))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 165))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 170))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L4'), price: 150))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L5'), price: 175))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('L6'), price: 150))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P4'), price: 170))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Bid.new(game.current_entity, company: game.company_by_id('P5'), price: 190))
+        do_action(game,       Action::Par.new(game.current_entity, corporation: game.corporation_by_id('KE'), share_price: game.share_price_by_id("67,5,6")))
+        do_action(game, Action::BuyShares.new(game.current_entity, shares: game.share_by_id('KE_2')))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Par.new(game.current_entity, corporation: game.corporation_by_id('VB'), share_price: game.share_price_by_id("67,5,6")))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,       Action::Par.new(game.current_entity, corporation: game.corporation_by_id('SD'), share_price: game.share_price_by_id("72,4,7")))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,   Action::LayTile.new(game.current_entity, hex: game.hex_by_id("J36"), tile: game.tile_by_id("55-0"), rotation: 0))
+        do_action(game,  Action::BuyTrain.new(game.current_entity, train: game.train_by_id("1+-0"), price: 100, variant: "1+"))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
+        do_action(game,      Action::Pass.new(game.current_entity))
 
         puts game.log.map {|m| m.message }
         puts "==="
@@ -52,8 +72,8 @@ module Engine
 
         puts game.players.map {|p| "#{p.name}:#{p.cash}" }.join(', ')
 
-        expect(game.players.sort_by{|p| p.name }.map {|p| p.cash }).to eq([155, 265, 690])
-        expect(game.current_entity).to be game.player_by_id("chuck")
+        # expect(game.players.sort_by{|p| p.name }.map {|p| p.cash }).to eq([155, 265, 690])
+        # expect(game.current_entity).to be game.player_by_id("chuck")
       end
 
       # it 'should reduce price of first company until bought' do
