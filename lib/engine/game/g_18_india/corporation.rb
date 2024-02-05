ba62b8ba171715bf8f6cd79f9dc4c289f5997663
EdPovi <118568589+EdPovi@users.noreply.github.com>
2024-02-04 19:29:14 -0500

    [18 India] Implements Initial Selection and Draft Round (new game) (#10232)
    * Game Setup information
    
    * Initial Hand Selection step
    
    * Selection and Draft steps in place
    
    * Linter Fixes
    
    * RuboCop fixes
    
    * Draft Updates
    
    * Draft updates
    
    * Draft Updates and Linter Fixes
    
    * draft order updates
    
    * Modify draft group and sort
    
    * Clean up for PR
    
    * Update lib/engine/game/g_18_india/game.rb
    
    fix naming
    
    Co-authored-by: Michael Brandt <michaelbrandt5@gmail.com>
    
    * Update lib/engine/game/g_18_india/game.rb
    
    remove line
    
    Co-authored-by: Michael Brandt <michaelbrandt5@gmail.com>
    
    ---------
    
    Co-authored-by: Michael Brandt <michaelbrandt5@gmail.com>

---
+++ lib/engine/game/g_18_india/corporation.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G18India
+      class Corporation < Engine::Corporation
+        def initialize(sym:, name:, **opts)
+          super
+        end
+
+        def book_value
+          # sum of cash and all assets
+          cash + @companies.sum(&:value) + value_of_trains + value_of_owned_shares
+        end
+
+        def book_value_per_share
+          book_value / 10.0.ceil
+        end
+
+        def total_value_per_share
+          share_price + book_value_per_share
+        end
+
+        def value_of_trains
+          trains.select { |t| t.owner == self }.sum(&:price)
+        end
+
+        def value_of_owned_shares
+          shares.select { |s| s.owner == self }.sum(&:price)
+        end
+
+        def mangaged_company?
+          !presidents_share.owned_by_player?
+        end
+      end
+    end
+  end
+end
