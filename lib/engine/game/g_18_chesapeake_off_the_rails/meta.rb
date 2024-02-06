c6894a9fd80da3d42ceb9ae9a28d064c9ca3295b
wongrm <matty.wongrose@gmail.com>
2024-02-06 11:43:37 -0800

    [18Chesapeake: Off the Rails] Updates to meta.rb (#10287)
    Add base game rules link to 18Chesapeake: Off the Rails meta.rb
    Links Off the Rail to it's respective wiki page.  From there users can navigate to the base game wiki entry if need be

--- lib/engine/game/g_18_chesapeake_off_the_rails/meta.rb
+++ lib/engine/game/g_18_chesapeake_off_the_rails/meta.rb
@@ -13,9 +13,14 @@ module Engine
         DEPENDS_ON = '18Chesapeake'
 
         GAME_DESIGNER = 'Scott Petersen'
-        GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18Chesapeake'
+        GAME_INFO_URL = 'https://github.com/tobymao/18xx/wiki/18Chesapeake%3A-Off-the-Rails'
         GAME_PUBLISHER = :all_aboard_games
-        GAME_RULES_URL = 'https://www.dropbox.com/s/ivm4jsopnzabhru/18ChesOTR_Rules.png?dl=0'
+        GAME_RULES_URL = {
+          'Base 18Chesapeake Rules' =>
+          'https://cdn.shopify.com/s/files/1/0252/9371/7588/files/18Chesapeake.pdf?v=1597256917',
+          'Off the Rails Expansion Rules' =>
+            'https://www.dropbox.com/s/ivm4jsopnzabhru/18ChesOTR_Rules.png?dl=0',
+        }.freeze
         GAME_TITLE = '18Chesapeake: Off the Rails'
         GAME_ALIASES = %w[OTR 18ChesapeakeOTR].freeze
         GAME_IS_VARIANT_OF = G18Chesapeake::Meta
