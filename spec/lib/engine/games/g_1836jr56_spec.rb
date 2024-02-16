d4411ca071b1a884a44218f7b76782835217c8c1
Andrew Zwicky <andrew.zwicky@gmail.com>
2024-02-15 20:57:21 -0600

    rubocop

--- spec/lib/engine/games/g_1836jr56_spec.rb
+++ spec/lib/engine/games/g_1836jr56_spec.rb
@@ -8,7 +8,7 @@ module Engine
     let(:players) { %w[a b c] }
 
     let(:game_file) do
-      Find.find(FIXTURES_DIR).find { |f| File.basename(f) == '#{game_file_name}.json' }
+      Find.find(FIXTURES_DIR).find { |f| File.basename(f) == "#{game_file_name}.json" }
     end
 
     let(:actions) do
@@ -17,11 +17,18 @@ module Engine
         { 'type' => 'bid', 'entity' => 'b', 'entity_type' => 'player', 'company' => 'E-SF', 'price' => 40, 'id' => 2 },
         { 'type' => 'bid', 'entity' => 'c', 'entity_type' => 'player', 'company' => 'CdH', 'price' => 50, 'id' => 3 },
         { 'type' => 'bid', 'entity' => 'a', 'entity_type' => 'player', 'company' => 'RdP', 'price' => 70, 'id' => 4 },
-        { 'type' => 'par', 'entity' => 'b', 'entity_type' => 'player', 'corporation' => 'B', 'share_price' => '65,5,4', 'id' => 5 },
-        { 'type' => 'buy_shares', 'entity' => 'c', 'entity_type' => 'player', 'shares'=> ['B_1'], 'percent'=> 10, 'id' => 6 },
-        { 'type' => 'buy_shares', 'entity' => 'a', 'entity_type' => 'player', 'shares'=> ['B_2'], 'percent'=> 10, 'id' => 7 },
-        { 'type' => 'buy_shares', 'entity' => 'b', 'entity_type' => 'player', 'shares'=> ['B_3'], 'percent'=> 10, 'id' => 8 },
-        { 'type' => 'buy_shares', 'entity' => 'c', 'entity_type' => 'player', 'shares'=> ['B_4'], 'percent'=> 10, 'id' => 9 },
+        {
+          'type' => 'par',
+          'entity' => 'b',
+          'entity_type' => 'player',
+          'corporation' => 'B',
+          'share_price' => '65,5,4',
+          'id' => 5,
+        },
+        { 'type' => 'buy_shares', 'entity' => 'c', 'entity_type' => 'player', 'shares' => ['B_1'], 'percent' => 10, 'id' => 6 },
+        { 'type' => 'buy_shares', 'entity' => 'a', 'entity_type' => 'player', 'shares' => ['B_2'], 'percent' => 10, 'id' => 7 },
+        { 'type' => 'buy_shares', 'entity' => 'b', 'entity_type' => 'player', 'shares' => ['B_3'], 'percent' => 10, 'id' => 8 },
+        { 'type' => 'buy_shares', 'entity' => 'c', 'entity_type' => 'player', 'shares' => ['B_4'], 'percent' => 10, 'id' => 9 },
         { 'type' => 'pass', 'entity' => 'a', 'entity_type' => 'player', 'id' => 10 },
         { 'type' => 'pass', 'entity' => 'b', 'entity_type' => 'player', 'id' => 11 },
         { 'type' => 'pass', 'entity' => 'c', 'entity_type' => 'player', 'id' => 12 },
@@ -39,12 +46,12 @@ module Engine
 
       it 'should not enter infinite loop' do
         expect(game.raw_actions.size).to be 12
-        expect(game.current_entity.name).to be "B"
+        expect(game.current_entity.name).to be 'B'
 
         action = Engine::Action::LayTile.new(game.current_entity,
-          tile: game.tile_by_id('6-0'),
-          hex: game.hex_by_id('G7'),
-          rotation: 0)
+                                             tile: game.tile_by_id('6-0'),
+                                             hex: game.hex_by_id('G7'),
+                                             rotation: 0)
         game.process_action(action, add_auto_actions: true).maybe_raise!
 
         expect(game.raw_actions.size).to be 13
