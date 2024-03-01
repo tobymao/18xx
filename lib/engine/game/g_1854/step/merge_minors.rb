b2e0c1e05d4198d0f3b67080325678a9efb62e4f
Andrew Zwicky <andrew.zwicky@gmail.com>
2024-02-29 21:20:15 -0600

    Adding second half of current step overrides

---
+++ lib/engine/game/g_1854/step/merge_minors.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/base'
+
+module Engine
+  module Game
+    module G1854
+      module Step
+        class MergeMinors < Engine::Step::Base
+          def actions(entity)
+            return [] if entity.corporation? && !entity.minor?
+            return [] if !entity.minor? || entity != current_entity #  || @game.done_this_round[entity]
+            return [] unless @game.mergeable?(entity)
+            return [] if target_corporations.empty?
+            return [] if @merge_target
+
+            actions = []
+            actions << 'merge' if @game.minor_mergers_allowed?
+            actions << 'pass' if @game.minor_mergers_allowed? && !@game.minor_mergers_required?
+            actions
+          end
+
+          def blocking?
+            @game.mergeable?(current_entity)
+          end
+
+          def active?
+            return false if @game.open_minors.empty?
+
+            @game.minor_mergers_allowed? || @game.minor_mergers_required?
+          end
+
+          def setup
+            @mergeable_ = {}
+            @merging = nil
+            @merge_target = nil
+          end
+
+          def auto_actions(entity)
+            return super if @merging
+
+            return [Engine::Action::Pass.new(entity)] if mergeable_candidates(entity).empty?
+
+            super
+          end
+
+          def merge_name(entity = nil)
+            return "Form #{entity&.name}" if @merging
+
+            "Merge with #{entity&.name}"
+          end
+
+          # corps available as a merge target
+          def target_corporations
+            @game.corporations.select { |c| @game.merge_target?(c) }
+          end
+
+          def description
+            return 'Choose corporation to form' if @merging
+
+            'Merge'
+          end
+
+          def process_merge(action)
+            if @merging
+              @merge_target = action.corporation
+              raise GameError, "#{@merge_target.name} is not available to merge into" unless @game.merge_target?(@merge_target)
+
+              @game.merge_minors_into_lokalbahn(action.entity, @merging, @merge_target)
+            else
+              other = action.minor
+
+              raise GameError, "#{other.name} is the wrong corporation type" if other.type != action.entity.type
+              raise GameError, "#{other.name} is not available to merge with" unless @game.mergeable?(other)
+
+              @merging = other
+
+              @log << "#{@game.formatted_minor_name(action.entity)} and #{@game.formatted_minor_name(other)} selected to merge."
+            end
+          end
+
+          def mergeable_type(corporation)
+            return "Corporations that #{corporation.name} and #{@merging.name} can be merged into to form" if @merging
+
+            "Minors that can merge with #{corporation.name}"
+          end
+
+          def mergeable_candidates(corporation)
+            # # Mergeable candidates must be connected by track and not through a regional border
+            # # They must be the same type (major/minor)
+            # parts = @game.token_graph_for_entity(corporation).connected_nodes(corporation).keys
+            # mergeable = parts.select { |n| n.city? && !n.pass? }.flat_map { |c| c.tokens.compact.map(&:corporation) }
+            @mergeable_[corporation] ||=
+              @game.minors.uniq.select { |c| c != corporation && @game.mergeable?(c) }
+          end
+
+          def mergeable(corporation)
+            return target_corporations if @merging
+
+            mergeable_candidates(corporation)
+          end
+
+          def show_other_players
+            false
+          end
+
+          def show_other
+            @merging
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/engine/game/g_1854/step/route.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/route'
+
+module Engine
+  module Game
+    module G1854
+      module Step
+        class Route < Engine::Step::Route
+          def process_run_routes(action)
+            super
+            return if @round.routes.empty?
+
+            # Close when a train is actually run, as opposed to just reaching the dividends step.
+            # This event is not in the generic abilities, so create one that is the same as in the entities
+            # and rely on the rest of the code to match them up.
+            @game.close_companies_on_event!(action.entity, 'ran_train')
+          end
+        end
+      end
+    end
+  end
+end
