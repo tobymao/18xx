4a31a92a6bc0bc0e0b5f7c91e1d8bcd656814688
Andrew Zwicky <andrew.zwicky@gmail.com>
2024-03-08 19:59:35 -0600

    Adding in changes from review

--- lib/engine/game/g_1854/step/buy_train.rb
+++ lib/engine/game/g_1854/step/buy_train.rb
@@ -8,22 +8,12 @@ module Engine
       module Step
         class BuyTrain < Engine::Step::BuyTrain
           def can_entity_buy_train?(entity)
-            return true if entity.corporation? || entity.minor?
-
-            super
+            entity.corporation? || entity.minor?
           end
 
           def buyable_trains(entity)
             # allow purchase of 3 train when 2s are all sold
             trains_to_buy = super
-
-            any_twos_upcoming = @game.depot.upcoming.any? { |t| t.name == '2' }
-            any_threes_upcoming = @game.depot.upcoming.any? { |t| t.name == '3' }
-            depot_three_available = trains_to_buy.any? { |t| t.name == '3' && t.owner == @game.depot }
-
-            if !any_twos_upcoming && any_threes_upcoming && !depot_three_available
-              trains_to_buy << @game.depot.upcoming.find { |t| t.name == '3' }
-            end
 
             trains_to_buy.reject! { |t| t.name == '3+' } if @game.depot.upcoming.any? { |t| t.name == '2+' }
 
