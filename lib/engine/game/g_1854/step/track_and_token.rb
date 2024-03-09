ae7156896cedb8180139052c428dab6bf803e01b
Andrew Zwicky <andrew.zwicky@gmail.com>
2024-03-08 20:05:17 -0600

    Commit suggestion from review and waterfall_comment move

--- lib/engine/game/g_1854/step/track_and_token.rb
+++ lib/engine/game/g_1854/step/track_and_token.rb
@@ -8,9 +8,9 @@ module Engine
       module Step
         class TrackAndToken < Engine::Step::TrackAndToken
           def actions(entity)
-            return [] unless entity == pending_entity
-
             actions = []
+            return actions unless entity == pending_entity
+
             actions << 'assign' unless @selected_hex
             actions << 'lay_tile' if can_lay_tile?(entity)
             actions << 'place_token' if can_place_token?(entity)
