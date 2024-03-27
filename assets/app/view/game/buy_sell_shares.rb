tobymao/18xx

commit bc6fa7d7c22f95e8d7aa19d6f457c321ea24e492

[18Ardennes] More work on exchanging minors for major shares
Allow minor companies to be exchanged for shares in majors.
This is not yet finished, the major company should be allowed to decline
trains and tokens, and consent might be required depending on when the
exchange happens.

assets/app/view/game/buy_sell_shares.rb
@@ -258,7 +258,7 @@ module View
       # Allow privates or minors to be exchanged for shares if they have the ability
       def render_exchanges
         children = []
-        source_entities = @game.companies + @game.minors
+        source_entities = @game.companies + @game.minors + @game.corporations
 
         source_entities.each do |entity|
           @game.abilities(entity, :exchange) do |ability|

lib/engine/game/g_18_ardennes/entities.rb
@@ -33,6 +33,7 @@ module Engine
             hide_shares: true,
             coordinates: 'B8',
             city: 0,
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '2',
@@ -48,6 +49,7 @@ module Engine
             hide_shares: true,
             coordinates: 'B8',
             city: 1,
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '3',
@@ -62,6 +64,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'E15',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '4',
@@ -76,6 +79,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'G25',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '5',
@@ -90,6 +94,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'J24',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '6',
@@ -104,6 +109,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'H6',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '7',
@@ -118,6 +124,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'I21',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '8',
@@ -133,6 +140,7 @@ module Engine
             hide_shares: true,
             coordinates: 'M7',
             city: 0,
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '9',
@@ -148,6 +156,7 @@ module Engine
             hide_shares: true,
             coordinates: 'M7',
             city: 1,
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '10',
@@ -162,6 +171,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'D18',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '11',
@@ -176,6 +186,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'B16',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '12',
@@ -190,6 +201,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'K11',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '13',
@@ -204,6 +216,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'E9',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '14',
@@ -218,6 +231,7 @@ module Engine
             max_ownership_percent: 100,
             hide_shares: true,
             coordinates: 'F10',
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: '15',
@@ -233,6 +247,7 @@ module Engine
             hide_shares: true,
             coordinates: 'E25',
             city: 1,
+            abilities: [ { type: 'exchange', corporations: 'ipoed', from: %w[ipo market] } ],
           },
           {
             sym: 'BY',
@@ -395,6 +410,16 @@ module Engine
           minor = pledged_minors[major]
           min_par = lowest_major_par
           (min_par.price * 3) - minor_sale_value(minor, min_par)
+        end
+
+        def exchange_corporations(exchange_ability)
+          minor = exchange_ability.owner
+          super.select do |major|
+            max_price = minor.share_price.price * 2 + liquidity(minor.owner)
+
+            major.share_price.price <= max_price &&
+              major_minor_connected?(major, minor)
+          end
         end
 
         private

lib/engine/game/g_18_ardennes/map.rb
@@ -360,6 +360,17 @@ module Engine
 
           super
         end
+
+        # Returns true if there is a route between one of the minor's tokens
+        # and one of the major's tokens, or if they both have tokens co-located
+        # on the same tile.
+        def major_minor_connected?(major, minor)
+          minor_cities = minor.placed_tokens.map(&:city)
+          major_cities = major.placed_tokens.map(&:city)
+
+          minor_cities.map(&:hex).intersect?(major_cities.map(&:hex)) ||
+            minor_cities.any? { |city| @graph.connected_nodes(major)[city] }
+        end
       end
     end
   end

lib/engine/game/g_18_ardennes/step/buy_sell_par_shares_companies.rb
@@ -38,6 +38,39 @@ module Engine
             super.select { |company| company.type == :minor }
           end
 
+          # Exchanging a minor for a share in a floated major corporation is
+          # done as a buy_share action.
+          def can_buy_any?(entity)
+            return false if bought?
+
+            super || can_exchange_any?(entity)
+          end
+
+          def can_gain?(entity, bundle, exchange: false)
+            # Can go above 60% ownership if exchanging a minor for a share.
+            exchange || super
+          end
+
+          # Checks whether a player can afford to exchange one of their minors
+          # for a share in a major corporation.
+          # **Note** This does not check whether there is a track connection
+          # between the minor and the major, which is required to carry out the
+          # exchange. This is not checked to avoid having to recalculate the
+          # game graph whilst a game is being loaded.
+          def can_exchange_any?(player)
+            majors = @game.major_corporations.select do |corp|
+              corp.ipoed && (corp.num_treasury_shares.positive? || corp.num_market_shares.positive?)
+            end
+            return false if majors.empty?
+
+            @game.minor_corporations.any? do |minor|
+              next false unless minor.owner == player
+
+              max_price = minor.share_price.price * 2 + @game.liquidity(player)
+              majors.any? { |corp| corp.share_price.price <= max_price }
+            end
+          end
+
           # Corporations whose cards are visible in the stock round.
           # Hide those whose concessions have not yet been auctioned.
           def visible_corporations
@@ -69,7 +102,7 @@ module Engine
             concession = major.par_via_exchange
 
             concession.close!
-            exchange_minor(minor, major)
+            exchange_minor(minor, major.treasury_shares.first.to_bundle)
           end
 
           def process_bankrupt(action)

lib/engine/game/g_18_ardennes/step/exchange.rb
@@ -10,10 +10,27 @@ module Engine
         class Exchange < Engine::Step::Exchange
           include MinorExchange
 
+          def bought?
+            @round.current_actions.any? do |action|
+              Engine::Step::BuySellParShares::PURCHASE_ACTIONS.include?(action.class)
+            end
+          end
+
           def can_exchange?(entity, _bundle = nil)
+            return false if bought?
             return false unless entity.corporation?
 
             entity.type == :minor
+          end
+
+          def process_buy_shares(action)
+            unless can_exchange?(action.entity, action.bundle)
+              raise GameError, "Cannot exchange #{action.entity.id} for " \
+                               "#{action.bundle.corporation.id}"
+            end
+
+            exchange_minor(action.entity, action.bundle)
+            @round.current_actions << action
           end
         end
       end

lib/engine/game/g_18_ardennes/step/minor_exchange.rb
@@ -8,9 +8,9 @@ module Engine
         module MinorExchange
           private
 
-          def exchange_minor(minor, major)
-            player = major.owner
-            bundle = major.treasury_shares.first.to_bundle
+          def exchange_minor(minor, bundle)
+            player = minor.owner
+            major = bundle.corporation
             extra_cost = [0, major.share_price.price - (minor.share_price.price * 2)].max
 
             msg = "#{player.name} exchanges minor #{minor.name} "

commit a617470bc26d08c61a53f782bcca01ca74b8dc71

Prevent spurious exchange buttons appearing in stock round
Bugs tobymao#10537 and tobymao#10563 were reports of spurious exchange
buttons appearing in stock rounds, in 1871 and 1858.

These buttons were appearing when the active player owned a private
company that could be exchanged for a share in a corporation. As well as
the button to exchange the private for a share, one or more extra
buttons were appearing, offering to exchange corporations for a share.

This was caused by commit bc6fa7d, which added `@game.corporations` to
the entities that are checked for exchange abilities. This was needed
for 18Ardennes, which has corporations with exchange abilities.

Neither 1858 or 1871 have corporations with exchange abilities, but
`Corporation.all_abilities` will also pick up any abilities owned by the
corporation's president which have an owning_player-type timing. This
caused these corporations to appear as an option to be exchanged.

This is fixed by removing `@game.corporations` from the default entities
that are checked for exchange abilities. To allow 18Ardennes to still
check those, a new method `@game.exchange_entities` is added which
defines which entities are examined for the exchange abilities. The
default implementation returns all companies and minors, and so reverts
to the behaviour before commit bc6fa7d. 18Ardennes has its own version
of this method added, which checks the minor-type corporations.

assets/app/view/game/buy_sell_shares.rb
@@ -272,7 +272,7 @@ module View
       # Allow privates or minors to be exchanged for shares if they have the ability
       def render_exchanges
         children = []
-        source_entities = @game.companies + @game.minors + @game.corporations
+        source_entities = @game.exchange_entities
 
         source_entities.each do |entity|
           @game.abilities(entity, :exchange) do |ability|

lib/engine/game/base.rb
@@ -2134,6 +2134,11 @@ module Engine
         nil
       end
 
+      # Entities that can own an exchange ability.
+      def exchange_entities
+        companies + minors
+      end
+
       def exchange_corporations(exchange_ability)
         candidates = case exchange_ability.corporations
                      when 'any'

lib/engine/game/g_18_ardennes/entities.rb
@@ -412,6 +412,11 @@ module Engine
           (min_par.price * 3) - minor_sale_value(minor, min_par)
         end
 
+        # Entities that can own an exchange ability.
+        def exchange_entities
+          minor_corporations
+        end
+
         def exchange_corporations(exchange_ability)
           minor = exchange_ability.owner
           super.select do |major|