843ffcb5528f3114dbf10c5c38956f7d605a2557
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2024-03-31 15:36:26 +0100

    [18Ardennes] Rubocop fix

--- lib/engine/game/g_18_ardennes/trains.rb
+++ lib/engine/game/g_18_ardennes/trains.rb
@@ -136,7 +136,7 @@ module Engine
         end
 
         def revenue_for(route, stops)
-          # TODO (maybe): I suspect that the calculation of the revenue for a
+          # TODO: (maybe) I suspect that the calculation of the revenue for a
           # long 4D route is inefficient, and might cause slow auto-routing.
           # It might be worth pruning out some of the stops from the route
           # to avoid calling this method so often.
