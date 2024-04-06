166d149eb019a9882bcda5dcd02b68c2c590d876
EdPovi <edpovi@hotmail.com>
2024-04-06 09:29:15 -0400

    Modify for special track

--- lib/engine/game/g_18_india/step/tracker.rb
+++ lib/engine/game/g_18_india/step/tracker.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../../../step/special_track'
+require_relative '../../../step/tracker'
 
 module Engine
   module Game
     module G18India
       module Step
-        class SpecialTrack < Engine::Step::SpecialTrack
+        module Tracker
 
           # modified to prevent removal of province borders that had a cost (the cost is from a water border)
           def remove_border_calculate_cost!(tile, entity_or_entities, spender)
@@ -29,13 +29,13 @@ module Engine
 
               types << border.type
               if border.type == :province && @game.phase.name != 'IV'
-                # if the border is a province prior to phase IV, don't delete, instead change cost to nil
+                # if the border is a province prior to phase IV, don't delete, instead change to gauge change border
                 LOGGER.debug " >> MODIFY Borders!!!"
                 tile.borders.delete(border)
-                tile.borders << gauge_change_border(tile, edge)
-                # add gauge change to neighbor tile also
+                tile.borders << add_gauge_change_border(tile, edge)
+                # remove old border and add gauge change to neighbor tile also
                 neighbor.tile.borders.map! { |nb| nb.edge == hex.invert(edge) ? nil : nb }.compact!
-                neighbor.tile.borders << gauge_change_border(neighbor.tile, hex.invert(edge))
+                neighbor.tile.borders << add_gauge_change_border(neighbor.tile, hex.invert(edge))
               else
                 LOGGER.debug " >> DELETE Borders!!!"
                 tile.borders.delete(border)
@@ -49,40 +49,10 @@ module Engine
             [total_cost, types]
           end
 
-          def gauge_change_border(tile, edge)
+          def add_gauge_change_border(tile, edge)
             new_border = Engine::Part::Border.new(edge, :gauge_change, nil)
             new_border.tile = tile
             new_border
-          end
-
-          # Bypass some Step::Tracker tests for Town to City upgrade: maintain exits, and check new exits are valid
-          # check tile color to active ability
-          def legal_tile_rotation?(entity, hex, tile)
-            return false unless (ability = abilities(entity))
-            return false if tile.color != :yellow && ability.upgrade_count.zero?
-            return false if tile.color == :yellow && ability.lay_count.zero?
-
-            old_tile = hex.tile
-            if @game.yellow_town_to_city_upgrade?(old_tile, tile)
-              all_new_exits_valid = tile.exits.all? { |edge| hex.neighbors[edge] }
-              return false unless all_new_exits_valid
-
-              return (old_tile.exits - tile.exits).empty?
-            end
-
-            super
-          end
-
-          # highlight according to active ability
-          def available_hex(entity, hex)
-            return unless (ability = abilities(entity))
-            return tracker_available_hex(entity, hex) if ability.hexes&.empty? && ability.consume_tile_lay
-
-            color = hex.tile.color
-            return nil if color != :white && ability.upgrade_count.zero?
-            return nil if color == :white && ability.lay_count.zero?
-
-            hex_neighbors(entity, hex)
           end
         end
       end
