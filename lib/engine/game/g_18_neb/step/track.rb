ede7a6a6de974d1a336fb2cea7a6b45dfe3d8ad7
Chris Rericha <crericha@gmail.com>
2024-04-12 01:56:16 -0400

    [18Neb] Make sure all exits on a green city upgrade tile are valid

--- lib/engine/game/g_18_neb/step/track.rb
+++ lib/engine/game/g_18_neb/step/track.rb
@@ -10,7 +10,7 @@ module Engine
           def legal_tile_rotation?(entity, hex, tile)
             old_tile = hex.tile
             if @game.town_to_city_upgrade?(old_tile, tile) || @game.omaha_green_upgrade?(old_tile, tile)
-              return (old_tile.exits & tile.exits) == old_tile.exits
+              return (old_tile.exits & tile.exits) == old_tile.exits && tile.exits.all? { |edge| hex.neighbors[edge] }
             end
 
             super
