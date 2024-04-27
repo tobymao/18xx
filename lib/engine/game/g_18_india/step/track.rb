d8be92506e9702006b18bc830b5e78cfce7e553e
EdPovi <edpovi@hotmail.com>
2024-04-27 11:40:17 -0400

    update track laying checks

--- lib/engine/game/g_18_india/step/track.rb
+++ lib/engine/game/g_18_india/step/track.rb
@@ -7,23 +7,34 @@ module Engine
     module G18India
       module Step
         class Track < Engine::Step::Track
-          # for debugging
+          # Added multple yellow tile check and Yellow OO reservation check
           def process_lay_tile(action)
-            LOGGER.debug "Track >> process_lay_tile >> entity: #{action.entity.inspect}, tile: #{action.tile.inspect}"
-            connected_to_track_laying_path?(action.hex) # verify that hex is connected to laying path
+            if action.tile.color == 'yellow'
+              raise GameError, 'New yellow tiles must extend path from railhead and previously laid tiles' \
+               unless connected_to_track_laying_path?(action.hex)
+              @round.laid_yellow_hexes << action.hex
+            end
             super
             move_oo_reservations(action) unless @round.pending_tokens.empty? # Pending token due to Yellow OO tile
             @round.next_empty_hexes = get_railhead_hexes
           end
 
-          # ------ Code for track laying rules
-          def round_state
-            { next_empty_hexes: [] }.merge(super)
+          def setup
+            super
+            @round.removed_gauge = []
+            @round.next_empty_hexes = []
+            @round.laid_yellow_hexes = []
           end
 
+          def round_state
+            super.merge({ removed_gauge: [], next_empty_hexes: [], laid_yellow_hexes: [] })
+          end
+
+          # ------ Code for track laying rules
+
           def connected_to_track_laying_path?(hex)
-            return if @game.loading
-            return if @round.laid_hexes.empty?
+            return true if @game.loading
+            return true if @round.laid_yellow_hexes.empty?
             return unless hex.tile.color == 'white'
 
             @round.next_empty_hexes = get_railhead_hexes if @round.next_empty_hexes.empty?
@@ -32,39 +43,42 @@ module Engine
 
           def get_railhead_hexes
             return [] if @game.loading
-            return [] if @round.laid_hexes.empty?
+            return [] if @round.laid_yellow_hexes.empty?
 
-            # check if there is only one 'white' neighbor connected to prior tile => return without walking
-            neighbors = @round.laid_hexes.last.tile.exits.map { |e| @round.laid_hexes.last.neighbors[e] }
+            # check simple case of only one 'white' neighbor connected to prior tile => return without walking
+            last_yellow_hex = @round.laid_yellow_hexes.last
+            neighbors = last_yellow_hex.tile.exits.map { |e| last_yellow_hex.neighbors[e] }
             empty_neighbors = neighbors.select { |h| h.tile.color == 'white' }
-            return empty_neighbors if empty_neighbors.size == 1
+            return empty_neighbors if empty_neighbors.one? && last_yellow_hex.tile.exits.size <= 2 # exclude triple town tiles
 
             corp = @round.current_operator
             railheads = corp.placed_tokens.map(&:city)
-            placed_paths = @round.laid_hexes.map(&:tile).map(&:paths) # paths on all previously laid hexes
+            placed_paths = @round.laid_yellow_hexes.map(&:tile).map(&:paths) # paths on all previously laid yellow hexes
             LOGGER.debug "railhead_hexes >> corp: #{corp.inspect}, railheads: #{railheads.inspect}"
             return [] unless railheads.any?
 
             next_hexes = []
-            railheads.first.walk(corporation: corp) do |path, v_p, visited|
-              # check if path ends at targeted "white" hex
-              empty_neighbors = path.exits.map { |e| path.hex.neighbors[e] }.reject { |h| h.tile.color != 'white' }
-              next unless empty_neighbors.any?
-              LOGGER.debug " walk >> path: #{path.inspect} > empty_neighbors: #{empty_neighbors.inspect} "
+            railheads.each do |railhead|
+              railhead.walk(corporation: corp) do |path, visited_paths, visited|
+                # check if path ends at targeted "white" hex
+                empty_neighbors = path.exits.map { |e| path.hex.neighbors[e] }.reject { |h| h.tile.color != 'white' }
+                next unless empty_neighbors.any?
+                LOGGER.debug " walk >> path: #{path.inspect} > empty_neighbors: #{empty_neighbors.inspect} "
 
-              # test if the visted path includes a path from all placed tiles
-              visited_path_array = v_p.keys
-              include_array = placed_paths.map { |paths_on_tile| (paths_on_tile & visited_path_array).any? }
-              next unless include_array.all?
+                # check if visited path has at least one path from all placed tiles (triple town tiles may have unused paths)
+                visited_path_array = visited_paths.keys
+                placed_paths_visited = placed_paths.map { |paths_on_tile| (paths_on_tile & visited_path_array).any? }
+                next unless placed_paths_visited.all?
 
-              # test that all tiles are visted in the correct sequence along the visted path
-              sequence = placed_paths.map { |t_ps| t_ps.map { |p| visited_path_array.index(p) }.compact.min }
-              next unless sequence.each_cons(2).all? { |x,y| x < y }
-              LOGGER.debug " >> path found!!! >> sequence: #{sequence.inspect} "
-              next_hexes << empty_neighbors
+                # confirm that placed tiles are visited in the correct sequence along the visited path
+                sequence = placed_paths.map { |tile_paths| tile_paths.map { |p| visited_path_array.index(p) }.compact.min }
+                next unless sequence.each_cons(2).all? { |x,y| x < y }
+                LOGGER.debug " >> path found!!! >> sequence: #{sequence.inspect} "
+                next_hexes << empty_neighbors
+              end
             end
-            LOGGER.debug "railhead_hexes >> next_hexes: #{next_hexes.inspect}"
-            next_hexes.flatten
+            LOGGER.debug "railhead_hexes >> next_hexes: #{next_hexes.flatten.uniq.inspect}"
+            next_hexes.flatten.uniq
           end
 
           def available_hex(entity_or_entities, hex)
