33268d24cd976ef190dacbe33ca8d16534bcbaee
Michael Brandt <michaelbrandt5@gmail.com>
2024-04-28 11:40:00 -0600

    18RoyalGorge to alpha

--- lib/engine/game/g_18_royal_gorge/meta.rb
+++ lib/engine/game/g_18_royal_gorge/meta.rb
@@ -8,7 +8,7 @@ module Engine
       module Meta
         include Game::Meta
 
-        DEV_STAGE = :prealpha
+        DEV_STAGE = :alpha
         PROTOTYPE = true
 
         GAME_SUBTITLE = 'The Rails of Fremont County and the Royal Gorge Wars'
