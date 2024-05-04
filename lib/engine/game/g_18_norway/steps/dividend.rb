04cb732311da24467e04ca1438a7a29d3f357c78
patrikolesen <patrik@hemma.org>
2024-05-04 05:43:24 +0200

    Update lib/engine/game/g_18_norway/steps/dividend.rb
    Co-authored-by: Oliver Burnett-Hall <olly@burnett-hall.co.uk>

--- lib/engine/game/g_18_norway/steps/dividend.rb
+++ lib/engine/game/g_18_norway/steps/dividend.rb
@@ -16,7 +16,7 @@ module Engine
             return {} if entity.minor?
 
             price = entity.share_price.price
-            return { share_direction: :left, share_times: 1 } if revenue < price / 2
+            return { share_direction: :left, share_times: 1 } if revenue * 2 < price
 
             times = 0
             times = 1 if revenue >= price
