dfd707ef43a3470d154c8a42fb0b076f356bcbac
Phil Campeau <philcampeau@gmail.com>
2024-05-21 14:56:00 -0400

    remove unnecessary electric_dreams? gate

--- lib/engine/game/g_1849/game.rb
+++ lib/engine/game/g_1849/game.rb
@@ -468,7 +468,7 @@ module Engine
           update_garibaldi
           # code below is for variant rules
           @log << 'E-token returned to bank' if corporation.e_token
-          corporation.e_token = false if electric_dreams?
+          corporation.e_token = false
 
           repaid_bond = corporation.loans.pop if bonds? && corporation.loans.any?
           @loans << repaid_bond if repaid_bond
