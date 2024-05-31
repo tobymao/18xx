f3d5c7c6580fd660abae907ac49e2b3e7c5b8502
Phil Campeau <philcampeau@gmail.com>
2024-05-30 21:58:53 -0400

    [18Mag] restricts expansion content to 3+ players

--- lib/engine/game/g_18_mag/meta.rb
+++ lib/engine/game/g_18_mag/meta.rb
@@ -33,22 +33,26 @@ module Engine
            sym: :new_minors_challenge,
            short_name: 'New minors - challenging',
            desc: 'Minors 14, 15, 16 are added to the game (3-6 players)',
+           players: [3, 4, 5, 6],
          },
          {
            sym: :new_minors_simple,
            short_name: 'New minors - simple',
            desc: 'Minors 14, 15, 16 are added and three random minors are removed at setup (3-6 players)',
+           players: [3, 4, 5, 6],
          },
          {
            sym: :new_major,
            short_name: 'New major',
            desc: 'Major CIWL is added. The new major grants a bonus for running red to red routes (3-6 players)',
+           players: [3, 4, 5, 6],
          },
          {
            sym: :supporters,
            short_name: 'Add Supporter Cards',
            desc: 'Players may choose one of six "supporter cards" as an optional action in the first SR. '\
                  'The chosen supporter may be used once per OR to benefit one of that player\'s minor companies (3-6 players)',
+           players: [3, 4, 5, 6],
          },
         ].freeze
       end
