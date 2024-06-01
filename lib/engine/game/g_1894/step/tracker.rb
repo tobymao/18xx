3e5831a8566e91e9f05906548c34d8e3913aabc4
Galatolol <galatolol@protonmail.com>
2024-06-01 21:42:53 +0200

    1894 - remove unnecessary function from tracker

--- lib/engine/game/g_1894/step/tracker.rb
+++ lib/engine/game/g_1894/step/tracker.rb
@@ -27,38 +27,6 @@ module Engine
             old_paths.all? { |path| new_paths.any? { |p| path <= p } } &&
             new_exits.sort == old_exits.sort
         end
-
-        def lay_tile(action, extra_cost: 0, entity: nil, spender: nil)
-          hex = action.hex
-          old_tile = hex.tile
-          new_tile = action.tile
-
-          if @game.class::BROWN_CITY_TILES.include?(new_tile.name)
-            # The city splits into two cities, so the reservation has to be for the whole hex
-            reservation = old_tile.cities.first.reservations.compact.first
-            if reservation
-              old_tile.cities.first.remove_all_reservations!
-              old_tile.add_reservation!(reservation.corporation, nil, false)
-            end
-
-            tokens = old_tile.cities.flat_map(&:tokens).compact
-            tokens_to_save = []
-            tokens.each do |token|
-              token.price = 0
-              tokens_to_save << {
-                entity: token.corporation,
-                hexes: [hex],
-                token: token,
-              }
-            end
-            @game.save_tokens(tokens_to_save)
-            @game.save_tokens_hex(hex)
-
-            tokens.each(&:remove!)
-          end
-
-          super
-        end
       end
     end
   end
