e7e7b9338f3b29abc9cefee975bafcd7aef253e7
Michael Brandt <michaelbrandt5@gmail.com>
2024-07-14 15:29:28 -0600

    22Mars to prealpha

--- lib/engine/game/g_22_mars/meta.rb
+++ lib/engine/game/g_22_mars/meta.rb
@@ -8,7 +8,7 @@ module Engine
       module Meta
         include Game::Meta
 
-        DEV_STAGE = :alpha
+        DEV_STAGE = :prealpha
         PROTOTYPE = true
 
         GAME_DESIGNER = 'Jonas Jones'
