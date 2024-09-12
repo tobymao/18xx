f22b30d419f4a870b79d23b37b87bb3897728767
Patrik Olesen <patrik@hemma.org>
2024-09-12 20:38:59 +0200

    [18Uruguay] Allow more than 60% to cover for loans
    Fixes #11110

--- lib/engine/game/g_18_uruguay/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_uruguay/step/buy_sell_par_shares.rb
@@ -9,8 +9,16 @@ module Engine
         class BuySellParShares < Engine::Step::BuySellParShares
           def can_buy?(entity, bundle)
             return false if bundle&.corporation == @game.rptla && !@game.phase.status.include?('rptla_available')
+            return true if loan_limit(entity, bundle)
 
             super(entity, bundle)
+          end
+
+          def loan_limit(_entity, bundle)
+            return false if bundle.nil?
+            return false if bundle.owner.corporation?
+
+            bundle.corporation.loans.size > @game.maximum_loans(bundle.corporation)
           end
         end
       end
