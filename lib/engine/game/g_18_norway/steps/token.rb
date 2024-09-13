fbe23516f7710c4588d5f91ea3ee2c85aba763bc
Patrik Olesen <patrik@hemma.org>
2024-09-13 02:19:15 +0200

    [18Norway] Updated the harbor logic

--- lib/engine/game/g_18_norway/steps/token.rb
+++ lib/engine/game/g_18_norway/steps/token.rb
@@ -8,20 +8,22 @@ module Engine
       module Step
         class Token < Engine::Step::Token
           def available_hex(entity, hex)
-            return true if super
+            return true if super(entity, hex)
 
-            harbor_available?(entity, hex)
+            return true if check_available_harbour(entity, hex)
+
+            false
           end
 
-          def tokener_available_hex(entity, hex)
-            return true if super
+          def check_available_harbour(entity, hex)
+            city = @game.harbor_city_id_by_harbor_id(hex.id)
+            return false if city.nil?
 
-            harbor_available?(entity, hex)
-          end
+            other_hex = @game.hex_by_id(city)
 
-          def harbor_available?(entity, hex)
-            other_hex = @game.hex_by_id(@game.harbor_city_coordinates(hex.id))
-            @game.graph.reachable_hexes(entity)[other_hex] || @game.ferry_graph.reachable_hexes(entity)[other_hex]
+            return false unless @game.ferry_graph.reachable_hexes(entity)[other_hex]
+
+            true
           end
 
           def can_place_token?(_entity)
@@ -31,20 +33,17 @@ module Engine
           def process_place_token(action)
             entity = action.entity
             city = action.city
-            hex = city.hex
-            connected_city = @game.token_graph_for_entity(entity).connected_nodes(entity)[city]
+            connected_city = @game.loading || @game.token_graph_for_entity(entity).connected_nodes(entity)[city]
             if connected_city
               place_token(entity, action.city, action.token)
-            elsif @game.harbor_hex?(city.hex)
-              city_string = city.hex.tile.cities.size > 1 ? " city #{city.index}" : ''
-              city_name = @game.harbor_city_coordinates(hex.id) + city_string
-              raise GameError, "Cannot reach city #{city_name}" unless harbor_available?(entity, hex)
+            elsif @game.harbor_hex?(city.hex) && !connected_city
+              reachable_hexes = @game.ferry_graph.reachable_hexes(entity)[city.hex]
 
-              place_token(entity, action.city, action.token, connected: false, extra_slot: true)
-
+              @game.abilities(entity, :token) do |ability|
+                place_token(entity, action.city, action.token, connected: false, special_ability: ability)
+              end
             else
-              city_string = hex.tile.cities.size > 1 ? " city #{city.index}" : ''
-              raise GameError, "Cannot place token on #{hex.name}#{city_string} because it is not connected"
+              raise GameError, "Cannot place token on #{city.hex.name} city is not connected"
             end
             @game.clear_graph
             pass!
