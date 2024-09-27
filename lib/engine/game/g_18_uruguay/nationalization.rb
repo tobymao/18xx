e51d38fe9cc598b853cf78e59557db8267fb1ebc
Patrik Olesen <patrik@hemma.org>
2024-09-27 07:28:39 +0200

    [18Uruguay] Rubocop

--- lib/engine/game/g_18_uruguay/nationalization.rb
+++ lib/engine/game/g_18_uruguay/nationalization.rb
@@ -38,7 +38,7 @@ module Engine
             bundle = Engine::ShareBundle.new(@fce.shares.reject(&:president).take(num_shares)) unless num_shares == 10
             @share_pool.transfer_shares(bundle, holder, allow_president_change: true)
             number = num_shares
-            @log << "#{holder.name} receives  #{num_shares*10}% in FCE in exchange to the nationalized shares"
+            @log << "#{holder.name} receives  #{num_shares * 10}% in FCE in exchange to the nationalized shares"
 
             next unless odd_share
 
