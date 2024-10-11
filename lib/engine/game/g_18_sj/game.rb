44cdbc0c143a7f01b3c751b20a4654b8e652e856
Phil Campeau <philcampeau@gmail.com>
2024-10-11 10:34:20 -0400

    updated fix

--- lib/engine/game/g_18_sj/game.rb
+++ lib/engine/game/g_18_sj/game.rb
@@ -788,12 +788,8 @@ module Engine
             return
           end
 
-          # Merge the corporation with highest share price, and use highest token on the pile as tie break
-          merged = candidates.max_by do |c|
-            [c.share_price.price, c.share_price.coordinates.last, c.share_price.corporations.find_index(c)]
-          end
-
-          nationalize_major(merged)
+          # nationalizes the highest-valued trainless corp (which has already operated) after a rusting event
+          nationalize_major(candidates.min)
         end
 
         # If there are 2 station markers on the same city the
