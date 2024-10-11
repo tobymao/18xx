fe1f38072746fae409524b9c04cd30511c173ae1
Chris Rericha <tristipher@gmail.com>
2024-10-11 01:46:57 -0400

    [1837] Implement train surrendering

--- lib/engine/game/g_1837/step/buy_train.rb
+++ lib/engine/game/g_1837/step/buy_train.rb
@@ -7,6 +7,17 @@ module Engine
     module G1837
       module Step
         class BuyTrain < Engine::Step::BuyTrain
+          def actions(entity)
+            return [] if entity != current_entity
+
+            actions = super.clone
+            unless scrappable_trains(entity).empty?
+              actions << 'pass' if actions.empty?
+              actions << 'scrap_train'
+            end
+            actions
+          end
+
           def buyable_train_variants(train, entity)
             variants = super
             variants.select! { |t| @game.freight_train?(t[:name]) } if entity.type == :coal
@@ -21,6 +32,37 @@ module Engine
 
           def can_entity_buy_train?(_entity)
             true
+          end
+
+          def scrappable_trains(entity)
+            entity.trains.select { |t| surrender_cost(t) <= entity.cash }
+          end
+
+          def surrender_cost(train)
+            train.price / 2
+          end
+
+          def scrap_info(_train)
+            ''
+          end
+
+          def scrap_button_text(train)
+            "Surrender (#{@game.format_currency(-surrender_cost(train))})"
+          end
+
+          def scrap_header_text
+            'Trains to Surrender'
+          end
+
+          def process_scrap_train(action)
+            entity = action.entity
+            train = action.train
+            raise GameError, 'Can only scrap trains owned by the corporation' if entity != train.owner
+
+            @log << "#{entity.name} spends #{@game.format_currency(surrender_cost(train))} to surrender" \
+                    " a #{train.name} train to the bank"
+            entity.spend(surrender_cost(train), @game.bank)
+            @game.depot.reclaim_train(train)
           end
         end
       end
