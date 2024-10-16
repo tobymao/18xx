fd97e5f99470b47eed909ad9a17ed18b78859240
Chris Rericha <tristipher@gmail.com>
2024-10-15 20:56:28 -0400

    [1837] Token step

--- lib/engine/game/g_1837/step/token.rb
+++ lib/engine/game/g_1837/step/token.rb
@@ -1,15 +1,56 @@
 # frozen_string_literal: true
 
+require_relative '../../../step/tokener'
 require_relative '../../../step/token'
-require_relative '../../../minor'
 
 module Engine
   module Game
     module G1837
       module Step
         class Token < Engine::Step::Token
-          def skip!
-            super unless @round.current_operator.is_a?(Minor)
+          def actions(entity)
+            return [] unless entity == current_entity
+            return [] unless has_multiple_tokens?(entity)
+
+            Engine::Step::Token::ACTIONS
+          end
+
+          def auto_actions(entity)
+            return unless has_multiple_tokens?(entity)
+
+            [Engine::Action::Pass.new(entity)] unless can_place_token?(entity)
+          end
+
+          def can_place_token?(entity)
+            # Cheaper to do the graph first, then check affordability
+            current_entity == entity &&
+              !(token = entity.next_token).nil? &&
+              @game.graph.can_token?(entity) &&
+              can_afford_token?(token, buying_power(entity))
+          end
+
+          def log_skip(entity)
+            super if has_multiple_tokens?(entity)
+          end
+
+          def has_multiple_tokens?(entity)
+            entity.tokens.size > 1
+          end
+
+          def can_afford_token?(token, cash)
+            @game.graph.tokenable_cities(token.corporation).any? do |city|
+              token_price(token, city.tile.hex) <= cash
+            end
+          end
+
+          def token_price(token, hex)
+            home_hex = @game.hex_by_id(token.corporation.coordinates)
+            home_hex.distance(hex) * token.price
+          end
+
+          def adjust_token_price_ability!(entity, token, hex, _city, special_ability: nil)
+            token.price = token_price(token, hex)
+            [token, nil]
           end
         end
       end
