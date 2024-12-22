75e458a424631f0ada5f75ad2cbddf06f40a0d5d
Galatolol <galatolol@protonmail.com>
2024-12-22 15:01:54 +0100

    [1894] Change one comment

--- lib/engine/game/g_18_fr/step/tracker.rb
+++ lib/engine/game/g_18_fr/step/tracker.rb
@@ -8,7 +8,7 @@ module Engine
       module Tracker
         include Engine::Step::Tracker
         def legal_tile_rotation?(entity_or_entities, hex, tile)
-          # We will remove a town from the yellow B tile, so we will not follow the normal path upgrade rules
+          # The town is removed from the yllow B tile, so the normal path upgrade rules are not followed
           return true if tile.name == @game.class::GREEN_B_TILE_NAME && tile.rotation == hex.tile.rotation
 
           super
