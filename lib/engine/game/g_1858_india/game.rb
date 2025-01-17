7c56ccdcb6ad9e7d02446123766836ac6069801b
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2025-01-17 17:26:22 +0000

    [1858 India] Add 1M trains, and set train counts
    Metre gauge track is available from the start of the game in 1858 India.
    
    Add a 1M train that can be bought in the yellow phase.
    
    Set the train counts.

--- lib/engine/game/g_1858_india/game.rb
+++ lib/engine/game/g_1858_india/game.rb
@@ -17,6 +17,45 @@ module Engine
         BANK_CASH = 16_000
         STARTING_CASH = { 3 => 665, 4 => 500, 5 => 400, 6 => 335 }.freeze
         CERT_LIMIT = { 3 => 27, 4 => 20, 5 => 16, 6 => 13 }.freeze
+
+        TRAIN_COUNTS = {
+          '2H' => 8,
+          '4H' => 7,
+          '6H' => 5,
+          '5E' => 4,
+          '6E' => 3,
+          '7E' => 20,
+          '5D' => 10,
+        }.freeze
+
+        def game_trains
+          unless @game_trains
+            @game_trains = super.map(&:dup)
+            # Add the 1M variant to the 2H train.
+            @game_trains.first['variants'] =
+              [
+                {
+                  name: '1M',
+                  distance: [{ 'nodes' => %w[city offboard], 'pay' => 1, 'visit' => 1 },
+                             { 'nodes' => %w[town], 'pay' => 99, 'visit' => 99 }],
+                  price: 70,
+                },
+              ]
+          end
+          @game_trains
+        end
+
+        def num_trains(train)
+          TRAIN_COUNTS[train[:name]]
+        end
+
+        def game_phases
+          unless @game_phases
+            @game_phases = super.map(&:dup)
+            @game_phases.first[:status] = %w[yellow_privates narrow_gauge]
+          end
+          @game_phases
+        end
       end
     end
   end
