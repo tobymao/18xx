7cadf58c5485726728531b41e933e8a47b11f50b
BenA <BENA@il.ibm.com>
2025-03-03 18:45:23 +0200

    fix more minor 18esp issues

--- lib/engine/game/g_18_esp/game.rb
+++ lib/engine/game/g_18_esp/game.rb
@@ -61,7 +61,7 @@ module Engine
 
         NORTH_SOUTH_DIVIDE = 13
 
-        P2_TRAIN_ID = '2-0'
+        P2_TRAIN_ID = '2P-0'
 
         ARANJUEZ_HEX = 'F26'
 
@@ -887,6 +887,7 @@ module Engine
                      city_by_id("#{hex.tile.id}-#{corporation.city}")
                    end
             @log << "#{corporation.name} places a token on #{hex.id}"
+            cheater = !city.tokenable?(corporation)
             city.place_token(corporation, token, cheater: true, check_tokenable: false)
           else
             super
