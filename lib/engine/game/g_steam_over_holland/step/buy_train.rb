29cfeec5b7ecd617cbd2d1d036af9c613fd7fc40
Phil Campeau <philcampeau@gmail.com>
2025-03-07 17:08:50 -0500

    [Steam Over Holland] fixes EMR bug

--- lib/engine/game/g_steam_over_holland/step/buy_train.rb
+++ lib/engine/game/g_steam_over_holland/step/buy_train.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../../../step/buy_train'
+require_relative '../../../step/train'
 
 module Engine
   module Game
@@ -8,9 +9,8 @@ module Engine
       module Step
         class BuyTrain < Engine::Step::BuyTrain
           def actions(entity)
-            return [] if entity != current_entity
-
             return ['sell_shares'] if entity == current_entity&.owner && can_ebuy_sell_shares?(current_entity)
+            return [] if entity != current_entity
             return %w[sell_shares buy_train pass] if president_may_contribute?(entity)
             return %w[buy_train pass] if can_buy_train?(entity)
 
@@ -32,6 +32,12 @@ module Engine
               raise GameError,
                     'Corporation can afford train, a train must be purchased.'
             end
+
+            if !@game.loading && !@corporations_sold.empty?
+              raise GameError,
+                    'Player sold shares, a train must be purchased.'
+            end
+
             return super unless can_close_corp?(entity)
 
             @game.close_corporation(entity)
@@ -41,14 +47,27 @@ module Engine
             bundle = action.bundle
             corporation = bundle.corporation
             old_price = corporation.share_price
-            @game.share_pool.sell_shares(bundle)
 
-            (bundle.num_shares - 1).times do
-              @game.stock_market.move_left(corporation)
+            if action.entity == corporation
+              @game.share_pool.sell_shares(bundle)
+
+              (bundle.num_shares - 1).times do
+                @game.stock_market.move_left(corporation)
+              end
+
+              @game.log_share_price(corporation, old_price)
+              @round.issued_shares[corporation] = true
+            else
+              @game.sell_shares_and_change_price(bundle)
+              @corporations_sold << corporation
             end
+          end
 
-            @game.log_share_price(corporation, old_price)
-            @round.issued_shares[corporation] = true
+          def can_sell?(entity, bundle)
+            return false if @game.class::MUST_SELL_IN_BLOCKS && @corporations_sold.include?(bundle.corporation)
+            return false if current_entity != entity && must_issue_before_ebuy?(current_entity)
+
+            super
           end
 
           def must_sell_shares?(corporation)
