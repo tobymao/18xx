14cbc93b5fbefc8e2de1c3b4cc4edee5fd808891
BenA <BENA@il.ibm.com>
2025-04-07 14:22:27 +0300

    fix mza bug when madrid is in brown
    add bank info on tenders, fix bug where there one less tender in bank
    
    handle pr comments
    
    fix mz mza cheater token
    
    address pr comments
    
    adress pr, fix more minor bugs
    
    address pr comments
    
    fix error on all 2p trains
    
    fix rubycop

--- lib/engine/game/g_18_esp/game.rb
+++ lib/engine/game/g_18_esp/game.rb
@@ -777,11 +777,9 @@ module Engine
           hexes = route.hexes.reject { |h| self.class::DOUBLE_HEX.include?(h.name) }
           raise GameError, 'Route visits same hex twice' if hexes.size != hexes.uniq.size
 
-          if route.train.id == '2P-0' && !@perm2_ran_aranjuez && route.hexes.none? do |hex|
-               hex.id == ARANJUEZ_HEX
-             end
+          if route.train.id == '2P-0' && !@perm2_ran_aranjuez && !p2_route_runs_aranjuez_to_mardid?(route)
             raise GameError,
-                  '2P first run must include Aranjuez'
+                  '2P first run must include Aranjuez and Madrid'
           end
           wrong_track = skip_route_track_type(route.train)
           raise GameError, 'Routes must use correct gauage' if wrong_track && route.paths.any? { |p| p.track == wrong_track }
@@ -792,11 +790,14 @@ module Engine
         def check_p2_aranjuez(routes)
           return if @perm2_ran_aranjuez
 
-          @perm2_ran_aranjuez = true if routes.any? do |route|
-                                          route.train.id == '2P-0' && route.hexes.any? do |hex|
-                                            hex.id == ARANJUEZ_HEX
-                                          end
-                                        end
+          @perm2_ran_aranjuez = routes.any? do |route|
+            p2_route_runs_aranjuez_to_mardid?(route)
+          end
+        end
+
+        def p2_route_runs_aranjuez_to_mardid?(route)
+          route.train.id == '2P-0' &&
+          ([ARANJUEZ_HEX, MADRID_HEX] - route.hexes.map(&:id)).empty?
         end
 
         def valid_interchange?(tile, entity)
@@ -885,7 +886,7 @@ module Engine
             hex = hex_by_id(corporation.coordinates)
             cities = hex.tile.cities
             city =
-              if cities.one? || !corporation_by_id('MZ')&.ipoed
+              if cities.one?
                 cities.first
               else
                 cities[corporation.city]
@@ -1143,6 +1144,17 @@ module Engine
 
           # close corp
           close_corporation(minor)
+        end
+
+        def close_corporation(corporation, quiet: false)
+          if corporation.type == :minor
+            corporation.placed_tokens.each do |token|
+              city = token.city
+              remove_slot = city.tokens.compact.any?(&:cheater)
+              city.delete_token!(token, remove_slot: remove_slot)
+            end
+          end
+          super
         end
 
         def southern_major_corps
