0fa4a0c699f424ae62f619b231389c4b1904d910
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2025-05-11 10:33:46 +0100

    [1807] Correct token calculation cost from London tokens
    The previous fix for ollybh#190 was incomplete. It had fixed the
    calculation of token cost when placing a token in London, but had not
    considered that you also need to look at the cost of placing a token
    outside London when a company has a token in London.
    
    This commit changes the calculation so that all token costs will be
    calculated using the location of London on the main map (hex U13), not
    the zoomed in London hexes in the North Sea.

--- lib/engine/game/g_1807/step/token.rb
+++ lib/engine/game/g_1807/step/token.rb
@@ -7,9 +7,18 @@ module Engine
     module G1807
       module Step
         class Token < G1867::Step::Token
+          private
+
           def hex_distance_from_token(used_tokens, hex)
-            hex = @game.london_small if @game.london_zoomed.include?(hex)
-            super(used_tokens, hex)
+            to = hex_on_map(hex)
+            used_tokens.map { |from| hex_on_map(from).distance(to) }.min
+          end
+
+          # For token cost calculations, distances to or from London cities
+          # should be calculated from the small London hex on the main map
+          # (U13), not the zoomed in London hex where the tokens are located.
+          def hex_on_map(hex)
+            @game.london_zoomed.include?(hex) ? @game.london_small : hex
           end
         end
       end
