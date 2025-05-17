230686ba7c28ef54cd03c3c91068a83057f1fdc8
Oliver Burnett-Hall <olly@burnett-hall.co.uk>
2025-05-17 19:27:29 +0100

    [1807] Implement minor take over
    In the merger rounds, on a minor company's turn, it can choose to be
    taken over by a public company. The public company receives the minor's
    assets (tokens, cash, trains, private companies), the minor's owner
    receives a share of the public company from its treasury.
    
    This commit makes changes to the merger round view to allow different
    groups of corporations to be shown, so you first have the minor
    companies that can merge with the active minor, then the public
    companies that could acquire it.

--- lib/engine/game/g_1807/step/merge.rb
+++ lib/engine/game/g_1807/step/merge.rb
@@ -7,6 +7,42 @@ module Engine
     module G1807
       module Step
         class Merge < G1867::Step::Merge
+          def description
+            return 'Choose public company' if @converting || @merge_major
+            return 'Merge public company' if @merging
+
+            'Convert, merge or takeover minor company'
+          end
+
+          def mergeable_type(corporation)
+            if @converting
+              'New public company for conversion:'
+            elsif @merge_major
+              'New public company for merger:'
+            else
+              "Minor companies that can merge with #{corporation.name}:"
+            end
+          end
+
+          def exchange_name(_entity = nil)
+            'Take over'
+          end
+
+          def exchangeable_type(corporation)
+            "Public companies that can take over #{corporation.name}:"
+          end
+
+          def actions(entity)
+            return [] unless entity == current_entity
+            return [] unless entity.corporation?
+            return [] if @round.converted
+
+            actions = super
+            # FIXME: need to avoid checking graph in actions method
+            actions << 'buy_shares' if !@merging && can_exchange?(entity)
+            actions
+          end
+
           def mergeable(corporation)
             if @converting || @merge_major
               @game.corporations.select do |target|
@@ -14,8 +50,29 @@ module Engine
                 !target.floated?
               end
             else
-              mergeable_candidates(corporation)
+              {
+                mergeable_type(corporation) => mergeable_candidates(corporation),
+                exchangeable_type(corporation) => exchange_candidates(corporation),
+              }
             end
+          end
+
+          def exchanging?(corporation)
+            can_exchange?(corporation)
+          end
+
+          def process_buy_shares(action)
+            bundle = action.bundle
+            major = bundle.corporation
+            minor = action.entity
+            player = minor.owner
+
+            if !@game.loading && !exchange_candidates(minor).include?(major)
+              raise GameError, "#{major.name} cannot take over #{minor.name}"
+            end
+
+            @game.share_pool.buy_shares(player, bundle, exchange: :free)
+            takeover(minor, major)
           end
 
           private
@@ -24,10 +81,79 @@ module Engine
             corporation.placed_tokens.map(&:city).intersect?(@game.london_cities)
           end
 
+          def token_location(token)
+            hex =
+              if @game.london_cities.include?(token.city)
+                @game.london_small
+              else
+                token.city.hex
+              end
+            "#{hex.location_name} (#{token.hex.coordinates})"
+          end
+
+          # Cities that a corporation can trace a route to.
           def connected_cities(corporation)
             return super unless london_token?(corporation)
 
             super | @game.london_cities
+          end
+
+          # Cities that are on the same hex as one where the corporation has a token.
+          # This will include the cities where the corporation's tokens are.
+          def colocated_cities(corporation)
+            cities = corporation.placed_tokens.flat_map { |t| t.hex.tile.cities }
+
+            if london_token?(corporation)
+              cities | @game.london_cities
+            else
+              cities
+            end
+          end
+
+          # Finds the public companies (majors) that are able to acquire a
+          # minor company. To do this they need to have a treasury share and
+          # must be able to trace a route to one of the minor's tokens, or have
+          # tokens co-located on the same tile.
+          def exchange_candidates(minor)
+            cities = connected_cities(minor) | colocated_cities(minor)
+            cities.flat_map { |city| city.tokens.compact.map(&:corporation) }
+                  .uniq
+                  .select { |corp| corp.type == :public && corp.num_treasury_shares.positive? }
+          end
+
+          def can_exchange?(corporation)
+            return false unless corporation.corporation?
+            return false unless corporation.type == :minor
+
+            !exchange_candidates(corporation).empty?
+          end
+
+          def remove_duplicate_tokens(surviving, others)
+            # When minors are merging, or a minor is being acquired by a public
+            # company then duplicate tokens are allowed in London.
+            return if surviving.type == :minor && london_token?(surviving)
+
+            # When public companies are merging to form a system then there
+            # are no restrictions on duplicate tokens.
+            return if surviving.type == :public
+
+            super
+          end
+
+          def takeover(minor, major)
+            remove_duplicate_tokens(minor, [major])
+            received = minor.placed_tokens.map do |token|
+              next unless (new_token = major.next_token)
+
+              location_name = token_location(token)
+              city = token.city
+              token.remove!
+              city.place_token(major, new_token, check_tokenable: false)
+              "a token in #{location_name}"
+            end.compact
+            received += move_assets(minor, major)
+            @game.close_corporation(minor)
+            @log << "#{major.name} takes over #{minor.name} receiving #{received.join(', ')}"
           end
         end
       end
