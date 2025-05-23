1f91a09d2351270a039a90e3b144fa2b2735df80
Patrik Olesen <patrik@hemma.org>
2025-05-23 05:49:17 +0200

    [18Norway] Nationalization rounds should not pay out companies

--- lib/engine/game/g_18_norway/round/nationalization.rb
+++ lib/engine/game/g_18_norway/round/nationalization.rb
@@ -7,6 +7,18 @@ module Engine
     module G18Norway
       module Round
         class Nationalization < Engine::Round::Operating
+          def initialize(game, steps, **opts)
+            super
+            @nationalization_complete = false
+          end
+
+          def process_action(action)
+            super
+            @nationalization_complete = true if action.is_a?(Engine::Action::Choose) && action.choice != 'decline'
+          end
+
+          attr_reader :nationalization_complete
+
           def name
             'Nationalization round'
           end
@@ -17,6 +29,10 @@ module Engine
 
           def show_in_history?
             false
+          end
+
+          def self.short_name
+            'Nationalization'
           end
         end
       end
