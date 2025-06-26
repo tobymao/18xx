2aa7165d090373672f1ed23a6732260a9db4350e
Phil Campeau <philcampeau@gmail.com>
2025-06-26 14:09:03 -0400

    another tweak

--- lib/engine/game/g_system18/map_britain_customization.rb
+++ lib/engine/game/g_system18/map_britain_customization.rb
@@ -320,8 +320,10 @@ module Engine
             case @round
             when Engine::Round::Stock
               map_britain_stock_round_finished
-              reorder_players
               @operating_rounds = @phase.operating_rounds
+              reorder_players
+              return if @finished
+
               new_operating_round
             when Round::Operating
               if @round.round_num < @operating_rounds
