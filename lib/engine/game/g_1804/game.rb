5442b4b22ffca72b36933c8f98b49609ad327e5e
jclancy15 <josephmclancy@gmail.com>
2025-07-27 02:41:50 -0400

    Add files via upload

---
+++ lib/engine/game/g_1804/game.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+require_relative 'entities'
+require_relative 'map'
+require_relative 'meta'
+require_relative '../base'
+
+module Engine
+  module Game
+    module G1804
+      class Game < Game::Base
+        include_meta(G1804::Meta)
+        include Entities
+        include Map
+
+        register_colors(red: '#d1232a',
+                        orange: '#f58121',
+                        black: '#110a0c',
+                        blue: '#025aaa',
+                        lightBlue: '#8dd7f6',
+                        yellow: '#ffe600',
+                        green: '#32763f',
+                        brightGreen: '#6ec037')
+        TRACK_RESTRICTION = :permissive
+        SELL_BUY_ORDER = :sell_buy_sell
+        TILE_RESERVATION_BLOCKS_OTHERS = :always
+        CURRENCY_FORMAT_STR = '$%s'
+
+        BANK_CASH = 12_000
+
+        CERT_LIMIT = { 2 => 28, 3 => 20, 4 => 16, 5 => 13, 6 => 11 }.freeze
+
+        STARTING_CASH = { 2 => 1200, 3 => 800, 4 => 600, 5 => 480, 6 => 400 }.freeze
+
+        MARKET = [
+          ['', '', '129', '148', '165', '190', '215', '240', '275', '300', '325', '350', '375', '400'],
+          %w[90y 100p 115 129 148 165 190 215 240 275 300 325 350 375],
+          %w[82y 90 100 114 128 146 165 190 215 240 275 300 325 350],
+          %w[76y 82y 90p 100 114 128 146 160 180 200 225 250 275 300],
+          %w[71y 76y 82 90 100 113 127 144 160 180 200 225 250 275],
+          %w[67y 71y 76y 82p 90 100 113 126 144 160 180 200 225 250],
+          %w[60y 67y 71y 76 82 90 100 112 126 142 160 180 200 220],
+          %w[53y 60y 66y 70y 76p 82 90 100 112 126 142 155 170 185],
+          %w[46y 55y 60y 65y 70 76 82 90 100 111 125 140 155 170],
+          %w[39b 48y 54y 60y 66y 71p 76 82 90 100 110 120 130],
+          %w[32b 41y 48y 55y 62y 67 71 76 82 90 100],
+          %w[25b 34b 42y 50y 58y 65y 67p 71 75 80],
+          %w[18b 27b 36b 45y 54y 63y 67],
+          %w[10b 20b 30b 40b 50y 60y 67],
+          ['', '10b', '20b', '30b', '40b', '50y', '60y'],
+          ['', '', '10b', '20b', '30b', '40b', '50y'],
+          ['', '', '', '10b', '20b', '30b', '40b'],
+        ].freeze
+
+        PHASES = [{
+          name: '2',
+          train_limit: 5,
+          tiles: [:yellow],
+          operating_rounds: 1,
+          status: ['can_buy_companies_from_other_players'],
+        },
+                  {
+                    name: '3',
+                    on: '3',
+                    train_limit: 5,
+                    tiles: %i[yellow green],
+                    operating_rounds: 2,
+                    status: %w[can_buy_companies can_buy_companies_from_other_players],
+                  },
+                  {
+                    name: '4',
+                    on: '4',
+                    train_limit: 4,
+                    tiles: %i[yellow green],
+                    operating_rounds: 2,
+                    status: %w[can_buy_companies can_buy_companies_from_other_players],
+                  },
+                  {
+                    name: '5',
+                    on: '5',
+                    train_limit: 3,
+                    tiles: %i[yellow green brown],
+                    operating_rounds: 3,
+                  },
+                  {
+                    name: '6',
+                    on: '6',
+                    train_limit: 3,
+                    tiles: %i[yellow green brown],
+                    operating_rounds: 3,
+                  },
+                  {
+                    name: 'D',
+                    on: 'D',
+                    train_limit: 3,
+                    tiles: %i[yellow green brown],
+                    operating_rounds: 3,
+                  }].freeze
+
+        TRAINS = [{ name: '2', distance: 2, price: 80, rusts_on: '4', num: 7 },
+                  { name: '3', distance: 3, price: 180, rusts_on: '6', num: 6 },
+                  { name: '4', distance: 4, price: 300, rusts_on: 'D', num: 5 },
+                  {
+                    name: '5',
+                    distance: 5,
+                    price: 450,
+                    num: 4,
+                    events: [{ 'type' => 'close_companies' }],
+                  },
+                  { name: '6', distance: 6, price: 630, num: 3 },
+                  {
+                    name: 'D',
+                    distance: 999,
+                    price: 1100,
+                    num: 20,
+                    available_on: '6',
+                    discount: { '4' => 300, '5' => 300, '6' => 300 },
+                  }].freeze
+
+        def operating_round(round_num)
+          Round::Operating.new(self, [
+            G1804::Step::ExploratoryCommittee,
+            Engine::Step::Bankrupt,
+            Engine::Step::Exchange,
+            Engine::Step::SpecialTrack,
+            Engine::Step::SpecialToken,
+            Engine::Step::BuyCompany,
+            Engine::Step::HomeToken,
+            Engine::Step::Track,
+            Engine::Step::Token,
+            Engine::Step::Route,
+            Engine::Step::Dividend,
+            Engine::Step::DiscardTrain,
+            G1804::Step::BuyTrain,
+            [Engine::Step::BuyCompany, { blocks: true }],
+          ], round_num: round_num)
+        end
+
+        def stock_round
+          Round::Stock.new(self, [
+            Engine::Step::DiscardTrain,
+            Engine::Step::Exchange,
+            Engine::Step::SpecialTrack,
+            G1804::Step::BuySellParShares,
+          ])
+        end
+
+        STATUS_TEXT = Base::STATUS_TEXT.merge(
+         'can_buy_companies_from_other_players' => ['Interplayer Company Buy',
+                                                    'Companies can be bought between players after first stock round'],
+       ).freeze
+
+        def multiple_buy_only_from_market?
+          false
+        end
+
+        def market_share_limit(_corporation = nil)
+          40
+        end
+      end
+    end
+  end
+end
