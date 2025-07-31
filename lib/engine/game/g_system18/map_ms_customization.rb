7ecdf9a555e73799652e55046a5a6f04d27b8cd8
Steve Undy <steve@roseundy.net>
2025-07-30 22:03:39 -0600

    Fix 4-train count for MS

--- lib/engine/game/g_system18/map_ms_customization.rb
+++ lib/engine/game/g_system18/map_ms_customization.rb
@@ -164,7 +164,7 @@ module Engine
         def map_ms_game_trains(trains)
           find_train(trains, '2')[:num] = 4
           find_train(trains, '3')[:num] = 3
-          find_train(trains, '4')[:num] = 2
+          find_train(trains, '4')[:num] = 3
           find_train(trains, '4')[:rusts_on] = '8'
           find_train(trains, '5')[:num] = 2
           find_train(trains, '5')[:price] = 450
