8cf51c1b521692214fbbd349fde01c338a030e82
chrstas <mail@christian-stasius.de>
2026-01-22 18:13:46 +0000

    18Cuba: sugar production logic

--- lib/engine/game/g_18_cuba/game.rb
+++ lib/engine/game/g_18_cuba/game.rb
@@ -207,7 +207,7 @@ module Engine
             Engine::Step::Track,
             Engine::Step::Token,
             Engine::Step::Route,
-            Engine::Step::Dividend,
+            G18Cuba::Step::Dividend,
             Engine::Step::DiscardTrain,
             Engine::Step::BuyTrain,
             [Engine::Step::BuyCompany, { blocks: true }],
@@ -236,10 +236,51 @@ module Engine
           @tile_groups = init_tile_groups
           initialize_tile_opposites!
           @unused_tiles = []
+          @sugar_cubes = Hash.new(0)
         end
 
         def init_tile_groups
           self.class::TILE_GROUPS
+        end
+
+        def sugar_production(corporation, total_revenue)
+          return if total_revenue.zero? || !corporation.minor?
+
+          sugar_cubes = case total_revenue
+                        when 0..29
+                          0
+                        when 30..79
+                          1
+                        when 80..150
+                          2
+                        else
+                          3
+                        end
+
+          @sugar_cubes[corporation] += sugar_cubes
+          @log << "#{corporation.name} produces #{sugar_cubes} sugar cube(s) "\
+                  "from #{format_currency(total_revenue)} revenue "\
+                  "(total sugar cubes: #{@sugar_cubes[corporation]})."
+        end
+
+        def transition_to_next_round!
+          finish_round!
+          super
+        end
+
+        def finish_round!
+          case @round
+          when Round::Operating
+            or_round_finished
+          end
+        end
+
+        def or_round_finished
+          # For the moment reset sugar cubes, handling for FC to be implemented later
+          return if @sugar_cubes.empty?
+
+          @sugar_cubes.clear
+          @log << 'All sugar cubes are removed at the end of the Operating Round.'
         end
       end
     end
