41d36c31c142710bb33c2d73d0e9ecc81a7cb682
Jude Hoekstra <90494861+JudeH1@users.noreply.github.com>
2026-02-22 11:27:08 -0600

    Update lib/engine/game/g_1832/step/dividend.rb
    Co-authored-by: Oliver Burnett-Hall <olly@burnett-hall.co.uk>

--- lib/engine/game/g_1832/step/dividend.rb
+++ lib/engine/game/g_1832/step/dividend.rb
@@ -2,26 +2,13 @@
 
 require_relative '../../../step/dividend'
 require_relative '../../../step/half_pay'
+require_relative '../../g_1870/step/dividend'
 
 module Engine
   module Game
     module G1870
       module Step
-        class Dividend < Engine::Step::Dividend
-          DIVIDEND_TYPES = %i[payout half withhold].freeze
-          include Engine::Step::HalfPay
-
-          def holder_for_corporation(entity)
-            entity
-          end
-
-          def share_price_change(_entity, revenue)
-            return { share_direction: :left, share_times: 1 } if revenue.zero?
-
-            return { share_direction: :right, share_times: 1 } if revenue == @game.routes_revenue(routes)
-
-            {}
-          end
+        class Dividend < G1870::Step::Dividend
 
           def payout_shares(entity, revenue)
             per_share = payout_per_share(entity, revenue)
