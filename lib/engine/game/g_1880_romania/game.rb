40fc37ef2994992c9b9defad1089a8753e50ced6
Phil Campeau <philcampeau@gmail.com>
2026-04-01 14:19:36 -0400

    mask company abilities on corps unless assigned

--- lib/engine/game/g_1880_romania/game.rb
+++ lib/engine/game/g_1880_romania/game.rb
@@ -4,6 +4,7 @@ require_relative 'meta'
 require_relative '../g_1880/game'
 require_relative 'map'
 require_relative 'entities'
+require_relative 'corporation'
 require_relative 'minor'
 require_relative 'step/route'
 require_relative 'step/special_choose'
@@ -15,6 +16,8 @@ module Engine
         include_meta(G1880Romania::Meta)
         include G1880Romania::Map
         include G1880Romania::Entities
+
+        CORPORATION_CLASS = G1880Romania::Corporation
 
         CURRENCY_FORMAT_STR = 'L%s'
 
