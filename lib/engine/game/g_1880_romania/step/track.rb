d3817b75f3150f15edebc90fa2b34d5373d33c5e
Phil Campeau <philcampeau@gmail.com>
2026-04-02 14:17:57 -0400

    add blocking step to track step for Malaxa assignment

--- lib/engine/game/g_1880_romania/step/track.rb
+++ lib/engine/game/g_1880_romania/step/track.rb
@@ -10,6 +10,13 @@ module Engine
         class Track < Engine::Step::Track
           include G1880Romania::Tracker
 
+          def tile_lay_abilities_should_block?(entity)
+            return true if !@game.can_build_track?(entity) &&
+                           entity.owner == @game.malaxa.owner
+
+            super
+          end
+
           def max_exits(tiles)
             # Ignore max exits for city/town option upgrades
             return tiles if tiles.any? { |t| !t.towns.empty? }
