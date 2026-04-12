6cdb65f5b30e7182b26d61193701256e0a118ea4
Witzman <witzman@witzman.de>
2026-04-12 23:03:38 +0200

    feaures

--- lib/engine/game/g_18_oe/step/home_token.rb
+++ lib/engine/game/g_18_oe/step/home_token.rb
@@ -12,7 +12,14 @@ module Engine
             region = G18OE::Game::NATIONAL_REGION_HEXES.select { |_key, value| value.include?(hex.name.to_s) }.keys.first
 
             token.price = G18OE::Game::TRACK_RIGHTS_COST[region] || 0
-            @game.minor_available_regions.delete_at(@game.minor_available_regions.index(region))
+
+            # TEMPORARY WORKAROUND: region is nil when hex falls outside all defined zones
+            # (NATIONAL_REGION_HEXES_COMPLETE = false). Guard delete_at against nil index
+            # so placement in undefined zones doesn't raise. Remove guard once all 8 zone
+            # hex lists are complete and NATIONAL_REGION_HEXES_COMPLETE is flipped to true.
+            idx = @game.minor_available_regions.index(region)
+            @game.minor_available_regions.delete_at(idx) if idx
+
             @game.minor_floated_regions[action.entity.id] = region
 
             super
