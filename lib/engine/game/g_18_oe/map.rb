68ffd824db69c5753e51a5c337600a1995ab5e6a
witzman <witzman@witzman.de>
2026-04-22 23:06:15 +0200

    fix

--- lib/engine/game/g_18_oe/map.rb
+++ lib/engine/game/g_18_oe/map.rb
@@ -521,8 +521,8 @@ module Engine
             ['B83'] => 'offboard=revenue:yellow_30|green_50|brown_60|gray_60',                                                           # Arkhangelsk — no path defined
             ['F87'] => 'offboard=revenue:yellow_30|green_50|brown_80|gray_100;city=revenue:0,slots:3;path=a:0,b:_0;path=a:_0,b:1;path=a:_0,b:2;path=a:_0,b:5', # Moskva
             ['N1'] => 'offboard=revenue:green_60|brown_100|gray_160;path=a:5,b:_0',                                                     # New York
-            ['N87'] => 'offboard=revenue:yellow_30|green_40|brown_60|gray_80;path=a:0,b:_0;path=a:1,b:_0;path=a:2,b:_0',               # Kharkov
-            ['T87'] => 'offboard=revenue:yellow_30|green_40|brown_60|gray_80;path=a:1,b:_0',                                            # Sevastopol
+            ['N87'] => 'offboard=revenue:yellow_30|green_40|brown_60|gray_80;city=revenue:0,slots:2;path=a:0,b:_0;path=a:_0,b:1;path=a:_0,b:2',               # Kharkov
+            ['T87'] => 'offboard=revenue:yellow_30|green_40|brown_60|gray_80;city=revenue:0,slots:2;path=a:0,b:_0;path=a:_0,b:3',         # Sevastopol
             ['Z1'] => 'offboard=revenue:yellow_30|green_40|brown_60|gray_80;city=revenue:0,slots:2;path=a:3,b:_0;path=a:_0,b:4', # Lisboa (2 station slots; RCP home)
             ['AB87'] => 'offboard=revenue:yellow_30|green_50|brown_80|gray_120;path=a:1,b:_0;path=a:2,b:_0',                            # Levant
             ['AD1'] => 'offboard=revenue:green_40|brown_80|gray_120;path=a:4,b:_0',                                                     # North Africa & The Americas
