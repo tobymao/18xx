341b7920bca9ebdef33d3009fde41dfe6a91e259
Jeremiah Lee <jeremiah.lee@gmail.com>
2026-06-14 21:00:14 -0700

    [1832] Add system code to corporation, add logic for system train buys.

--- lib/engine/game/g_1832/corporation.rb
+++ lib/engine/game/g_1832/corporation.rb
@@ -6,7 +6,11 @@ module Engine
   module Game
     module G1832
       class Corporation < Engine::Corporation
-        attr_accessor :coal_token
+        attr_accessor :coal_token, :is_system, :system_shells
+
+        def system?
+          is_system
+        end
       end
     end
   end
