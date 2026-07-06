e4491f4f917d285d2a73cc2a1382f92c7554c9d3
Stefan Bleeck <bleeck@gmail.com>
2026-07-06 18:02:24 +0100

    1835 Pfb p6

--- lib/engine/game/g_1835/step/special_track.rb
+++ lib/engine/game/g_1835/step/special_track.rb
@@ -7,28 +7,36 @@ module Engine
     module G1835
       module Step
         class SpecialTrack < Engine::Step::SpecialTrack
-          def actions(entity)
-            if entity.company?
-              return [] unless entity.owner == @round.current_entity&.owner
-
-              ability = @game.abilities(entity, :tile_lay)
-              return [] unless ability
-
-              ['lay_tile']
-            else
-              super
-            end
-          end
-
           def active_entities
+            entities = super
             companies = @game.companies.select do |c|
               c.owner == @round.current_entity&.owner && @game.abilities(c, :tile_lay)
             end
-            companies.empty? ? super : companies
+            combined = (entities + companies).uniq
+
+            # HYPOTHESIS TEST LOGGING
+            @game.log << "TEST LOG - active_entities: #{combined.map(&:name).join(', ')}" if companies.any?
+
+            combined
           end
 
-          def can_process_action?(action)
-            action.entity.company? && super
+          def actions(entity)
+            acts = super
+            if entity.company? && entity.owner == @round.current_entity&.owner && @game.abilities(entity, :tile_lay)
+              acts |= ['lay_tile']
+            end
+
+            # HYPOTHESIS TEST LOGGING
+            @game.log << "TEST LOG - actions for #{entity.name}: #{acts.inspect}" if acts.include?('lay_tile')
+
+            acts
+          end
+
+          def process_lay_tile(action)
+            # HYPOTHESIS TEST LOGGING
+            @game.log << "TEST LOG - process_lay_tile triggered by: #{action.entity.name}"
+
+            super
           end
         end
       end
