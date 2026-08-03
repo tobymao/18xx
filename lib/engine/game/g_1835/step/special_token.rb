f49043d38eb6f64a9a355f6388b8373c235899fb
Stefan Bleeck <bleeck@gmail.com>
2026-08-03 19:01:38 +0200

    1835: Core engine setup, entity definitions, and step rules

--- lib/engine/game/g_1835/step/special_token.rb
+++ lib/engine/game/g_1835/step/special_token.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
-require_relative '../../../step/special_track'
+require_relative '../../../step/special_token'
 
 module Engine
   module Game
     module G1835
       module Step
         class SpecialToken < Engine::Step::SpecialToken
-          def available_tokens(entity)
-            return [] unless entity.corporation?
-
-            entity.tokens_by_type
-          end
         end
       end
     end
